fa707185f9f16b5ea0c384c7d920a4e6f77c853d
Andrew Hearin <andrew.hearin@yale.edu>
2014-07-01 18:17:30 -0400

    minor and temporary modifications to test_hod_mocks.py

---
+++ test_HOD_mocks.py
@@ -0,0 +1,11 @@
+import halo_occupation as ho
+import numpy as np
+import mock
+import read_nbody
+
+
+m = mock.HOD_mock()
+print("")
+print("Mock successfully created")
+print("Satellite fraction = "+str(m.galaxies['satellite_fraction']))
+print('')
