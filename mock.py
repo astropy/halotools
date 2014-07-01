f9d16dfaaefd6cf70f94f2d75f738f3829c35b1f
Andrew Hearin <andrew.hearin@yale.edu>
2014-07-01 11:53:35 -0400

    Changed data structure of mock.halos to be a dictionary of numpy arrays

--- mock.py
+++ mock.py
@@ -1,15 +1,18 @@
 
 
 import read_nbody
-import halo_occupation
-
+import halo_occupation as ho
+import numpy as np
 
 class HOD_mock(object):
 	'''Base class for any HOD-based mock object'''
 
 	def __init__(self):
-		halos = read_nbody.load_bolshoi_host_halos_fits()
-		self.halos = halos
+		temp_halos = read_nbody.load_bolshoi_host_halos_fits()
+		self.halos = {}
+		self.halos['logM'] = np.array(np.log10(temp_halos.MVIR))
+		self.halos['conc'] = np.array(temp_halos.RVIR/temp_halos.RS)
+
 
 
 
