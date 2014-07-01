17cbaee73c3f3389e2864c73cbb7fb9130c7afd1
Andrew Hearin <andrew.hearin@yale.edu>
2014-07-01 12:19:27 -0400

    Reconfigured mock object to use hod_dict as container dictionary for all HOD params

--- halo_occupation.py
+++ halo_occupation.py
@@ -8,24 +8,37 @@ Created on Mon Jun 23 10:52:05 2014
 import numpy as np
 from scipy.special import erf
 
-def ncen(logM,logMmin_cen=11.75,sigma_logM=0.2):
+def mean_ncen(logM,hod_dict=None):
     """Expected number of central galaxies in a halo of mass 10**logM"""
-    mean_ncen = 0.5*(1.0 + erf((logM - logMmin_cen)/sigma_logM))
+
+    if hod_dict == None:
+    	hod_dict['logMmin_cen'] = 11.75
+    	hod_dict['sigma_logM'] = 0.2
+
+    mean_ncen = 0.5*(1.0 + erf((logM - hod_dict['logMmin_cen'])/hod_dict['sigma_logM']))
     return mean_ncen
 
-def nsat(logM,logMmin_sat=12,Msat_ratio=20,alpha_sat=1):
+#def ng_ncen(logM)
+
+def mean_nsat(logM,hod_dict=None):
     """Expected number of satellite galaxies in a halo of mass 10**logM."""
     halo_mass = 10.**logM
-    Mmin_sat = 10.**logMmin_sat
-    M1_sat = Msat_ratio*Mmin_sat
+
+    if hod_dict == None:
+    	hod_dict['logMmin_sat'] = 12.25
+    	hod_dict['Msat_ratio'] = 20.0
+    	hod_dict['alpha_sat'] = 1.0
+
+    Mmin_sat = 10.**hod_dict['logMmin_sat']
+    M1_sat = hod_dict['Msat_ratio']*Mmin_sat
 
     mean_nsat = np.zeros(len(logM),dtype='f8')
     idx_nonzero_satellites = (halo_mass - Mmin_sat) > 0
-    mean_nsat[idx_nonzero_satellites] = ((halo_mass[idx_nonzero_satellites] - Mmin_sat)/M1_sat)**alpha_sat
+    mean_nsat[idx_nonzero_satellites] = ((halo_mass[idx_nonzero_satellites] - Mmin_sat)/M1_sat)**hod_dict['alpha_sat']
 
     return mean_nsat
 
-    
+
 
 
 
--- mock.py
+++ mock.py
@@ -8,10 +8,25 @@ class HOD_mock(object):
 	'''Base class for any HOD-based mock object'''
 
 	def __init__(self):
+		# read in .fits file containing pre-processed z=0 ROCKSTAR host halo catalog
 		temp_halos = read_nbody.load_bolshoi_host_halos_fits()
+		# create a dictonary of numpy arrays containing relevant halo information
 		self.halos = {}
 		self.halos['logM'] = np.array(np.log10(temp_halos.MVIR))
 		self.halos['conc'] = np.array(temp_halos.RVIR/temp_halos.RS)
+
+		# create a dictionary of numpy arrays containing mock galaxies
+		self.galaxies = {}
+
+		# create a dictionary containing the HOD parameters
+		self.hod_dict = {}
+		self.hod_dict['logMmin_cen'] = 12.0
+		self.hod_dict['sigma_logM'] = 0.2
+		self.hod_dict['logMmin_sat'] = 12.25
+		self.hod_dict['Msat_ratio'] = 20.0
+		self.hod_dict['alpha_sat'] = 1.0
+
+
 
 
 
