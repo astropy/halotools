469d0c61c54f4b3569867ea9adcedb89bdfc2382
Andrew Hearin <andrew.hearin@yale.edu>
2014-07-06 16:43:07 -0400

    Simple changes to defaults.py

--- defaults.py
+++ defaults.py
@@ -5,6 +5,8 @@ This module expresses the default values for the halo occupation models.
 class simulation(object):
     ''' Container class for properties of the simulation being used.
     
+    Still unused.
+    
     
     '''
     
@@ -19,6 +21,19 @@ class simulation(object):
             'particle_mass':1.35e8,
             'softening':1.0
             }
+        elif simulation_nickname is 'Bolshoi':
+            self.halo_catalog_filename='/Users/aphearin/Dropbox/mock_for_surhud/VALUE_ADDED_HALOS/value_added_z0_halos.fits'
+            self.simulation_dict = {
+            'catalog_filename':default_halo_catalog_filename,
+            'Lbox':250.0,
+            'scale_factor':1.0003,
+            'particle_mass':1.35e8,
+            'softening':1.0
+            }
+        else:
+            pass
+        
+                    
 
 ### Default halo catalog
 default_halo_catalog_filename='/Users/aphearin/Dropbox/mock_for_surhud/VALUE_ADDED_HALOS/value_added_z0_halos.fits'
