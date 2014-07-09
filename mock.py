77dd03dfc670369af4572914d1d0c4f71f8d4c14
Andrew Hearin <andrew.hearin@yale.edu>
2014-07-09 10:33:28 -0400

    Changed mock.halos and mock.galaxies to be numpy record arrays

--- mock.py
+++ mock.py
@@ -4,7 +4,6 @@ import read_nbody
 import halo_occupation as ho
 import numpy as np
 import defaults
-import copy
 
 class HOD_mock(object):
 	'''Base class for any HOD-based mock galaxy catalog object.
@@ -30,13 +29,14 @@ class HOD_mock(object):
 		simulation_data = read_nbody.load_bolshoi_host_halos_fits()
 		temp_halos = simulation_data['halos']
 
-		# create a dictonary of numpy arrays containing relevant halo information
-		self.halos = {}
-		self.halos['logM'] = np.array(np.log10(temp_halos.MVIR))
-		self.halos['conc'] = np.array(temp_halos.RVIR/temp_halos.RS)
-		self.halos['ID'] = np.array(temp_halos.ID)
-		self.halos['pos'] = np.array([temp_halos.POS[:,0],temp_halos.POS[:,1],temp_halos.POS[:,2]])
-		self.halos['vel'] = np.array([temp_halos.VEL[:,0],temp_halos.VEL[:,1],temp_halos.VEL[:,2]])
+		# create a dictonary of numpy arrays containing relevant halo information		
+		halo_data_structure=[('logM','f4'),('conc','f4'),('ID','i8'),('pos','3float32'),('vel','3float32'),('rvir','f4')]
+		self.halos = np.zeros(len(temp_halos.MVIR),dtype=halo_data_structure)				
+		self.halos['logM'] = np.log10(temp_halos.MVIR)
+		self.halos['conc'] = temp_halos.RVIR/temp_halos.RS
+		self.halos['ID'] = temp_halos.ID
+		self.halos['pos'] = temp_halos.POS
+		self.halos['vel'] = temp_halos.VEL
 		self.halos['rvir'] = np.array(temp_halos.RVIR)
 
 		# mock object should know the basic attributs of its simulation
@@ -54,14 +54,18 @@ class HOD_mock(object):
 		else:
 			self.color_dict = color_dict
 
-		self.halos['ncen'] = ho.num_ncen(self.halos['logM'],self.hod_dict)
-		self.halos['nsat'] = ho.num_nsat(self.halos['logM'],self.hod_dict)
+		ncen_array = ho.num_ncen(self.halos['logM'],self.hod_dict)
+		nsat_array = ho.num_nsat(self.halos['logM'],self.hod_dict)
+		total_number_centrals = np.sum(ncen_array)
+		total_number_satellites = np.sum(nsat_array)
+		total_number_gals = total_number_centrals + total_number_satellites
 
 				# create a dictionary of numpy arrays containing mock galaxies
-		self.galaxies = {}
-		self.galaxies['num_gals'] = np.sum(self.halos['ncen']) + np.sum(self.halos['nsat'])
-		self.galaxies['satellite_fraction'] = (1.0*np.sum(self.halos['nsat']))/self.galaxies['num_gals']
-		self.galaxies['logM'] = self.halos['logM']
+		galaxy_data_structure=[('logM','f4'),('conc','f4'),('haloID','i8'),('pos','3float32'),('vel','3float32'),('rvir','f4'),('icen','i2'),('ired','i2')]
+		self.galaxies = np.zeros(total_number_gals,dtype=galaxy_data_structure)
+
+
+		self.satellite_fraction = 1.0*total_number_satellites/(1.0*total_number_gals)
 
 
 
--- test_HOD_mocks.py
+++ test_HOD_mocks.py
@@ -7,7 +7,7 @@ import read_nbody
 m = mock.HOD_mock()
 print("")
 print("Mock with all defaults successfully created")
-print("Satellite fraction = "+str(m.galaxies['satellite_fraction']))
+print("Satellite fraction = "+str(m.satellite_fraction))
 print('')
 
 temp_halos = read_nbody.load_bolshoi_host_halos_fits()
@@ -16,3 +16,8 @@ x=[0,1,-1,2]
 y=[10,15,11,26]
 coeff = ho.solve_for_quenching_polynomial_coefficients(x,y)
 #coeff should be [10,2,3,0]
+
+z = np.zeros(3,dtype=[('col1','i8'),('col2','f4'),('col3','3float32')])
+z['col1'] = [100000,488888,299999]
+z['col2']=[0.4,0.5,0.2]
+z['col3']=[(3,4,5),(4,2.,0),(5,7,45)]
