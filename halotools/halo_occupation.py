692134043a1491c13ad217cdbf64fb8ba1cdb784
Andrew Hearin <andrew.hearin@yale.edu>
2014-07-29 19:04:20 -0400

    Created abstract base class for models of galaxy color

--- halotools/halo_occupation.py
+++ halotools/halo_occupation.py
@@ -21,33 +21,6 @@ from abc import ABCMeta, abstractmethod
 import warnings
 
 
-
-def mean_ncen(logM,hod_dict=None):
-    """ Expected number of central galaxies in a halo of mass 10**logM.
-
-    Parameters
-    ----------        
-    logM : float or array
-    hod_dict : dictionary
-
-    Returns
-    -------
-    mean_ncen : float or array
-    
-    Synopsis
-    -------
-    Mean number of central galaxies in a host halo of the specified mass. Values are restricted 0 <= mean_ncen <= 1.
-
-
-    """
-
-    if hod_dict == None:
-        hod_dict = defaults.default_hod_dict
-
-    mean_ncen = 0.5*(1.0 + erf((logM - hod_dict['logMmin_cen'])/hod_dict['sigma_logM']))
-    return mean_ncen
-
-
 def num_ncen(logM,hod_dict):
     """ Returns Monte Carlo-generated array of 0 or 1 specifying whether there is a central in the halo.
 
@@ -65,35 +38,6 @@ def num_ncen(logM,hod_dict):
 
     num_ncen_array = np.array(mean_ncen(logM,hod_dict) > np.random.random(len(logM)),dtype=int)
     return num_ncen_array
-
-def mean_nsat(logM,hod_dict=None):
-    """Expected number of satellite galaxies in a halo of mass 10**logM.
-
-    Parameters
-    ----------
-    logM : float or array
-    hod_dict : dictionary
-
-    Returns
-    -------
-    mean_nsat : float or array
-        Mean number of satellite galaxies in a host halo of the specified mass. 
-
-    
-    """
-    halo_mass = 10.**logM
-
-    if hod_dict == None:
-        hod_dict = defaults.default_hod_dict
-
-    Mmin_sat = 10.**hod_dict['logMmin_sat']
-    M1_sat = hod_dict['Msat_ratio']*Mmin_sat
-
-    mean_nsat = np.zeros(len(logM),dtype='f8')
-    idx_nonzero_satellites = (halo_mass - Mmin_sat) > 0
-    mean_nsat[idx_nonzero_satellites] = ((halo_mass[idx_nonzero_satellites] - Mmin_sat)/M1_sat)**hod_dict['alpha_sat']
-
-    return mean_nsat
 
 def num_nsat(logM,hod_dict):
     '''  Returns Monte Carlo-generated array of integers specifying the number of satellites in the halo.
@@ -119,9 +63,6 @@ def num_nsat(logM,hod_dict):
     num_nsat_array = poisson.rvs(Prob_sat)
 
     return num_nsat_array
-
-def quenched_fraction_centrals(logM,hod_dict):
-    pass
 
 
 
@@ -252,8 +193,6 @@ def cumulative_NFW_PDF(r,c):
     return (np.log(1.+r*c) - r*c/(1.+r*c))/norm
 
 
-
-
 @six.add_metaclass(ABCMeta)
 class HOD_Model(object):
     """ Base class for model parameters determining the HOD.
@@ -298,12 +237,45 @@ class Zheng07_HOD_Model(HOD_Model):
 
 
     def mean_ncen(self,logM):
+        """
+        Expected number of central galaxies in a halo of mass 10**logM.
+
+        Parameters
+        ----------        
+        logM : float or array
+        hod_dict : dictionary
+
+        Returns
+        -------
+        mean_ncen : float or array
+    
+        Synopsis
+        -------
+        Mean number of central galaxies in a host halo of the specified mass. Values are restricted 0 <= mean_ncen <= 1.
+
+        """
+
         if not isinstance(logM,np.ndarray):
             raise TypeError("Input logM to mean_ncen must be a numpy array")
         mean_ncen = 0.5*(1.0 + erf((logM - self.parameter_dict['logMmin_cen'])/self.parameter_dict['sigma_logM']))
         return mean_ncen
 
     def mean_nsat(self,logM):
+        """Expected number of satellite galaxies in a halo of mass 10**logM.
+
+        Parameters
+        ----------
+        logM : float or array
+        hod_dict : dictionary
+
+        Returns
+        -------
+        mean_nsat : float or array
+            Mean number of satellite galaxies in a host halo of the specified mass. 
+
+    
+        """
+
         if not isinstance(logM,np.ndarray):
             raise TypeError("Input logM to mean_ncen must be a numpy array")
         halo_mass = 10.**logM
@@ -356,6 +328,25 @@ class Zheng07_HOD_Model(HOD_Model):
             raise TypeError("Input luminosity threshold does not match any of the Table 1 values of Zheng et al. 2007.")
 
         return parameter_dict
+
+
+@six.add_metaclass(ABCMeta)
+class Color_Model(object):
+    """ Base class for model parameters determining mock galaxy colors.
+    
+    
+    """
+
+    def __init__(self,model_nickname):
+        self.color_model_nickname = model_nickname
+
+    @abstractmethod
+    def quenched_fraction_centrals(self,logM):
+        raise NotImplementedError("quenched_fraction_centrals is not implemented")
+
+    @abstractmethod
+    def quenched_fraction_satellites(self,logM):
+        raise NotImplementedError("quenched_fraction_satellites is not implemented")
 
 
 
