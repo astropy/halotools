e7f7b2e7687695afcd3fdfa74bc1dcc7084cc671
Andrew Hearin <andrew.hearin@yale.edu>
2014-07-31 16:09:35 -0400

    Slight modification to test/test_make_mocks.py to work with py.test

--- halotools/test/test_make_mocks.py
+++ halotools/test/test_make_mocks.py
@@ -10,52 +10,15 @@ Will copy and paste my additional tests once I figure out the basic design conve
 #                        unicode_literals)
 
 import numpy as np
-from ..make_mocks import HOD_mock
-import timeit
-from copy import copy
+from ..halo_occupation import Zheng07_HOD_Model
 
 
-def test_make_HOD_mock():
+def test_zheng_model():
 
-	m = HOD_mock()
+	m = Zheng07_HOD_Model(-20.0)
+	test_mean_ncen = m.mean_ncen(np.array([10,11,12,13,14,15]))
 
-	assert np.all(m.galaxies['icen'][0:m.ncens] == 1)
-#	assert np.all(m.galaxies['icen'][m.ncens:-1] == 0)
-
-
-"""
-def time_mock():
-	timer_string = "m=make_mocks.HOD_mock(simulation_data = bolshoi_simulation)"
-	setup_string = "import make_mocks; import read_nbody; bolshoi_simulation = read_nbody.load_bolshoi_host_halos_fits()"
-	t = timeit.Timer(timer_string,setup=setup_string)
-	timeit_results =  t.repeat(3,1)
-	average_runtime_of_mock_creation = np.mean(timeit_results)
-	print("Average number of seconds to create mock:")
-	print(average_runtime_of_mock_creation)
-	print("")
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###################################################################################################
-# Trigger
-###################################################################################################
-
-if __name__ == "__main__":
-	main()
-
+	assert np.all(test_mean_ncen >= 0)
 
 
 
