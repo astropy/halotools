5bace1cffe6c481184ba50f5e926553d7a387e2b
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-08-18 17:02:54 -0700

    removed distance calcs from this file.

--- halotools/mock_observables/observables.py
+++ halotools/mock_observables/observables.py
@@ -7,68 +7,11 @@ the pair counter and subvolume membership methods.
 #from __future__ import (absolute_import, division, print_function,
 #                        unicode_literals)
 
-__all__=['one_dimensional_periodic_distance','three_dimensional_periodic_distance',
-'two_point_correlation_function']
+__all__=['two_point_correlation_function']
 
 import numpy as np
 from math import pi
 
-def one_dimensional_periodic_distance(x1,x2,Lbox):
-    """ 
-    Find the 1d Euclidean distance between x1 & x2, accounting for box periodicity.
-
-    Parameters
-    ----------
-    x1 : array_like
-        numpy array of 1-d positions. Should be between zero and Lbox.
-
-    x2 : array_like
-        numpy array of 1-d positions. Should be between zero and Lbox.
-
-    Lbox : float
-        Size of the simulation. Defines periodic boundary conditioning.
-
-    Returns
-    -------
-    periodic_distance : array
-
-    """
-    periodic_distance = np.abs(x1 - x2)
-    test = periodic_distance > 0.5*Lbox
-    periodic_distance[test] = np.abs(periodic_distance[test] - Lbox)
-    return periodic_distance
-
-def three_dimensional_periodic_distance(pos1,pos2,Lbox):
-    """ 
-    Find the 3d Euclidean distance between pos1 & pos2, accounting for box periodicity.
-
-    Parameters
-    ----------
-
-    pos1 : array_like
-        Npts x 3 numpy array containing 3d positions of Npts. Should be between zero and Lbox.
-
-    pos2 : array_like
-        Npts x 3 numpy array containing 3d positions of Npts. Should be between zero and Lbox.
-
-    Lbox : float
-        Size of the simulation. Defines periodic boundary conditioning.
-
-    Returns
-    -------
-    periodic_distance : array_like
-        Npts-length array containing element-wise distance between pos1 and pos2, 
-        accounting for box periodicity.
-
-    """
-
-    periodic_xdistance = one_dimensional_periodic_distance(pos1[:,0],pos2[:,0],Lbox)
-    periodic_ydistance = one_dimensional_periodic_distance(pos1[:,1],pos2[:,1],Lbox)
-    periodic_zdistance = one_dimensional_periodic_distance(pos1[:,2],pos2[:,2],Lbox)
-
-    distances = np.sqrt(periodic_xdistance**2 + periodic_ydistance**2 + periodic_zdistance**2)
-    
-    return distances 
 
 def two_point_correlation_function(sample1, rbins, sample2 = None, Lbox=None, max_sample_size=int(1e4)):
     """ Place-holder function for the two-point function. 
