f33b41fc90b06ec8a016af8c42d53c39278fc543
Andrew Hearin <andrew.hearin@yale.edu>
2014-10-12 14:55:19 -0400

    Beginning to revamp read_nbody. Currently committed version is likely broken.

--- halotools/read_nbody.py
+++ halotools/read_nbody.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Methods to load halo and particle catalogs into memory.
+Methods and classes to load halo and particle catalogs into memory.
 
 """
 
@@ -9,12 +9,69 @@ __all__=['simulation','particles']
 #from __future__ import (absolute_import, division, print_function,
 #                        unicode_literals)
 
-import astropy
-import defaults
+from astropy.io import fits as fits
 from astropy.table import Table
+from astropy.utils.data import get_readable_fileobj
+from astropy.utils.data import _get_download_cache_locs as get_download_cache_locs
+from astropy.utils.data import _open_shelve as open_shelve
+
+
 from configuration import Config
 import os, sys, warnings, urllib2
+import defaults
 
+
+
+def get_local_filename_from_remote_url(remote_url):
+    """
+    Helper function used to test whether catalog data has already been 
+    downloaded and stored in the astropy cache directory.
+
+    Parameters
+    ----------
+    remote_url : string 
+        String giving the url of the catalog to be downloaded.
+
+    Returns
+    -------
+    local_filename : string
+        String giving the filename where the catalog is stored locally.
+        The directory will be the default astropy cache download directory.
+        If None, then there is no catalog present in the local cache directory 
+        that corresponds to the input remote_url.
+
+    Notes 
+    -----
+    Brief explanation of the astropy cache system. Refer to the main docs page for details. 
+
+
+    """
+
+    url_key = remote_url
+
+    dldir, urlmapfn = get_download_cache_locs()
+    with open_shelve(urlmapfn, True) as url2hash:
+        if url_key in url2hash:
+            local_filename = url2hash[url_key]
+        else:
+            local_filename = None
+
+    return local_filename
+
+
+def get_halo_catalog_filename(nickname=defaults.default_simulation_name,
+    redshift=defaults.default_redshift,
+    scale_factor=defaults.default_scale_factor,
+    halo_finder=defaults.default_halo_finder,
+    web_location=defaults.aph_web_location):
+
+    #output_string = web_location+nickname+'_'+
+
+    
+    
+    pass
+
+##bolshoi_a1.0003_rockstar_V1.5_host_halos.fits'
 
 
 class simulation(object):
@@ -65,7 +122,7 @@ class simulation(object):
             self.simulation_name,self.scale_factor,self.halo_finder,self.use_subhalos)
 
         if os.path.isfile(cache_dir+self.filename)==True:
-            hdulist = astropy.io.fits.open(cache_dir+self.filename)
+            hdulist = fits.open(cache_dir+self.filename)
             halos = Table(hdulist[1].data)
 #            halos = Table(pyfits.getdata(cache_dir+self.filename,0))
         else:
@@ -81,7 +138,7 @@ class simulation(object):
                 output = open(cache_dir+self.filename,'wb')
                 output.write(fileobj.read())
                 output.close()
-                hdulist = astropy.io.fits.open(cache_dir+self.filename)
+                hdulist = fits.open(cache_dir+self.filename)
                 halos = Table(hdulist[1].data)
                 #halos = Table(pyfits.getdata(cache_dir+self.filename,0))
             else:
@@ -92,7 +149,47 @@ class simulation(object):
 
 ###################################################################################################
 
+class slac_rockstar(object):
+    """ Container class for managing I/O of Rockstar catalogs hosted at SLAC.
+    """
 
+    def __init__(self):
+        self.directory_urls = {'bolshoi_halos' : 'http://www.slac.stanford.edu/~behroozi/Bolshoi_Catalogs/',
+        'bolshoi_bdm_halos' : 'http://www.slac.stanford.edu/~behroozi/Bolshoi_Catalogs_BDM/',
+        'multidark_halos' : 'http://slac.stanford.edu/~behroozi/MultiDark_Hlists_Rockstar/',
+        'consuelo_halos' : 'http://www.slac.stanford.edu/~behroozi/Consuelo_Catalogs/'
+        }
+
+    def retrieve_file_list(self,url,catalog_type='halos'):
+        """ Get the full list of filenames available at the provided url.
+
+        """
+        from bs4 import BeautifulSoup
+        import requests
+
+        if catalog_type == 'halo' or 'halos': 
+            expected_filename_prefix = 'hlist_'
+        elif catalog_type == 'tree' or 'trees':
+            expected_filename_prefix = 'tree_'
+        else:
+            raise TypeError("Input catalog type must either be 'halos' or 'trees'")
+
+        soup = BeautifulSoup(requests.get(url).text)
+        file_list = []
+
+        for a in soup.find_all('a'):
+            link = a['href']
+            if link[0:len(expected_filename_prefix)]==expected_filename_prefix: 
+                file_list.append(a['href'])
+
+        return file_list
+
+
+
+
+
+
+###################################################################################################
 
 class particles(object):
     """ Container class for the simulation particles.    
@@ -117,6 +214,28 @@ class particles(object):
 
         self.particle_data = self.get_particles(manual_dirname,ask_permission)
 
+    def retrieve_particles(self,ask_permission=False):
+        """ Method to load simulation particle data into memory. 
+
+        The behavior of this method is governed by the astropy utility get_readable_fileobj. 
+        If the particle dataset is already present in the astropy cache directory, 
+        get_readable_fileobj will detect it and the retrieve_particles method 
+        will use astropy.io.fits to load the particles into memory. 
+        If the catalog is not there, 
+        get_readable_fileobj will download it from www.astro.yale.edu/aphearin, 
+        and then load it into memory, again using astropy.io.fits.
+
+        """
+
+        with get_readable_fileobj(url_string,cache=True) as f: 
+            fits_object = fits.HDUList.fromfile(f)
+            particle_catalog = fits_object[1].data
+            return particle_catalog
+
+
+
+        pass
+
 
     def get_particles(self,manual_dirname=None,ask_permission=False):
         """ Method to load simulation particle data into memory. 
@@ -139,7 +258,7 @@ class particles(object):
             self.simulation_name,self.scale_factor,self.num_ptcl_string)
 
         if os.path.isfile(cache_dir+self.filename)==True:
-            hdulist = astropy.io.fits.open(cache_dir+self.filename)
+            hdulist = fits.open(cache_dir+self.filename)
             particle_data = Table(hdulist[1].data)
 #            halos = Table(pyfits.getdata(cache_dir+self.filename,0))
         else:
@@ -156,7 +275,7 @@ class particles(object):
                 output = open(cache_dir+self.filename,'wb')
                 output.write(fileobj.read())
                 output.close()
-                hdulist = astropy.io.fits.open(cache_dir+self.filename)
+                hdulist = fits.open(cache_dir+self.filename)
                 particle_data = Table(hdulist[1].data)
                 #halos = Table(pyfits.getdata(cache_dir+self.filename,0))
             else:
@@ -166,5 +285,21 @@ class particles(object):
         return particle_data
 
 ###################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
