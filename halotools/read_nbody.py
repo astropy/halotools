2b5c50fda1a112b8df2c80db117f76c757466a82
Andrew Hearin <andrew.hearin@yale.edu>
2014-10-14 16:18:42 -0400

    Removed obsolete simulation object from read_nbody

--- halotools/read_nbody.py
+++ halotools/read_nbody.py
@@ -4,7 +4,7 @@ Methods and classes to load halo and particle catalogs into memory.
 
 """
 
-__all__=['simulation','particles']
+__all__=['simulation','processed_snapshot','Catalog_Manager']
 
 #from __future__ import (absolute_import, division, print_function,
 #                        unicode_literals)
@@ -492,89 +492,4 @@ class Catalog_Manager(object):
 
 
 ###################################################################################################
-
-
-class simulation(object):
-    """ Container class for the simulation being populated with mock galaxies.    
-    
-    """
-    
-    def __init__(self,simulation_name=defaults.default_simulation_name, 
-        scale_factor=defaults.default_scale_factor, 
-        halo_finder=defaults.default_halo_finder, 
-        use_subhalos=False,manual_dirname=None,ask_permission=False):
-
-
-        self.simulation_name = simulation_name
-        if simulation_name=='bolshoi':
-            self.Lbox = 250.0
-        else:
-            self.Lbox = None
-        
-        self.scale_factor = scale_factor
-        self.halo_finder = halo_finder
-        self.use_subhalos = use_subhalos
-
-        self.halos = self.get_catalog(manual_dirname,ask_permission)
-
-
-    def get_catalog(self,manual_dirname=None,ask_permission=False):
-        """ Method to load halo catalogs into memory. 
-
-        If the halo catalog is already present in the cache directory, 
-        simply use astropy.io.fits to load it. If the catalog is not there, 
-        download it from www.astro.yale.edu/aphearin, and then load it into memory.
-
-        """
-
-        download_yes_or_no = None
-
-        configobj = Config()
-        if manual_dirname != None:
-            warnings.warn("using hard-coded directory name to load simulation")
-            cache_dir = manual_dirname
-        else:
-            cache_dir = configobj.getCatalogDir()
-
-        #print("cache directory = "+cache_dir)
-
-        self.filename = configobj.getSimulationFilename(
-            self.simulation_name,self.scale_factor,self.halo_finder,self.use_subhalos)
-
-        if os.path.isfile(cache_dir+self.filename)==True:
-            hdulist = fits.open(cache_dir+self.filename)
-            halos = Table(hdulist[1].data)
-#            halos = Table(pyfits.getdata(cache_dir+self.filename,0))
-        else:
-            if ask_permission==True:
-                warnings.warn("\n Host halo catalog not found in cache directory")
-                download_yes_or_no = raw_input(" \n Enter yes to download, "
-                    "any other key to exit:\n (File size is ~200Mb) \n\n ")
-
-            if (download_yes_or_no=='y') or (download_yes_or_no=='yes') or (ask_permission==False):
-                print("\n...downloading halo catalog from www.astro.yale.edu/aphearin")
-                fileobj = urllib2.urlopen(configobj.hearin_url+self.filename)
-
-                output = open(cache_dir+self.filename,'wb')
-                output.write(fileobj.read())
-                output.close()
-                hdulist = fits.open(cache_dir+self.filename)
-                halos = Table(hdulist[1].data)
-                #halos = Table(pyfits.getdata(cache_dir+self.filename,0))
-            else:
-                warnings.warn("\n ...Exiting halotools \n")
-                os._exit(1)
-
-        return halos
-
-
-
-
-
-
-
-
-
-
-
 
