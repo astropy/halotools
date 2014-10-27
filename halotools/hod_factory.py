2119207c6e43b214dfe14cb4cd44f2357ccb4539
Andrew Hearin <andrew.hearin@yale.edu>
2014-10-26 21:03:11 -0400

    Simplified HOD_factory by throwing out unnecessary features

--- halotools/hod_factory.py
+++ halotools/hod_factory.py
@@ -16,8 +16,7 @@ class HOD_Model(object):
 		self.component_model_dict = component_model_dict
 		self.gal_types = component_model_dict.keys()
 
-
-	def mean_occupation(self,gal_type,**kwargs):
+	def mean_occupation(self,gal_type,*args):
 		""" Method supplying the mean abundance of gal_type galaxies. 
 		The behavior of this method is inherited from one of the component models. 
 		"""
@@ -31,61 +30,20 @@ class HOD_Model(object):
 				" mean_occupation in the provided component model")
 
 		# For galaxies of type gal_type, the behavior of this method 
-		# will be set by the occupation_model object 
-		# stored in the dictionary of component models passed to the constructor
+		# will be set by the inherited occupation_model object 
 		occupation_model = self.component_model_dict[gal_type]['occupation_model']
 
-		# This method has two options for the set of supported inputs.
-		# First, you can simply pass a 'halos' object, in which case 
-		# there should be no other arguments besides gal_type. In this case, 
-		# the following lines will identify whether there is a secondary halo property 
-		# being used, and call the appropriate method of occupation_model.
-
-		if 'halos' in kwargs.keys():
-
-			# Forbid passing both 'halos' object and arrays of halo properties
-			if primary_haloprop in kwargs.keys():
-				raise IOError("Pass either halos object, or ndarrays of halo properties, "
-					"to component occupation model. Passing both is ambiguous, and not permitted.")
-
-			# Get the primary halo property array, and the secondary array, if using.
-			try:
-				primary_haloprop = halos[occupation_model.primary_haloprop_key]
-			except KeyError:
-				print("primary_haloprop_key not found in "
-					"input halos object")
-
-			# Check to see if we are using an additional halo property in this model
-			if hasattr(occupation_model,'secondary_haloprop_key'):
-				# If so, pass both primary and secondary arrays to occupation_model
-				try:
-					secondary_haloprop = halos[occupation_model.secondary_haloprop_key]
-					output_occupation = (
-						occupation_model.mean_occupation(
-							primary_haloprop,secondary_haloprop))
-				except KeyError:
-					print("secondary_haloprop_key not found in input halos object")
-				# If not, just pass the primary halo property to occupation_model
-			else:
-				output_occupation = occupation_model.mean_occupation(primary_haloprop)
-
-		# if 'halos' object has not been passed to this method, 
-		# directly use the passed ndarrays
+		if len(args)==1:
+			primary_haloprop = args[0]
+			output_occupation = occupation_model.mean_occupation(primary_haloprop)
+		elif len(args)==2:
+			primary_haloprop = args[0]
+			secondary_haloprop = args[1]
+			output_occupation = occupation_model.mean_occupation(
+				primary_haloprop,secondary_haloprop)
 		else:
-			try:
-				primary_haloprop = kwargs['primary_haloprop']
-			except KeyError:
-				print("If not passing a halos object to mean_occupation, "
-				" you must pass a primary_haloprop argument containing a numpy array.")
-
-			if 'secondary_haloprop' in kwargs.keys():
-				secondary_haloprop = kwargs['secondary_haloprop']
-				output_occupation = (
-					occupation_model.mean_occupation(
-						primary_haloprop,secondary_haloprop))
-			else:
-				output_occupation = occupation_model.mean_occupation(primary_haloprop)
-
+			raise TypeError("Only one or two halo property inputs are supported by "
+				"mean_occupation method")				
 
 		return output_occupation
 
