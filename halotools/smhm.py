5f6dbc062af722b3e22b96193ce436c45383a74b
Andrew Hearin <andrew.hearin@yale.edu>
2014-10-27 08:41:58 -0400

    Moved cf_yao into extern/ directory.

--- halotools/smhm.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-
-This module contains the classes and methods used to model 
-the connection between (sub)halos and the stellar mass and/or luminosity 
-of the galaxies at their centers. 
-
-"""
