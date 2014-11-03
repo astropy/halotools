2fc8d9f9db51d6be66285693b9f74f95067143ef
Andrew Hearin <andrew.hearin@yale.edu>
2014-11-03 11:15:46 -0500

    Developing container class for profile model

--- halotools/profile_components.py
+++ halotools/profile_components.py
@@ -8,7 +8,32 @@ by composing the behavior of the components.
 
 """
 
-#class NFW_profile(object):
+from abc import ABCMeta, abstractmethod, abstractproperty
+import numpy as np
+
+@six.add_metaclass(ABCMeta)
+class Halo_Profile_Model(object):
+	""" Container class for any halo profile.
+	"""
+
+	def __init__(self):
+		pass
+
+	def profile(self):
+		pass
+
+	def cumulative_profile(self):
+		pass
+
+	def inverse_cumulative_profile(self):
+		pass
+
+
+
+
+
+
+
 
 
 
