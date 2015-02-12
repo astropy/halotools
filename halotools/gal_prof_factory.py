991e13f6b24d086a161cd137517bbaf49ea4a871
Andrew Hearin <andrew.hearin@yale.edu>
2015-02-12 11:18:52 -0500

    Working on init constructor of GalProfModel

--- halotools/gal_prof_factory.py
+++ halotools/gal_prof_factory.py
@@ -6,20 +6,28 @@ galaxy profiles from a set of components.
 
 """
 
-from astropy.extern import six
-from abc import ABCMeta, abstractmethod, abstractproperty
-
 import numpy as np
 from scipy.interpolate import UnivariateSpline as spline
 
+import defaults
 from utils.array_utils import array_like_length as aph_len
 import occupation_helpers as occuhelp 
-import defaults
-
 from functools import partial
+import halo_prof_components as hpc
+import gal_prof_components as gpc
 
-class GalProf(object):
 
-	def __init__(self):
-		pass
+class GalProfModel(object):
+
+	def __init__(self, gal_type, halo_prof_model,
+		spatial_bias_model = None,
+		):
+
+		self.gal_type = gal_type
+		self.halo_prof_model = halo_prof_model
+		self.cosmology = self.halo_prof_model.cosmology
+		self.redshift = self.halo_prof_model.redshift
+
+
+
 
