802d65e1da5712d196b8a9e495fdcb1712cbc160
Kevin Tan <kevin.tan@yale.edu>
2015-02-17 17:49:06 -0500

    deleted extraneous files

--- halotools/mock_observables/sinha_pair_wrapper/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize([Extension("wrapper", ["wrapper.pyx"], libraries =["countpairs"], library_dirs=["../../../cextern/sinha_pairs"])])
-)
