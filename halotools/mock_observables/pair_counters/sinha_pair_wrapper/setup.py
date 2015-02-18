44dee45bb81ab42f9be1d234adfaf225d0ad990d
Kevin Tan <kevin.tan@yale.edu>
2015-02-18 12:45:01 -0500

    change path to cextern in setup.py

--- halotools/mock_observables/pair_counters/sinha_pair_wrapper/setup.py
+++ halotools/mock_observables/pair_counters/sinha_pair_wrapper/setup.py
@@ -3,5 +3,5 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize([Extension("sinha_pair_counter_wrapper", ["sinha_pair_counter_wrapper.pyx"], libraries =["countpairs"], library_dirs=["../../../../cextern/sinha_paircounter"])])
+    ext_modules = cythonize([Extension("sinha_pair_counter_wrapper", ["sinha_pair_counter_wrapper.pyx"], libraries =["countpairs"], library_dirs=["../../../../cextern/sinha_pair_counter"])])
 )
