6da2a254904b3222a9eb9db087194bec7ab2787b
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-02-18 13:14:24 -0500

    removed manodeeps pair counter and wrapper from the repo until we can git it into tip-top shape.

--- cextern/sinha_pair_counter/io.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-
-unsigned int read_positions(const char *filename, const char *format, void **xpos, void **ypos, void **zpos, const size_t size);
