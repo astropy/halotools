c1e9499cf51767a1eae31e9ce774648ffa7a1f2e
Andrew Hearin <andrew.hearin@yale.edu>
2015-03-11 13:27:26 -0400

    Added mock test that fails

---
+++ halotools/empirical_models/tests/test_mock_factory.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import numpy as np 
+from .. import preloaded_hod_models
+from .. import hod_factory
+from .. import mock_factory
+from ...sim_manager import read_nbody
+
+def test_kravtsov04_mock():
+	default_sim = read_nbody.processed_snapshot()
+	kravtsov_model_dict = preloaded_hod_models.Kravtsov04()
+	kravtsov_model = hod_factory.HodModel(kravtsov_model_dict)
+
+	mock = mock_factory.HodMockFactory(
+		default_sim, kravtsov_model, 
+		populate=False)
