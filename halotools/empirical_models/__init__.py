c63a9fdc7bbc4a91102daaa8c3599a635868416f
Andrew Hearin <andrew.hearin@yale.edu>
2015-03-14 17:45:53 -0400

    Included hod components in the empirical_models package

--- halotools/empirical_models/__init__.py
+++ halotools/empirical_models/__init__.py
@@ -6,3 +6,4 @@ from __future__ import (absolute_import, division, print_function,
 from .mock_factory import *
 from .hod_factory import *
 from .preloaded_hod_blueprints import *
+from .hod_components import *
