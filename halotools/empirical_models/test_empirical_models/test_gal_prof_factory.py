bea99c7f77f3fcd9c09b658e1303d35fc9ad7b51
Andrew Hearin <andrew.hearin@yale.edu>
2015-03-19 09:14:44 -0400

    Starting to build tests for the galaxy profile factory

--- halotools/empirical_models/test_empirical_models/test_gal_prof_factory.py
+++ halotools/empirical_models/test_empirical_models/test_gal_prof_factory.py
@@ -2,10 +2,24 @@
 from .. import halo_prof_components as hpc
 from .. import gal_prof_components as gpc
 from .. import gal_prof_factory as gpf
+from astropy import cosmology
 
-def test_gal_prof_factory_instance():
-	nfw = hpc.NFWProfile()
-	gal_type = 'sats'
+def test_unbiased_trivial():
+	trivial_prof = hpc.TrivialProfile()
+	gal_type = 'cens'
 
-	gal_prof_model = gpf.GalProfModel(gal_type, nfw)
-	assert gal_prof_model.gal_type == gal_type	
+	cen_prof = gpf.GalProfModel(gal_type, trivial_prof)
+	assert cen_prof.gal_type == gal_type
+
+	assert isinstance(cen_prof.halo_prof_model, hpc.TrivialProfile)
+
+	assert hasattr(cen_prof, 'cosmology')
+	assert isinstance(cen_prof.cosmology, cosmology.FlatLambdaCDM)
+	assert hasattr(cen_prof, 'redshift')
+	assert 0 <= cen_prof.redshift <= 100
+
+	assert hasattr(cen_prof,'haloprop_key_dict')
+	assert cen_prof.haloprop_key_dict == {}
+
+	assert hasattr(cen_prof,'spatial_bias_model')
+
