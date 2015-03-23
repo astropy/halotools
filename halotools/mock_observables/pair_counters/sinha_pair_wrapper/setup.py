159e282ea9e8b7ee82eb946de9ec9affa1052340
Kevin Tan <kevin.tan@yale.edu>
2015-03-22 21:33:45 -0400

    correct setup.py file for sinha pair counter

--- halotools/mock_observables/pair_counters/sinha_pair_wrapper/setup.py
+++ halotools/mock_observables/pair_counters/sinha_pair_wrapper/setup.py
@@ -2,6 +2,17 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+
+
+
+extensions = [
+    Extension("sinha_pair_counter_wrapper", 
+    	["sinha_pair_counter_wrapper.pyx"], 
+    	libraries =["countpairs"],
+    	library_dirs=["/Users/kt/halotools/cextern/sinha_pair_counter"],
+    )
+]
 setup(
-    ext_modules = cythonize([Extension("sinha_pair_counter_wrapper", ["sinha_pair_counter_wrapper.pyx"], libraries =["countpairs"], library_dirs=["../../../../cextern/sinha_pair_counter"])])
+    name = "sinha_pair_setup",
+    ext_modules = cythonize(extensions),
 )
