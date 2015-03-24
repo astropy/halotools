efa41de8c217c7d085e1f44ee65601ff8d0cf817
Kevin Tan <kevin.tan@yale.edu>
2015-03-24 15:28:31 -0400

    working wrapper with libcountpairs.so in wrapper directory

--- halotools/mock_observables/pair_counters/sinha_pair_wrapper/setup.py
+++ halotools/mock_observables/pair_counters/sinha_pair_wrapper/setup.py
@@ -9,7 +9,7 @@ extensions = [
     Extension("sinha_pair_counter_wrapper", 
     	["sinha_pair_counter_wrapper.pyx"], 
     	libraries =["countpairs"],
-    	library_dirs=["/Users/kt/halotools/cextern/sinha_pair_counter"],
+    	library_dirs=["../../../../cextern/sinha_pair_counter/"],
     )
 ]
 setup(
