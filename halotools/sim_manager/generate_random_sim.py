e813a6b8af5fdb6fe53892d45a314c4904b7e06f
Andrew Hearin <andrew.hearin@yale.edu>
2015-03-27 10:30:28 -0400

    Finished random halos method of FakeSim

--- halotools/sim_manager/generate_random_sim.py
+++ halotools/sim_manager/generate_random_sim.py
@@ -20,21 +20,20 @@ class FakeSim(object):
 		self.particle_mass = 1.e8
 		self.simulation_name = 'fake'
 
-		self.seed = 43
+		self.seed = seed
 
 		self.num_massbins = num_massbins
 		self.num_halos_per_massbin = num_halos_per_massbin
 		self.num_halos = self.num_massbins*self.num_halos_per_massbin
 		self.num_ptcl = num_ptcl
 
-
-
 	@property 
 	def halos(self):
 
+		np.random.seed(self.seed)
+
 		haloid = np.arange(1e9, 1e9+self.num_halos)
 
-		np.random.seed(self.seed)
 		randomizer = np.random.random(self.num_halos)
 		subhalo_fraction = 0.1
 		upid = np.where(randomizer > subhalo_fraction, -1, 1)
@@ -43,17 +42,14 @@ class FakeSim(object):
 		mvir = np.repeat(massbins, self.num_halos_per_massbin, axis=0)
 		logrvirbins = (np.log10(massbins) - 15)/3.
 		rvir = np.repeat(10.**logrvirbins, self.num_halos_per_massbin, axis=0)
-
-		conc = np.random.uniform(4, 15, self.num_halos)
-		last_mm = np.random.uniform(0, 1, self.num_halos)
-
 		logvmaxbins = -4.25 + 0.5*(np.log10(massbins) - logrvirbins)
 		vmax = np.repeat(10.**logvmaxbins, self.num_halos_per_massbin, axis=0)
 
-		np.random.seed(self.seed)
+		conc = np.random.uniform(4, 15, self.num_halos)
+		zhalf = np.random.uniform(0, 10, self.num_halos)
+
 		pos = np.random.uniform(
 			0, self.Lbox, self.num_halos*3).reshape(self.num_halos, 3)
-		np.random.seed(self.seed)
 		vel = np.random.uniform(
 			-500, 500, self.num_halos*3).reshape(self.num_halos, 3)
 
@@ -63,7 +59,7 @@ class FakeSim(object):
 			'mvir': mvir, 
 			'rvir': rvir, 
 			'conc': conc, 
-			'last_mm': last_mm, 
+			'zhalf': zhalf, 
 			'vmax': vmax, 
 			'pos': pos, 
 			'vel': vel
