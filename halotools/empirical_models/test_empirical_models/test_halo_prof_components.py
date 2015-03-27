53afaca27236afd84277af927dcd3aa80cabcac5
Andrew Hearin <andrew.hearin@yale.edu>
2015-03-27 12:03:24 -0400

    Improvements to test_halo_prof_components

--- halotools/empirical_models/test_empirical_models/test_halo_prof_components.py
+++ halotools/empirical_models/test_empirical_models/test_halo_prof_components.py
@@ -5,6 +5,40 @@ import numpy as np
 
 __all__ = ['test_TrivialProfile','test_NFWProfile']
 
+def test_HaloProfileModel():
+	prof_model_list = hpc.__all__
+	parent_class = hpc.HaloProfileModel
+
+	# First create a list of all sub-classes to test
+	component_models_to_test = []
+	for clname in prof_model_list:
+		cl = getattr(hpc, clname)
+
+		if (issubclass(cl, parent_class)) & (cl != parent_class):
+			component_models_to_test.append(cl)
+
+	# Now we will test that all sub-classes inherit the correct behavior
+	for model_class in component_models_to_test:
+		model_instance = model_class()
+
+		assert hasattr(model_instance, 'cosmology')
+		assert isinstance(model_instance.cosmology, cosmology.FlatLambdaCDM)
+
+		assert hasattr(model_instance, 'cumu_inv_func_table')
+		assert type(model_instance.cumu_inv_func_table) == np.ndarray
+
+		assert hasattr(model_instance, 'cumu_inv_param_table_dict')
+		assert type(model_instance.cumu_inv_param_table_dict) == dict
+
+		assert hasattr(model_instance, 'build_inv_cumu_lookup_table')
+		model_instance.build_inv_cumu_lookup_table()
+
+
+
+
+
+
+
 def test_TrivialProfile():
 	""" Simple tests of `~halotools.empirical_models.halo_prof_components.TrivialProfile`. 
 
@@ -24,11 +58,7 @@ def test_TrivialProfile():
 		* ``haloprop_key_dict``
 	"""
 	profile_model = hpc.TrivialProfile()
-
-	assert hasattr(profile_model, 'cosmology')
-	assert isinstance(profile_model.cosmology, cosmology.FlatLambdaCDM)
-
-	assert type(profile_model.cumu_inv_func_table) == np.ndarray
+	
 	assert len(profile_model.cumu_inv_func_table) == 0
 
 	assert profile_model.cumu_inv_param_table_dict == {}
