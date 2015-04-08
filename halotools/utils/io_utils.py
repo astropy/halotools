f67fa5b45f9e13e8ca4f7227d5962bd61141d27f
Andrew Hearin <andrew.hearin@yale.edu>
2015-04-08 14:28:53 -0400

    Added io util routine to download a file from the web to a desired location and print out a progress bar

--- halotools/utils/io_utils.py
+++ halotools/utils/io_utils.py
@@ -1,16 +1,35 @@
+# -*- coding: utf-8 -*-
+import sys, urllib
+
+__all__ = ['file_len', 'get_file_from_url']
 
 def file_len(fname):
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
-    return i + 1
+    return i + 1    
+    
 
+def get_file_from_url(url, fname):
+    """ Function to download a file from the web to a specific location, 
+    and print a progress bar along the way. 
 
+    Parameters 
+    ----------
+    url : string 
+        web location of desired file, e.g., 
+        ``http://www.some.website.com/somefile.txt``. 
 
+    fname : string 
+        Location and filename to store the downloaded file, e.g., 
+        ``/Users/username/dirname/possibly_new_filename.txt``
+    """
 
+    def reporthook(a,b,c): 
+        print "% 3.1f%% of %d bytes\r" % (min(100, float(a * b) / c * 100), c),
+        sys.stdout.flush()
 
-
-
+    urllib.urlretrieve(url, fname, reporthook)
 
 
 
