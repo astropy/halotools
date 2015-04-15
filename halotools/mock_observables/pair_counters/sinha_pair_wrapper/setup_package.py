752e3c6398230a855ed2d646520000c0ab2c0fe3
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-04-15 13:11:47 -0400

    added setup package script

---
+++ halotools/mock_observables/pair_counters/sinha_pair_wrapper/setup_package.py
@@ -0,0 +1,23 @@
+from distutils.extension import Extension
+import os
+
+PATH_TO_WRAPPER = os.path.relpath(os.path.dirname(__file__))
+SOURCES = ["sinha_pairs.pyx", "source/countpairs.c", "source/gridlink.c", "source/utils.c"]
+
+def get_extensions():
+    
+    name = "halotools.mock_observables.pair_counters.sinha_pairs"
+    sources = [os.path.join(PATH_TO_WRAPPER, srcfn) for srcfn in SOURCES]
+    include_dirs = [PATH_TO_WRAPPER+'include/']
+    libraries = []
+    extra_compile_args = ["-DPERIODIC","-DDOUBLE_PREC"]
+
+    extensions= [Extension(name=name, 
+                 sources=sources, 
+                 include_dirs=include_dirs, 
+                 libraries=libraries,
+                 extra_compile_args=extra_compile_args,
+                 extra_link_args=extra_link_args)
+                ]
+                
+    return extensions
