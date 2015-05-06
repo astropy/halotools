b0fe7a32f86b9ca74e2051ce5dc61528174d37f2
Andrew Hearin <andrew.hearin@yale.edu>
2015-05-06 15:59:10 -0400

    Renamed two modules: configuration --> cache_config, and sim_specs --> supported_sims

--- halotools/sim_manager/__init__.py
+++ halotools/sim_manager/__init__.py
@@ -4,4 +4,4 @@ from __future__ import (absolute_import, division, print_function,
 
 from .read_nbody import *
 from .generate_random_sim import *
-from .configuration import *
+from .cache_config import *
--- halotools/sim_manager/cache_config.py
+++ halotools/sim_manager/cache_config.py
@@ -4,7 +4,7 @@ various files used throughout the halotools package.
 """
 
 __all__ = (
-    ['get_halotools_cache_dir','get_catalogs_dir','infer_simulation_from_fname']
+    ['get_halotools_cache_dir','get_catalogs_dir']
     )
 
 
@@ -127,24 +127,5 @@ def get_catalogs_dir(catalog_type, **kwargs):
                 halo_finder_dirname = os.path.join(simname_dirname, kwargs['halo_finder'])
                 defensively_create_dir(halo_finder_dirname)
                 return halo_finder_dirname
-
-
-def infer_simulation_from_fname(fname):
-
-    halocat_path = os.path.dirname(fname)
-    halotools_cache_dir = get_halotools_cache_dir()
-    subdir_list = [x[0] for x in os.walk(halotools_cache_dir)]
-    if halocat_path not in subdir_list:
-        print("Input fname is not a subdirectory of "
-            "the Halotools cache: \n cannot infer simulations from external sources")
-        return None
-    else:
-        halo_finder = os.path.basename(halocat_path)
-        simulation_path = os.path.abspath(os.path.join(halocat_path, os.pardir))
-        simname = os.path.basename(simulation_path)
-        return simname, halo_finder
-
-
-
 
 
