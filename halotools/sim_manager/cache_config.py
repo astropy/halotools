d39249c174f45c17649a9836d129b2a3b2debfda
Andrew Hearin <andrew.hearin@yale.edu>
2015-05-06 16:51:55 -0400

    Significant refactoring to cache_config, with intent to address Issue #47. Still largely untested

--- halotools/sim_manager/cache_config.py
+++ halotools/sim_manager/cache_config.py
@@ -13,6 +13,7 @@ from astropy.config.paths import get_cache_dir as get_astropy_cache_dir
 import warnings
 
 from . import sim_defaults
+from . import supported_sims
 
 def get_halotools_cache_dir():
     """ Find the path to the halotools cache directory. 
@@ -38,6 +39,71 @@ def get_halotools_cache_dir():
         raise IOError(msg.format(halotools_cache_dir))
 
     return halotools_cache_dir
+
+def defensively_create_subdir(dirname):
+    if not os.path.exists(dirname):
+        try:
+            os.mkdir(dirname)
+        except OSError as e:
+            if not os.path.exists(dirname):
+                raise IOError("OS Error during creation of path %s" % dirname)
+    elif not os.path.isdir(dirname):
+        msg = 'Pathname {0} is not a directory'
+        raise IOError(msg.format(dirname))
+
+def get_supported_simnames():
+    class_list = supported_sims.__all__
+    parent_class = supported_sims.SimulationSpecs
+
+    supported_simnames = []
+    for clname in class_list:
+        clobj = getattr(supported_sims, clname)
+        if (issubclass(clobj, parent_class)) & (clobj.__name__ != parent_class.__name__):
+            clinst = clobj()
+            supported_simnames.append(clinst.simname)
+
+    return list(set(supported_simnames))
+
+def get_supported_halo_finders(input_simname):
+    class_list = supported_sims.__all__
+    parent_class = supported_sims.HaloCat
+
+    supported_halo_finders = []
+    for clname in class_list:
+        clobj = getattr(supported_sims, clname)
+        if (issubclass(clobj, parent_class)) & (clobj.__name__ != parent_class.__name__):
+            clinst = clobj()
+            if clinst.simname == input_simname:
+                supported_halo_finders.append(clinst.halo_finder)
+
+    return list(set(supported_halo_finders))
+
+def cache_subdir_for_simulation(parentdir, simname):
+    simulation_cache = os.path.join(parentdir, simname)
+    if os.path.exists(simulation_cache):
+        return simulation_cache
+    else:
+        supported_sims = get_supported_simnames()
+        if simname in supported_sims:
+            return simulation_cache
+        else:
+            raise IOError("It is not permissible to create a subdirectory of "
+                "Halotools cache \nfor simulations which have no class defined in "
+                "the halotools/sim_manager/supported_sims module. \n")
+
+def cache_subdir_for_halo_finder(parentdir, simname, halo_finder):
+    halo_finder_cache = os.path.join(parentdir, halo_finder)
+    if os.path.exists(halo_finder_cache):
+        return halo_finder_cache
+    else:
+        supported_halo_finders = get_supported_halo_finders(simname)
+        if halo_finder in supported_halo_finders:
+            return halo_finder_cache
+        else:
+            raise IOError("It is not permissible to create a subdirectory of "
+                "Halotools cache \nfor a combination of "
+                "simulation + halo-finder which has no corresponding class defined in "
+                "the halotools/sim_manager/supported_sims module. \n")
 
 
 def get_catalogs_dir(catalog_type, **kwargs):
@@ -87,20 +153,7 @@ def get_catalogs_dir(catalog_type, **kwargs):
         subdir_name = 'raw_halo_catalogs'
         default_cache_dir = sim_defaults.raw_halocat_cache_dir
     else:
-        raise TypeError("Input catalog_type must be either 'subhalos' or 'particles'")
-
-
-    def defensively_create_dir(dirname):
-        if not os.path.exists(dirname):
-            try:
-                os.mkdir(dirname)
-            except OSError as e:
-                if not os.path.exists(dirname):
-                    raise IOError("OS Error during creation of path %s" % dirname)
-        elif not os.path.isdir(dirname):
-            msg = 'Pathname {0} is not a directory'
-            raise IOError(msg.format(dirname))
-
+        raise TypeError("Input catalog_type must be either 'raw_halos', 'halos', or 'particles'")
 
     # Check to see whether we are using the package default or user-provided cache directory
     if default_cache_dir != 'pkg_default':
@@ -113,19 +166,36 @@ def get_catalogs_dir(catalog_type, **kwargs):
         else:
             return default_cache_dir
     else:
-        # Use the cache directory provided by the package
+        # Create the directory .astropy/cache/halotools/subdir_name
         catalog_type_dirname = os.path.join(get_halotools_cache_dir(), subdir_name)
-        defensively_create_dir(catalog_type_dirname)
+        defensively_create_subdir(catalog_type_dirname)
+
+        # Now check to see if there exists a cache subdirectory for simname
         if 'simname' not in kwargs.keys():
             return catalog_type_dirname
         else:
-            simname_dirname = os.path.join(catalog_type_dirname, kwargs['simname'])
-            defensively_create_dir(simname_dirname)
+            simname_dirname = cache_subdir_for_simulation(catalog_type_dirname, kwargs['simname'])
+            defensively_create_subdir(simname_dirname)
+
             if 'halo_finder' not in kwargs.keys():
                 return simname_dirname
             else:
-                halo_finder_dirname = os.path.join(simname_dirname, kwargs['halo_finder'])
-                defensively_create_dir(halo_finder_dirname)
+                halo_finder_dirname = (
+                    cache_subdir_for_halo_finder(
+                        simname_dirname, kwargs['simname'], kwargs['halo_finder'])
+                    )
+                defensively_create_subdir(halo_finder_dirname)
                 return halo_finder_dirname
+
+
+
+
+
+
+
+
+
+
+
 
 
--- halotools/sim_manager/tests/test_cache_config.py
+++ halotools/sim_manager/tests/test_cache_config.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import numpy as np
+import os
 
-from .. import read_nbody
+from .. import cache_config
 
 __all__ = ['test_cache_config']
 
 def test_cache_config():
-	pass
+	astropy_cache = cache_config.get_astropy_cache_dir()
+	halotools_cache = cache_config.get_halotools_cache_dir()
+	assert os.path.join(astropy_cache, 'halotools') == halotools_cache
+
+def test_catalogs_config():
+	halotools_cache = cache_config.get_halotools_cache_dir()
+		
+
+
