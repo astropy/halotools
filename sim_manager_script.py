f848a9e45644ad7615227da5e7a3dd4ba2a183bb
Andrew Hearin <andrew.hearin@yale.edu>
2015-05-07 10:58:19 -0400

    Temporarily adding testing script to root directory due to frustration with py.test

---
+++ sim_manager_script.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import numpy as np
+import os
+
+from halotools import sim_manager, supported_sims
+
+catman = sim_manager.CatalogManager()
+
+########################################################
+####### CHECK THAT RAW HALO CATALOGS GO TO CACHE #######
+
+
+
+
+
+
+
+
+########################################################
+
+
+########################################################
+
+
+########################################################
+
+
+
+
