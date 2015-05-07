9263579f3e4578be93f7d8a3be2d46d5d9e2a6bd
Andrew Hearin <andrew.hearin@yale.edu>
2015-05-07 12:23:32 -0400

    Added method all_halocats_in_cache to catman

--- sim_manager_script.py
+++ sim_manager_script.py
@@ -8,8 +8,8 @@ reload(sim_manager)
 
 catman = sim_manager.CatalogManager()
 
-########################################################
-####### CHECK THAT RAW HALO CATALOGS GO TO CACHE #######
+#############################################################################
+####### CHECK THAT WE CAN LOCATE AVAILABLE AND CLOSEST HALO CATALOGS #######
 
 supported_halocats = catman.available_halocats
 simname, halo_finder = supported_halocats[0]
@@ -19,32 +19,32 @@ available_cats_in_cache = catman.available_snapshots('cache', 'raw_halos', simna
 location = 'web'
 catalog_type = 'raw_halos'
 desired_redshift = 1
-closest_cat = catman.closest_halocat(location, catalog_type, simname, halo_finder, desired_redshift)
-print("\n Closest matching catalog on the web = \n%s\n " % closest_cat[0])
+closest_cat_on_web = catman.closest_halocat(location, catalog_type, simname, halo_finder, desired_redshift)
+print("\n Closest matching catalog on the web = \n%s\n " % closest_cat_on_web[0])
 location = 'cache'
-closest_cat = catman.closest_halocat(location, catalog_type, simname, halo_finder, desired_redshift)
-print("\n Closest matching catalog in cache = \n%s\n " % closest_cat[0])
+closest_cat_in_cache = catman.closest_halocat(location, catalog_type, simname, halo_finder, desired_redshift)
+print("\n Closest matching catalog in cache = \n%s\n " % closest_cat_in_cache[0])
 
-########################################################
+print("\n")
+all_cached_files = catman.all_halocats_in_cache('halos')
+for f in all_cached_files:
+	print(f)
+#############################################################################
+####### CHECK THAT RAW HALO CATALOG DOWNLOADS GO TO CACHE #######
 
 
-########################################################
+
+#############################################################################
 ####### CHECK THE HALOCAT_OBJ PROPERTIES #######
 halocat_obj = sim_manager.read_nbody.get_halocat_obj(simname, halo_finder)
 webloc = halocat_obj.raw_halocat_web_location
 
 
-########################################################
-
-########################################################
-
-
-########################################################
-
-########################################################
-
-
-########################################################
-
-
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
 
