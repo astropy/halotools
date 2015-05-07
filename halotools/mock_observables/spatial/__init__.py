5462d4b740e47ebbe33777f2b748eecdea8190ac
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-05-07 12:26:29 -0700

    cleaned up init file

--- halotools/mock_observables/spatial/__init__.py
+++ halotools/mock_observables/spatial/__init__.py
@@ -1,5 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 """
 spatial is a sub-package designed to calculate spatial quantities. 
 """
+
+__all__=['geometry', 'distances', 'kdtrees']
+
+from .geometry import *
+from .distances import *
