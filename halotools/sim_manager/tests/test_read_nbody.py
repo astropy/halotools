f9b9e35b05257c6840da57ac32ec62a79b0acbcd
Andrew Hearin <andrew.hearin@yale.edu>
2015-05-07 10:23:57 -0400

    Starting to implement pytest into test suite

--- halotools/sim_manager/tests/test_read_nbody.py
+++ halotools/sim_manager/tests/test_read_nbody.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 import numpy as np
+from astropy.tests.helper import pytest
 
 from .. import read_nbody
 
-__all__ = ['test_catalog_manager']
+__all__ = ['test_catalog_manager', 'TestDummyClass']
 
 def test_catalog_manager():
 	catman = read_nbody.CatalogManager()
 
 
+class TestDummyClass(object):
+
+	@pytest.mark.marf
+	def test_dummy(self):
+		assert 5==5
 
