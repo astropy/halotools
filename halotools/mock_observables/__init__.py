c5269d8e42e1c8a185fdd3c50a496427a4f22113
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-05-07 12:26:42 -0700

    cleaned up init file

--- halotools/mock_observables/__init__.py
+++ halotools/mock_observables/__init__.py
@@ -4,3 +4,7 @@ __all__=['observables','spatial','pair_counters','two_point_functions','make_red
 This sub-package contains code that computes various observational statistical quantities
 from a mock galaxy catalog.
 """
+
+from .two_point_functions import *
+from .make_redshift_survey import *
+from .observables import *
