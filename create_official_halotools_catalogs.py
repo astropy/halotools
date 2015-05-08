2e6ae133ab342a23d0c72d6e56a3c6bb34a79d42
Andrew Hearin <andrew.hearin@yale.edu>
2015-05-08 16:19:59 -0400

    Finished script that makes official versions of reduced binaries

--- create_official_halotools_catalogs.py
+++ create_official_halotools_catalogs.py
@@ -49,7 +49,7 @@ notes = {
 	' the default_halocat_cut method of RockstarReader' + 
 	' is to throw out all (sub)halos with Mpeak < 300 particles'
 	}
-output_fname_bolshoi_z2_rockstar = catman.store_processed_halocat(
+output_fname_bolshoi_z1_rockstar = catman.store_processed_halocat(
 	arr, reader, version_name, notes = notes, overwrite=True)
 ########################################################################
 ### Create Bolshoi Rockstar z=0.5 reduced binary
@@ -71,9 +71,78 @@ notes = {
 	' the default_halocat_cut method of RockstarReader' + 
 	' is to throw out all (sub)halos with Mpeak < 300 particles'
 	}
-output_fname_bolshoi_z2_rockstar = catman.store_processed_halocat(
+output_fname_bolshoi_z05_rockstar = catman.store_processed_halocat(
 	arr, reader, version_name, notes = notes, overwrite=True)
 ########################################################################
+### Create Bolshoi Rockstar z=0 reduced binary
+catalog_type = 'raw_halos'
+desired_redshift = 0
+simname = 'bolshoi'
+halo_finder = 'rockstar'
 
-
+disk_location = os.path.join(os.path.join('/Volumes/NbodyDisk1/raw_halos', simname), halo_finder)
+closest_fname, z = catman.closest_halocat(disk_location, catalog_type, simname, halo_finder, desired_redshift)
+arr, reader = (
+	catman.process_raw_halocat(closest_fname, simname, halo_finder)
+	)
+### Store the catalog
+version_name = 'halotools.official.version'
+notes = {
+	'cuts_description': 
+	'The only cut on the original catalog made by ' + 
+	' the default_halocat_cut method of RockstarReader' + 
+	' is to throw out all (sub)halos with Mpeak < 300 particles'
+	}
+output_fname_bolshoi_z0_rockstar = catman.store_processed_halocat(
+	arr, reader, version_name, notes = notes, overwrite=True)
 ########################################################################
+### Create Bolshoi BDM z=0 reduced binary
+catalog_type = 'raw_halos'
+desired_redshift = 0
+simname = 'bolshoi'
+halo_finder = 'bdm'
+
+disk_location = os.path.join(os.path.join('/Volumes/NbodyDisk1/raw_halos', simname), halo_finder)
+closest_fname, z = catman.closest_halocat(disk_location, catalog_type, simname, halo_finder, desired_redshift)
+arr, reader = (
+	catman.process_raw_halocat(closest_fname, simname, halo_finder)
+	)
+### Store the catalog
+version_name = 'halotools.official.version'
+notes = {
+	'cuts_description': 
+	'The only cut on the original catalog made by ' + 
+	' the default_halocat_cut method of RockstarReader' + 
+	' is to throw out all (sub)halos with Mpeak < 300 particles'
+	}
+output_fname_bolshoi_z0_bdm = catman.store_processed_halocat(
+	arr, reader, version_name, notes = notes, overwrite=True)
+########################################################################
+### Create Bolshoi-Pl Rockstar z=0 reduced binary
+catalog_type = 'raw_halos'
+desired_redshift = 0
+simname = 'bolshoipl'
+halo_finder = 'rockstar'
+
+disk_location = os.path.join(os.path.join('/Volumes/NbodyDisk1/raw_halos', simname), halo_finder)
+closest_fname, z = catman.closest_halocat(disk_location, catalog_type, simname, halo_finder, desired_redshift)
+arr, reader = (
+	catman.process_raw_halocat(closest_fname, simname, halo_finder)
+	)
+### Store the catalog
+version_name = 'halotools.official.version'
+notes = {
+	'cuts_description': 
+	'The only cut on the original catalog made by ' + 
+	' the default_halocat_cut method of RockstarReader' + 
+	' is to throw out all (sub)halos with Mpeak < 300 particles'
+	}
+output_fname_bolshoipl_z0_rockstar = catman.store_processed_halocat(
+	arr, reader, version_name, notes = notes, overwrite=True)
+
+
+
+
+
+
+
