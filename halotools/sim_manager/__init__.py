bb1d3b60170007fb92844299004227771711c0c3
Andrew Hearin <andrew.hearin@yale.edu>
2015-05-09 10:50:35 -0400

    __init__ modified to import supported_sims module

--- halotools/sim_manager/__init__.py
+++ halotools/sim_manager/__init__.py
@@ -5,3 +5,4 @@ from __future__ import (absolute_import, division, print_function,
 from .read_nbody import *
 from .generate_random_sim import *
 from .cache_config import *
+from .supported_sims import *
