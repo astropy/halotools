194a6adef1a6bb22d6167298d3a21fc1b8e0fe48
Andrew Hearin <andrew.hearin@yale.edu>
2015-05-12 18:33:47 -0400

    Deleted entire mock_observables sub-package

--- halotools/mock_observables/spatial/test_spatial/__init__.py
+++
@@ -1,7 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
-#from .test_distances import *
-#from .test_geometry import *
-#from .test_ckdtree import *
