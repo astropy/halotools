da45ff89e8b71059738040a49fb5a6c028e24d1a
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-06-03 17:30:43 -0400

    updated init files

--- halotools/mock_observables/pair_counters/test_pair_counters/__init__.py
+++ halotools/mock_observables/pair_counters/test_pair_counters/__init__.py
@@ -2,4 +2,4 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
                         
-from .tpcf import *
+from .test_grid_pairs import *
