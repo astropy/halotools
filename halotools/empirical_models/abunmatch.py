249d47d223df62965a293de483bce5606915afa9
Andrew Hearin <andrew.hearin@yale.edu>
2015-06-18 18:41:50 -0400

    Reorganizing to have an abunmatch module

---
+++ halotools/empirical_models/abunmatch.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Module containing classes used to perform abundance matching (SHAM)
+and conditional abundance matching (CAM). 
+
+"""
+import numpy as np
+
+from astropy.extern import six
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+from . import model_defaults
+from . import model_helpers as model_helpers
+from .smhm_components import PrimGalpropModel
+
+from ..utils.array_utils import array_like_length as custom_len
+from ..sim_manager import sim_defaults 
+
+from warnings import warn
+from functools import partial
+
+class AbunMatchSmHm(PrimGalpropModel):
+    """ Stellar-to-halo-mass relation based on traditional abundance matching. 
+    """
+
+    def __init__(self, galaxy_abundance_abcissa, galaxy_abundance_ordinates, 
+        scatter_level = 0.2, **kwargs):
+        """
+        Parameters 
+        ----------
+        galprop_key : string, optional keyword argument 
+            Name of the galaxy property being assigned. Default is ``stellar mass``, 
+            though another common case may be ``luminosity``. 
+
+        galaxy_abundance_ordinates : array_like
+            Length-Ng array storing the comoving number density of galaxies 
+            The value ``galaxy_abundance_ordinates[i]`` gives the comoving number density 
+            of galaxies evaluated at the galaxy property stored in ``galaxy_abundance_abcissa[i]``. 
+            The most common two cases are where ``galaxy_abundance_abcissa`` stores either 
+            stellar mass or luminosity, in which case ``galaxy_abundance_ordinates`` would 
+            simply be the stellar mass function or the luminosity function, respectively. 
+
+        galaxy_abundance_abcissa : array_like
+            Length-Ng array storing the property of the galaxies for which the 
+            abundance has been tabulated. 
+             The value ``galaxy_abundance_ordinates[i]`` gives the comoving number density 
+            of galaxies evaluated at the galaxy property stored in ``galaxy_abundance_abcissa[i]``. 
+            The most common two cases are where ``galaxy_abundance_abcissa`` stores either 
+            stellar mass or luminosity, in which case ``galaxy_abundance_ordinates`` would 
+            simply be the stellar mass function or the luminosity function, respectively. 
+
+        subhalo_abundance_ordinates : array_like, optional keyword argument 
+            Length-Nh array storing the comoving number density of subhalos.
+            The value ``subhalo_abundance_ordinates[i]`` gives the comoving number density 
+            of subhalos of property ``subhalo_abundance_abcissa[i]``. 
+            If keyword arguments ``subhalo_abundance_ordinates`` 
+            and ``subhalo_abundance_abcissa`` are not passed, 
+            then keyword arguments ``prim_haloprop_key`` and ``halos`` must be passed. 
+
+        subhalo_abundance_abcissa : array_like, optional keyword argument 
+            Length-Nh array storing the stellar mass of subhalos. 
+            The value ``subhalo_abundance_ordinates[i]`` gives the comoving number density 
+            of subhalos of property ``subhalo_abundance_abcissa[i]``. 
+            If keyword arguments ``subhalo_abundance_ordinates`` 
+            and ``subhalo_abundance_abcissa`` are not passed, 
+            then keyword arguments ``prim_haloprop_key`` and ``halos`` must be passed. 
+
+        scatter_level : float, optional keyword argument 
+            Level of constant scatter in dex. Default is 0.2. 
+
+        input_param_dict : dict, optional keyword argument
+            Dictionary containing values for the parameters specifying the model.
+            If none is passed, the `Moster13SmHm` instance will be initialized to 
+            the best-fit values taken from Moster et al. (2013). 
+        """
+
+        kwargs['scatter_model'] = LogNormalScatterModel
+        kwargs['scatter_abcissa'] = [12]
+        kwargs['scatter_ordinates'] = [scatter_level]
+
+        super(AbunMatchSmHm, self).__init__(**kwargs)
+
+        self.publications = ['arXiv:0903.4682', 'arXiv:1205.5807']
+
+    def mean_stellar_mass(self, **kwargs):
+        return None
+
+
+
+
+
