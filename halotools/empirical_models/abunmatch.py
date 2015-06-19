51b697dcea6ded817b45fb41edb08f108bffcfbd
Andrew Hearin <andrew.hearin@yale.edu>
2015-06-18 22:20:43 -0400

    Finished draft of zero_scatter_relation. Still needs testing

--- halotools/empirical_models/abunmatch.py
+++ halotools/empirical_models/abunmatch.py
@@ -86,19 +86,129 @@ class AbunMatchSmHm(PrimGalpropModel):
         return None
 
 class ConditionalAbunMatch(model_helpers.GalPropModel):
-    """ Generally-written class to produce any CAM-style model of a galaxy property, 
-    such as age matching.  
+    """ Class to produce any CAM-style model of a galaxy property, such as age matching.  
     """
-
-    def __init__(self, galprop_key, **kwargs):
+    def __init__(self, **kwargs):
         """ 
+        Parameters 
+        ----------
+        galprop_key : string, keyword argument
+            Name of the galaxy property being modeled. 
+
+        prim_galprop_key : string, keyword argument 
+
+        input_galaxy_table : data table 
+            Astropy Table object storing the input galaxy population.  
+            The conditional one-point functions of this population 
+            will be used as inputs when building the primary behavior 
+            of the `ConditionalAbunMatch` model. 
+
+        correlation_strength : float or array, optional keyword argument 
+            Specifies the desired Spearmann rank-order correlation coefficient 
+            between the secondary halo property and the galprop. 
+            If a float, the correlation strength will be assumed constant 
+            for all values of the prim_galprop. If an array, the i^th entry 
+            specifies the correlation strength when prim_galprop equals  
+            ``prim_galprop_bins[i]``. Default is None, in which case 
+            zero scatter is assumed. 
+
+        prim_galprop_bins : array 
+            Array used to bin the input galaxy population by the 
+            prim_galprop of the model. 
         """
-        setattr(self, 'mc_'+galprop_key, self._mc_galprop)
-        super(ConditionalAbunMatch, self).__init__(galprop_key=galprop_key)
+
+        required_kwargs = (
+            ['galprop_key', 'prim_galprop_key', 
+            'input_galaxy_table', 'prim_galprop_bins'])
+        model_helpers.bind_required_kwargs(required_kwargs, self, **kwargs)
+        setattr(self, 'mc_'+self.galprop_key, self._mc_galprop)
+        super(ConditionalAbunMatch, self).__init__(galprop_key=self.galprop_key)
 
 
     def _mc_galprop(self, **kwargs):
+        no_scatter_result = zero_scatter_relation(**kwargs)
+        ### add scatter, if non-zero
         return None
+
+    def zero_scatter_relation(self, seed=None, **kwargs):
+        """
+        Parameters 
+        ----------
+        galaxy_table : data table 
+            Astropy Table object storing the mock galaxy population.  
+
+        galaxy_table_slice_array : array, optional keyword argument 
+            Array of slice objects. The i^th entry of 
+            ``galaxy_table_slice_array`` stores the slice of 
+            the halo catalog which falls into the i^th 
+            stellar mass bin. Useful if exploring models 
+            with fixed stellar masses. Default is None, 
+            in which case the `zero_scatter_relation` determines 
+            this information for itself (at a performance cost). 
+
+        Returns 
+        -------
+        output_galprop : array 
+            Numpy array storing a Monte Carlo realization of 
+            the modeled galaxy property. The `zero_scatter_relation` 
+            assumes there is no scatter between the sec_haloprop 
+            and the galprop. 
+        """
+        galaxy_table = kwargs['galaxy_table']
+
+        if 'galaxy_table_slice_array' not in kwargs.keys():
+            binned_prim_galprop = np.digitize(
+                galaxy_table[self.prim_galprop_key], 
+                self.prim_galprop_bins)
+
+        # All at once, draw all the randoms we will need
+        all_randoms = np.random.random(len(galaxy_table), seed=seed)
+
+        # Initialize the output array
+        output_galprop = np.zeros(len(galaxy_table))
+
+        for i, prim_galprop in enumerate(binned_prim_galprop):
+
+            # Determine the slice corresponding to the i^th prim_galprop bin
+            if 'galaxy_table_slice_array' not in kwargs.keys():
+                idx_bini = np.where(binned_prim_galprop==i)[0]
+            else:
+                idx_bini = kwargs['galaxy_table_slice_array'][i]
+
+            # Determine the indices that would sort the mock galaxies 
+            # within the i^th prim_galprop bin
+            idx_sorted = np.argsort(galaxy_table[idx_bini][self.sec_haloprop_key])
+
+            # Fetch the appropriate number of randoms
+            # for the i^th prim_galprop bin, and sort them
+            randoms_bini = all_randoms[idx_bini]
+            randoms_bini.sort()
+
+            # Use method of transformation of variables to 
+            # determine a realization galprop values for all 
+            # mock galaxies in the i^th prim_galprop bin
+            output_galprop[idx_bini][idx_sorted] = (
+                self.one_point_lookup_table[i](cumulative_prob=randoms_bini)
+                )
+
+        return output_galprop
+
+
+    def build_one_point_lookup_table(self, cumulative_prob):
+        """
+        Parameters 
+        ----------
+        input_galaxy_table : data table 
+            Astropy Table object storing the input galaxy population.  
+            The conditional one-point functions of this population 
+            will be used as inputs when building the primary behavior 
+            of the `ConditionalAbunMatch` model. 
+        """
+
+        pass
+
+
+
 
 
 
