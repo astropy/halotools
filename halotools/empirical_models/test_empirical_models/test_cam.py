a92409991f255efb87bf6790fd7147028e454e3c
Andrew Hearin <andrew.hearin@yale.edu>
2015-06-20 06:39:22 -0400

    Starting to develop cam test suite

---
+++ halotools/empirical_models/test_empirical_models/test_cam.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import numpy as np 
+from astropy.table import Table 
+
+from .. abunmatch import ConditionalAbunMatch
+from .. import model_defaults
+
+
+
+
+
+
+
+
+
+
+
+
+
+
