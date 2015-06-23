2b8c0422cfbf6c2ef4c585749f6f906d7f4fcf7f
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-06-23 14:08:28 -0400

    updated hwo w_p is calculated.  updated doc strings

--- halotools/mock_observables/test_clustering/test_wp.py
+++ halotools/mock_observables/test_clustering/test_wp.py
@@ -16,10 +16,10 @@ def test_wp_auto():
     randoms = np.random.random((100,3))
     period = np.array([1,1,1])
     rp_bins = np.linspace(0,0.5,5)
-    pi_bins = np.linspace(0,0.5,5)
+    pi_max = 0.5
     
     #with randoms
-    result = wp(sample1, rp_bins, pi_bins, sample2 = None, 
+    result = wp(sample1, rp_bins, pi_max, sample2 = None, 
                   randoms=randoms, period = None, 
                   max_sample_size=int(1e4), estimator='Natural')
 
@@ -32,13 +32,14 @@ def test_wp_auto_periodic():
     randoms = np.random.random((100,3))
     period = np.array([1,1,1])
     rp_bins = np.linspace(0,0.5,5)
-    pi_bins = np.linspace(0,0.5,5)
+    pi_max = 0.5
     
     #with randoms
-    result = wp(sample1, rp_bins, pi_bins, sample2 = None, 
-                  randoms=randoms, period = period, 
-                  max_sample_size=int(1e4), estimator='Natural')
+    result = wp(sample1, rp_bins, pi_max, sample2 = None, 
+                randoms=randoms, period = period, 
+                max_sample_size=int(1e4), estimator='Natural')
     
+
     assert result.ndim == 1, "More than one correlation function returned erroneously."
 
 
@@ -48,13 +49,13 @@ def test_wp_cross_periodic():
     randoms = np.random.random((100,3))
     period = np.array([1,1,1])
     rp_bins = np.linspace(0,0.5,5)
-    pi_bins = np.linspace(0,0.5,5)
+    pi_max = 0.5
     
     #with randoms
-    result = wp(sample1, rp_bins, pi_bins, sample2 = sample2, 
-                  randoms=randoms, period = period, 
-                  max_sample_size=int(1e4), estimator='Natural')
-    
+    result = wp(sample1, rp_bins, pi_max, sample2 = sample2, 
+                randoms=randoms, period = period, 
+                max_sample_size=int(1e4), estimator='Natural')
+
     assert len(result)==3, "wrong number of correlations returned"
     assert result[0].ndim == 1, "dimension of auto incorrect"
     assert result[1].ndim == 1, "dimension of cross incorrect"
