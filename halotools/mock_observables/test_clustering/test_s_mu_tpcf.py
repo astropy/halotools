f65c7074fbb25e88fc9289fa7a9db5623a67cc0a
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-07-02 12:51:17 -0400

    added s_mu_tpcf function

---
+++ halotools/mock_observables/test_clustering/test_s_mu_tpcf.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from __future__ import division, print_function
+import numpy as np
+import sys
+from ..clustering import tpcf, s_mu_tpcf
+
+####two point correlation function########################################################
+
+def test_TPCF_auto():
+    
+    sample1 = np.random.random((100,3))
+    sample2 = np.random.random((100,3))
+    randoms = np.random.random((100,3))
+    period = np.array([1,1,1])
+    s_bins = np.linspace(0,0.5,5)
+    mu_bins = np.linspace(0,1.0,10)
+    
+    
+    result = s_mu_tpcf(sample1, s_bins, mu_bins, sample2 = None, 
+                       randoms=randoms, period = None, 
+                       max_sample_size=int(1e4), estimator='Natural')
+    
+    assert result.ndim == 2, "correlation function returned has wrong dimension."
