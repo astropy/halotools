9f9d924dfd7d7fbb106b7f3cd6ed4b204ed30d8c
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-16 13:36:09 -0400

    Hacky workaround of simulation loading problem

--- halotools/sim_manager/tests/test_cached_simulations.py
+++ halotools/sim_manager/tests/test_cached_simulations.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 from ..read_nbody import ProcessedSnapshot
+import os
+from astropy.config.paths import _find_home 
 
 def test_cached_simulations():
+	xch = os.environ.get('XDG_CACHE_HOME')
+	home = _find_home()
+	astropy_cache = os.path.join(home, '.astropy')
+	os.environ['XDG_CACHE_HOME'] = astropy_cache
 	simobj = ProcessedSnapshot()
+	os.environ['XDG_CACHE_HOME'] = xch
+	
