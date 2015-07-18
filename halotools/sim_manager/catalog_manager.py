ff5f23289d1c2ddd311894d648d2bf8964c0e89d
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-18 09:50:07 -0400

    Starting to develop catalog_manager module, which basically just refactors existing behavior

---
+++ halotools/sim_manager/catalog_manager.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Methods and classes for halo catalog I/O and organization.
+
+"""
+
+class CatalogManager(object):
+	""" Class used to scrape the web for simulation data  
+	and manage the set of cached catalogs. 
+	"""
+
+	def __init__(self):
+		pass
+
+
+
+class HaloCatalogProcessor(object):
+	""" Class used to read halo catalog ASCII data, 
+	produce a value-added halo catalog, and store the catalog  
+	in the cache directory or other desired location. 
+	"""
+
+	def __init__(self):
+		pass
+
+
+
