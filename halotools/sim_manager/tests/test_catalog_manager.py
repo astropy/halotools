0759af2d5b64e121af41fc9d94654ae3d532fdee
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-18 16:45:10 -0400

    Have debugged version of processed_halocats_in_cache in catalog_manager

--- halotools/sim_manager/tests/test_catalog_manager.py
+++ halotools/sim_manager/tests/test_catalog_manager.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 
-import os
+import os, fnmatch
+import numpy as np
+
 from ..catalog_manager import CatalogManager
 
+
 def test_processed_halocats_in_cache():
-	temp_fnames = []
-	temp_fnames.append(os.path.abspath('dummy_sim1/dummy_hfinder1/dummy_fname1.dummyversion1.hdf5'))
-	temp_fnames.append(os.path.abspath('dummy_sim1/dummy_hfinder1/dummy_fname2.dummyversion1.hdf5'))
-	temp_fnames.append(os.path.abspath('dummy_sim1/dummy_hfinder1/dummy_fname3.dummyversion1.hdf5'))
+	pass
 
-	temp_fnames.append(os.path.abspath('dummy_sim2/dummy_hfinder1/dummy_fname1.dummyversion1.hdf5'))
-	temp_fnames.append(os.path.abspath('dummy_sim2/dummy_hfinder1/dummy_fname1.dummyversion2.hdf5'))
-	temp_fnames.append(os.path.abspath('dummy_sim2/dummy_hfinder1/dummy_fname2.dummyversion1.hdf5'))
-	temp_fnames.append(os.path.abspath('dummy_sim2/dummy_hfinder2/dummy_fname1.dummyversion1.hdf5'))
+#	result = catman.processed_halocats_in_cache(external_cache_loc=dummyloc)
+#	assert set(result) == set(temp_fnames)
 
-	for fname in temp_fnames:
-		os.system('touch ' + fname)
 
-	catman = CatalogManager()
-	
+
+
+
 
 
 
