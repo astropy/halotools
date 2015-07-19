c7f0c86d0d11080bac692b1d1063e0dbe2054462
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-18 20:19:49 -0400

    Finished first pass at test of test_processed_halocats_in_cache

--- halotools/sim_manager/tests/test_catalog_manager.py
+++ halotools/sim_manager/tests/test_catalog_manager.py
@@ -28,25 +28,93 @@ class TestCatalogManager(TestCase):
 
         # First create an empty directory where we will 
         # temporarily store a collection of empty files
-        dummydir = 'temp_directory_for_halotools_testing'
-        self.dummyloc = os.path.join(homedir, dummydir)
+        dummydir = os.path.join(homedir, 'temp_directory_for_halotools_testing')
+        defensively_create_empty_dir(dummydir)
+        self.dummyloc = os.path.join(dummydir, 'halotools')
         defensively_create_empty_dir(self.dummyloc)
+
+        self.halocat_dir = os.path.join(self.dummyloc, 'halo_catalogs')
+        defensively_create_empty_dir(self.halocat_dir)
+
+        self.ptclcat_dir = os.path.join(self.dummyloc, 'particle_catalogs')
+        defensively_create_empty_dir(self.ptclcat_dir)
+
+        self.raw_halocat_dir = os.path.join(self.dummyloc, 'raw_halo_catalogs')
+        defensively_create_empty_dir(self.raw_halocat_dir)
 
         self.simnames = ['bolshoi', 'bolshoiplanck', 'multidark', 'consuelo']
         self.halo_finders = ['rockstar', 'bdm']
         self.dummy_version_names = ['halotools.alpha', 'some_other_cuts']
-        self.extension = ['.hdf5']
+        self.extension = '.hdf5'
 
-        self.dummy_fnames = ['hlist_1.00030', 'hlist_0.547', 'hlist_0.3397']
+        self.bolshoi_fnames = ['hlist_0.33035', 'hlist_0.54435', 'hlist_0.67035', 'hlist_1.00035']
+        self.bolshoi_bdm_fnames = ['hlist_0.33030', 'hlist_0.49830', 'hlist_0.66430', 'hlist_1.00035']
+        self.bolshoiplanck_fnames = ['hlist_0.33035', 'hlist_0.54435', 'hlist_0.67035', 'hlist_1.00035']
+        self.consuelo_fnames = ['hlist_0.33324', 'hlist_0.50648', 'hlist_0.67540', 'hlist_1.00000']
+        self.multidark_fnames = ['hlist_0.31765', 'hlist_0.49990', 'hlist_0.68215', 'hlist_1.00109']
 
-        assert os.path.isdir(self.dummyloc)
+        # make all relevant subdirectories and dummy files
+        for simname in self.simnames:
+            simdir = os.path.join(self.halocat_dir, simname)
+            defensively_create_empty_dir(simdir)
+            rockstardir = os.path.join(simdir, 'rockstar')
+            defensively_create_empty_dir(rockstardir)
 
-        fname = os.path.join(self.dummyloc, 'marf.txt')
-        os.system('touch ' + fname)
-        assert os.path.isfile(fname)
+            if simname == 'bolshoi':
+                fnames = self.bolshoi_fnames
+            elif simname == 'bolshoiplanck':
+                fnames = self.bolshoiplanck_fnames
+            elif simname == 'consuelo':
+                fnames = self.consuelo_fnames
+            elif simname == 'multidark':
+                fnames = self.multidark_fnames
 
-#        for simname in self.dummy_simnames:
-#            os.mkdir(os.path.join(self.dummyloc, simname))
+            for name in fnames:
+                for version in self.dummy_version_names:
+                    full_fname = name + '.' + version + self.extension
+                    abs_fname = os.path.join(rockstardir, full_fname)
+                    os.system('touch ' + abs_fname)
+
+            if simname == 'bolshoi':
+                simdir = os.path.join(self.halocat_dir, simname)
+                bdmdir = os.path.join(simdir, 'bdm')
+                defensively_create_empty_dir(bdmdir)
+                fnames = self.bolshoi_bdm_fnames
+                for name in fnames:
+                    for version in self.dummy_version_names:
+                        full_fname = name + '.' + version + self.extension
+                        abs_fname = os.path.join(bdmdir, full_fname)
+                        os.system('touch ' + abs_fname)
+
+        #p = os.path.join(self.halocat_dir, 'bolshoi', 'rockstar')
+        #assert os.path.isdir(p)
+        #f = 'hlist_0.33035.halotools.alpha.hdf5'
+        #full_fname = os.path.join(p, f)
+        #assert os.path.isfile(full_fname)
+
+    def test_processed_halocats_in_cache(self):
+        catman = CatalogManager()
+
+        for simname in self.simnames:
+            attrname = simname + '_fnames'
+            basenames_from_self = getattr(self, attrname)
+
+            for version in self.dummy_version_names:
+                basenames_from_setup = [f + '.' + version + self.extension for f in basenames_from_self]
+
+
+                result = catman.processed_halocats_in_cache(external_cache_loc=self.halocat_dir, 
+                    simname = simname, halo_finder = 'rockstar', version_name = version)
+                assert result != []
+
+                basenames_from_catman = [os.path.basename(f) for f in result]
+                
+                assert set(basenames_from_catman) == set(basenames_from_setup)
+
+
+
+
+
 
 
     def teardown_class(self):
@@ -56,40 +124,6 @@ class TestCatalogManager(TestCase):
 
 
 
-"""
-    def test_processed_halocats_in_cache(self):
-
-
-        catman = CatalogManager()
-
-
-        self.defensively_create_empty_dir(dummyloc)
-        self.defensively_create_empty_dir(os.path.join(dummyloc, 'dummy_sim1'))
-        self.defensively_create_empty_dir(os.path.join(dummyloc, 'dummy_sim1', 'dummy_hfinder1'))
-        self.defensively_create_empty_dir(os.path.join(dummyloc, 'dummy_sim1', 'dummy_hfinder2'))
-        self.defensively_create_empty_dir(os.path.join(dummyloc, 'dummy_sim2'))
-        self.defensively_create_empty_dir(os.path.join(dummyloc, 'dummy_sim2', 'dummy_hfinder1'))
-        self.defensively_create_empty_dir(os.path.join(dummyloc, 'dummy_sim2', 'dummy_hfinder2'))
-
-        temp_fnames = []
-        temp_fnames.append(dummyloc + '/dummy_sim1/dummy_hfinder1/dummy_fname2.dummyversion1.hdf5')
-        temp_fnames.append(dummyloc + '/dummy_sim1/dummy_hfinder1/dummy_fname3.dummyversion1.hdf5')
-        temp_fnames.append(dummyloc + '/dummy_sim1/dummy_hfinder1/dummy_fname1.dummyversion1.hdf5')
-        temp_fnames.append(dummyloc + '/dummy_sim2/dummy_hfinder1/dummy_fname1.dummyversion1.hdf5')
-        temp_fnames.append(dummyloc + '/dummy_sim2/dummy_hfinder1/dummy_fname1.dummyversion2.hdf5')
-        temp_fnames.append(dummyloc + '/dummy_sim2/dummy_hfinder1/dummy_fname2.dummyversion1.hdf5')
-        temp_fnames.append(dummyloc + '/dummy_sim2/dummy_hfinder2/dummy_fname1.dummyversion1.hdf5')
-        temp_fnames = list(set(temp_fnames))
-
-        for f in temp_fnames:
-            os.system('touch ' + f)
-                
-        result = catman.processed_halocats_in_cache(external_cache_loc=dummyloc)
-        assert set(result) == set(temp_fnames)
-"""
-
-#   result = catman.processed_halocats_in_cache(external_cache_loc=dummyloc)
-#   assert set(result) == set(temp_fnames)
 
 
 
