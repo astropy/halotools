c2609806342248b6da054b472ab65beee07b36a4
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-20 15:27:06 -0400

    Fixed bug in default and header definitions, which appeared at the top of the raw_halocat_column_info module before they were defined. Now in sim_defaults

--- halotools/sim_manager/sim_defaults.py
+++ halotools/sim_manager/sim_defaults.py
@@ -9,6 +9,7 @@ import os, sys
 import numpy as np
 
 from astropy import cosmology
+from . import raw_halocat_column_info
 
 ### Default halo catalog (used in read_nbody)
 # The following parameters are used by the 
@@ -25,3 +26,25 @@ default_cosmology = cosmology.WMAP5
 processed_halo_tables_webloc = 'http://www.astro.yale.edu/aphearin/Data_files/halo_catalogs'
 ptcl_tables_webloc = 'http://www.astro.yale.edu/aphearin/Data_files/particle_catalogs'
 default_version_name = 'halotools.official.version'
+
+############################################################
+### Current versions of dtype and header 
+# for reading ASCII data are defined below 
+# Definitions are in terms of historical information 
+#defined in raw_halocat_column_info 
+dtype_consuelo_rockstar = raw_halocat_column_info.dtype_slac_consuelo_rockstar_july19_2015
+header_consuelo_rockstar = raw_halocat_column_info.header_slac_consuelo_rockstar_july19_2015
+#
+dtype_bolshoi_rockstar = raw_halocat_column_info.dtype_slac_bolshoi_rockstar_july19_2015
+header_bolshoi_rockstar = raw_halocat_column_info.header_slac_bolshoi_rockstar_july19_2015
+#
+dtype_bolplanck_rockstar = raw_halocat_column_info.dtype_slac_bolplanck_rockstar_july19_2015
+header_bolplanck_rockstar = raw_halocat_column_info.header_slac_bolplanck_rockstar_july19_2015
+#
+dtype_multidark_rockstar = raw_halocat_column_info.dtype_slac_multidark_rockstar_july19_2015
+header_multidark_rockstar = raw_halocat_column_info.header_slac_multidark_rockstar_july19_2015
+#
+dtype_bolshoi_bdm = raw_halocat_column_info.dtype_slac_bolshoi_bdm_july19_2015
+header_bolshoi_bdm = raw_halocat_column_info.header_slac_bolshoi_bdm_july19_2015
+############################################################
+
