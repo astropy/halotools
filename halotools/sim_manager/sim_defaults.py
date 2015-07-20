7bbf92366598b5b65077beae594a6efeb2cafc00
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-20 16:08:08 -0400

    Added return_dtype_and_header function to sim_defaults, and retrieve_simclass to supported_sims. Both functions are used by HaloCatalog constructor

--- halotools/sim_manager/sim_defaults.py
+++ halotools/sim_manager/sim_defaults.py
@@ -10,6 +10,7 @@ import numpy as np
 
 from astropy import cosmology
 from . import raw_halocat_column_info
+from ..halotools_exceptions import UnsupportedSimError, HalotoolsCacheError
 
 ### Default halo catalog (used in read_nbody)
 # The following parameters are used by the 
@@ -32,8 +33,6 @@ default_version_name = 'halotools.official.version'
 # for reading ASCII data are defined below 
 # Definitions are in terms of historical information 
 #defined in raw_halocat_column_info 
-dtype_consuelo_rockstar = raw_halocat_column_info.dtype_slac_consuelo_rockstar_july19_2015
-header_consuelo_rockstar = raw_halocat_column_info.header_slac_consuelo_rockstar_july19_2015
 #
 dtype_bolshoi_rockstar = raw_halocat_column_info.dtype_slac_bolshoi_rockstar_july19_2015
 header_bolshoi_rockstar = raw_halocat_column_info.header_slac_bolshoi_rockstar_july19_2015
@@ -44,7 +43,39 @@ header_bolplanck_rockstar = raw_halocat_column_info.header_slac_bolplanck_rockst
 dtype_multidark_rockstar = raw_halocat_column_info.dtype_slac_multidark_rockstar_july19_2015
 header_multidark_rockstar = raw_halocat_column_info.header_slac_multidark_rockstar_july19_2015
 #
+dtype_consuelo_rockstar = raw_halocat_column_info.dtype_slac_consuelo_rockstar_july19_2015
+header_consuelo_rockstar = raw_halocat_column_info.header_slac_consuelo_rockstar_july19_2015
+#
 dtype_bolshoi_bdm = raw_halocat_column_info.dtype_slac_bolshoi_bdm_july19_2015
 header_bolshoi_bdm = raw_halocat_column_info.header_slac_bolshoi_bdm_july19_2015
+
+def return_dtype_and_header(simname, halo_finder):
+	if halo_finder == 'rockstar':
+		if simname == 'bolshoi':
+			return dtype_bolshoi_rockstar, header_bolshoi_rockstar
+		elif simname == 'bolplanck':
+			return dtype_bolplanck_rockstar, header_bolplanck_rockstar
+		elif simname == 'multidark':
+			return dtype_multidark_rockstar, header_multidark_rockstar
+		elif simname == 'consuelo':
+			return dtype_consuelo_rockstar, header_consuelo_rockstar
+		else:
+			raise UnsupportedSimError(simname)
+	elif halo_finder == 'bdm':
+		if simname == 'bolshoi':
+			return dtype_bolshoi_bdm, header_bolshoi_bdm
+	else:
+		msg = "Unsupported combination of halo_finder = ``%s`` and simname = ``%s``"
+		raise HalotoolsCacheError(msg % (halo_finder, simname))
+
+
+
+
+
+
+
+
+
+
 ############################################################
 
