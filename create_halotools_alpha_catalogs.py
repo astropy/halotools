fb35a0151e2c1699dffdf95ff22a04536452be9b
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-21 09:23:29 -0400

    Starting to develop catalog creation script for alpha version

---
+++ create_halotools_alpha_catalogs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import numpy as np
+
+from halotools.sim_manager.catalog_manager import CatalogManager
+from halotools.sim_manager.read_nbody_ascii import BehrooziASCIIReader
+
+
+catman = CatalogManager()
+
+
