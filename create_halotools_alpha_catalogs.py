5441bb5af64dbd07a2e79c9809ed9952a001786f
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-21 14:34:05 -0400

    Added module to collect commonly called functions on halo catalogs. Currently only includes host_status function

--- create_halotools_alpha_catalogs.py
+++ create_halotools_alpha_catalogs.py
@@ -8,21 +8,21 @@ from halotools.sim_manager.read_nbody_ascii import BehrooziASCIIReader
 catman = CatalogManager()
 
 
-flist = catman.raw_halo_tables_in_cache(simname='bolshoi', halo_finder='bdm')
+flist = catman.raw_halo_tables_in_cache(simname='bolshoi', halo_finder='rockstar')
 fname = flist[0]
 
 
-column_bounds = [('halo_mpeak', 150, float("inf"))]
-reader = BehrooziASCIIReader(input_fname = fname, recompress=False, column_bounds=column_bounds)
-#reader = BehrooziASCIIReader(input_fname = fname, recompress=False, cuts_funcobj='nocut')
+column_bounds = [('halo_mpeak', 5e9, float("inf"))]
+
+#reader = BehrooziASCIIReader(input_fname = fname, recompress=False, column_bounds=column_bounds)
+reader = BehrooziASCIIReader(input_fname = fname, recompress=False, cuts_funcobj='nocut')
 #reader = BehrooziASCIIReader(input_fname = fname, recompress=False)
 #reader = BehrooziASCIIReader(input_fname = fname, recompress=False)
 
 t = reader.read_halocat()
 
-bolshoi_bdm_keys_to_keep = (['halo_scale_factor', 
+keys_to_keep = (['halo_scale_factor', 
 	'halo_id',
-	'halo_id_desc',
 	'halo_pid',
 	'halo_upid',
 	'halo_phantom',
@@ -43,22 +43,38 @@ bolshoi_bdm_keys_to_keep = (['halo_scale_factor',
 	'halo_jz',
 	'halo_spin',
 	'halo_id_tree_root',
+	'halo_rs_klypin',
 	'halo_xoff',
-	'halo_2K/Ep-1',
+	'halo_voff',
+	'halo_spin_bullock',
 	'halo_b_to_a',
 	'halo_c_to_a',
 	'halo_axisA_x',
 	'halo_axisA_y',
 	'halo_axisA_z',
+	'halo_t_by_u',
 	'halo_macc',
 	'halo_mpeak',
 	'halo_vacc',
-	'halo_vpeak'
+	'halo_vpeak',
+	'halo_halfmass_scale',
+	'halo_dmvir_dt_inst',
+	'halo_dmvir_dt_100myr',
+	'halo_dmvir_dt_tdyn',
+	'halo_scale_factor_mpeak',
+	'halo_scale_factor_lastacc',
+	'halo_scale_factor_firstacc',
+	'halo_mvir_firstacc',
+	'halo_vmax_firstacc',
+	'halo_vmax_mpeak'
 	])
 
 for key in t.keys():
-	if key not in bolshoi_bdm_keys_to_keep:
+	if key not in keys_to_keep:
 		del t[key]
+t['halo_nfw_conc'] = t['halo_rvir'] / t['halo_rs']
+del t['halo_rs']
+t['halo_rvir'] /= 1000.
 
 print("Number of halos = %i" % len(t))
 
