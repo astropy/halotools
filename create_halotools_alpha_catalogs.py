89a5b8b995df0a88742605e7667f2e2af6e86c33
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-21 16:40:02 -0400

    Slight modification to function signature of main() of the create_halotools_alpha_catalogs script.

--- create_halotools_alpha_catalogs.py
+++ create_halotools_alpha_catalogs.py
@@ -4,41 +4,42 @@ import os, sys
 
 from halotools.sim_manager.catalog_manager import CatalogManager
 from halotools.sim_manager.read_nbody_ascii import BehrooziASCIIReader
-from halotools.simname import sim_defaults
+from halotools.sim_manager import sim_defaults
 from halotools.utils import halocat_utils
 
 
 #	external_cache_loc = os.path.abspath('/Volumes/NbodyDisk1/July19_new_catalogs')
-#fname, z= catman.closest_catalog_in_cache(catalog_type='raw_halos', desired_redshift=0, simname='bolshoi', halo_finder='rockstar',external_cache_loc = external_cache_loc) 
 
-
-def main(fname):
-
+def main(input_cache_loc):
 
 	catman = CatalogManager()
 
-	reader = BehrooziASCIIReader(input_fname = fname, overwrite=True)
-	halo_table = reader.read_halocat()
+	def process_and_store_result(fname):
 
-	keys_to_keep = sim_defaults.default_ascii_columns_to_keep
-	for key in halo_table.keys():
-		if key not in keys_to_keep:
-			del halo_table[key]
-	halo_table['halo_nfw_conc'] = halo_table['halo_rvir'] / halo_table['halo_rs']
-	del halo_table['halo_rs']
-	halo_table['halo_rvir'] /= 1000. # convert rvir to Mpc
+		reader = BehrooziASCIIReader(input_fname = fname, overwrite=True)
+		halo_table = reader.read_halocat()
 
-	halo_table['halo_hostid'] = halo_table['halo_upid']
-	host_mask = halo_table['halo_upid'] == -1
-	halo_table['halo_hostid'][host_mask] = halo_table['halo_id'][host_mask]
+		keys_to_keep = sim_defaults.default_ascii_columns_to_keep
+		for key in halo_table.keys():
+			if key not in keys_to_keep:
+				del halo_table[key]
+		halo_table['halo_nfw_conc'] = halo_table['halo_rvir'] / halo_table['halo_rs']
+		del halo_table['halo_rs']
+		halo_table['halo_rvir'] /= 1000. # convert rvir to Mpc
 
-	halo_table['host_halo_status'] = halocat_utils.host_status(halo_table)
+		halo_table['halo_hostid'] = halo_table['halo_upid']
+		host_mask = halo_table['halo_upid'] == -1
+		halo_table['halo_hostid'][host_mask] = halo_table['halo_id'][host_mask]
 
-	catman.store_newly_processed_halo_table(
-		halo_table, reader, sim_defaults.default_version_name, overwrite=True)
+		halo_table['host_halo_status'] = halocat_utils.host_status(halo_table)
+
+		catman.store_newly_processed_halo_table(
+			halo_table, reader, sim_defaults.default_version_name, overwrite=True)
 
 
-
+	raw_halo_files_to_process = catman.raw_halo_tables_in_cache(external_cache_loc = input_cache_loc)
+	for fname in raw_halo_files_to_process:
+		process_and_store_result(fname)
 
 
 
@@ -47,7 +48,7 @@ def main(fname):
 ###################################################################################################
 
 if __name__ == "__main__":
-        main()
+        main(sys.argv[1])
 
 
 
