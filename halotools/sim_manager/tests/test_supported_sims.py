e32e06b95f18c62b37efa13682ef54b6175f9407
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-23 09:17:52 -0400

    All HaloCatalog objects instantiate when there is a matching catalog in cache. All halo_table and ptcl_table attributes correctly load. Full test suite passes

--- halotools/sim_manager/tests/test_supported_sims.py
+++ halotools/sim_manager/tests/test_supported_sims.py
@@ -17,11 +17,23 @@ else:
 from .. import cache_config
 from ..supported_sims import HaloCatalog 
 
+APH_MACHINE = False
+
 @pytest.mark.skipif('not APH_MACHINE')
 def test_load_halo_catalogs():
 
     simnames = cache_config.supported_sim_list
-    zlist = [0, 0.5, 1, 2]
+    adict = {'bolshoi': [0.33035, 0.54435, 0.67035, 1], 'bolplanck': [0.33406, 0.50112, 0.67, 1], 
+        'consuelo': [0.333, 0.506, 0.6754, 1], 'multidark': [0.318, 0.5, 0.68, 1]}
     for simname in simnames:
-        for z in zlist:
+        alist = adict[simname]
+        for a in alist:
+            z = 1/a - 1
             halocat = HaloCatalog(simname = simname, redshift = z)
+            halos = halocat.halo_table
+            if simname not in ['bolshoi', 'multidark']:
+                particles = halocat.ptcl_table
+            else:
+                if a == 1:
+                    particles = halocat.ptcl_table
+
