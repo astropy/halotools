8485271fd1d04d5fa3bd6f5f3e426086e822e25f
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-23 15:06:23 -0400

    Switched back default cen_profile of Zheng07_blueprint to be an IsotropicGalProf rather than a TrivialProfile

--- halotools/empirical_models/preloaded_hod_blueprints.py
+++ halotools/empirical_models/preloaded_hod_blueprints.py
@@ -57,11 +57,8 @@ def Zheng07_blueprint(**kwargs):
 	cen_model_dict['occupation'] = dark_side_cen_model
 	# Build the profile model
 	
-	#cen_profile = gpf.IsotropicGalProf(
-		#gal_type=cen_key, halo_prof_model=hpc.TrivialProfile)
-
-	cen_profile = hpc.TrivialProfile()
-
+	cen_profile = gpf.IsotropicGalProf(
+		gal_type=cen_key, halo_prof_model=hpc.TrivialProfile)
 
 	cen_model_dict['profile'] = cen_profile
 
