68d145328a8cd6fade98b5a0c3061ccb87508af4
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-24 08:09:35 -0400

    Added new pre-loaded blueprint for Leauthaud11. Added non-trivial tests of param_dict propagation. Test suite passes.

--- halotools/empirical_models/preloaded_hod_blueprints.py
+++ halotools/empirical_models/preloaded_hod_blueprints.py
@@ -47,9 +47,9 @@ def Zheng07_blueprint(threshold = model_defaults.default_luminosity_threshold, *
 	cen_key = 'centrals'
 	cen_model_dict = {}
 	# Build the occupation model
-	dark_side_cen_model = hoc.Zheng07Cens(gal_type=cen_key, 
+	occu_cen_model = hoc.Zheng07Cens(gal_type=cen_key, 
 		threshold = threshold)
-	cen_model_dict['occupation'] = dark_side_cen_model
+	cen_model_dict['occupation'] = occu_cen_model
 	# Build the profile model
 	
 	cen_profile = gpf.IsotropicGalProf(
@@ -61,22 +61,84 @@ def Zheng07_blueprint(threshold = model_defaults.default_luminosity_threshold, *
 	sat_key = 'satellites'
 	sat_model_dict = {}
 	# Build the occupation model
-	dark_side_sat_model = hoc.Zheng07Sats(gal_type=sat_key, 
+	occu_sat_model = hoc.Zheng07Sats(gal_type=sat_key, 
 		threshold = threshold)
-	sat_model_dict['occupation'] = dark_side_sat_model
+	sat_model_dict['occupation'] = occu_sat_model
 	# Build the profile model
 	sat_profile = gpf.IsotropicGalProf(
 		gal_type=sat_key, halo_prof_model=hpc.NFWProfile)
 	sat_model_dict['profile'] = sat_profile
 
 	model_blueprint = {
-		dark_side_cen_model.gal_type : cen_model_dict,
-		dark_side_sat_model.gal_type : sat_model_dict, 
+		occu_cen_model.gal_type : cen_model_dict,
+		occu_sat_model.gal_type : sat_model_dict, 
 		'mock_factory' : mock_factories.HodMockFactory
 		}
 
 	return model_blueprint
 
+
+def Leauthaud11_blueprint(threshold = model_defaults.default_stellar_mass_threshold, **kwargs):
+	""" Blueprint for the simplest pre-loaded HOD model. 
+	There are two populations, 
+	centrals and satellites, with occupation statistics, 
+	positions and velocities based on Kravtsov et al. (2004). 
+
+	Documentation of the test suite of this blueprint can be found at 
+	`~halotools.empirical_models.test_empirical_models.test_Zheng07_blueprint`
+
+	Parameters 
+	----------
+	threshold : float, optional 
+		Luminosity threshold of the galaxy sample being modeled. 
+
+	Returns 
+	-------
+	model_blueprint : dict 
+		Dictionary containing instructions for how to build the model. 
+		When model_blueprint is passed to `~halotools.empirical_models.HodModelFactory`, 
+		the factory returns the Zheng07 model object. 
+
+	Examples 
+	--------
+	>>> from halotools.empirical_models import preloaded_hod_blueprints
+	>>> blueprint = preloaded_hod_blueprints.Leauthaud11_blueprint()
+	>>> blueprint  = preloaded_hod_blueprints.Leauthaud11_blueprint(threshold = 11.25)
+	"""		
+
+	### Build model for centrals
+	cen_key = 'centrals'
+	cen_model_dict = {}
+	# Build the occupation model
+	occu_cen_model = hoc.Leauthaud11Cens(gal_type=cen_key, 
+		threshold = threshold)
+	cen_model_dict['occupation'] = occu_cen_model
+	# Build the profile model
+	
+	cen_profile = gpf.IsotropicGalProf(
+		gal_type=cen_key, halo_prof_model=hpc.TrivialProfile)
+
+	cen_model_dict['profile'] = cen_profile
+
+	### Build model for satellites
+	sat_key = 'satellites'
+	sat_model_dict = {}
+	# Build the occupation model
+	occu_sat_model = hoc.Leauthaud11Sats(gal_type=sat_key, 
+		threshold = threshold)
+	sat_model_dict['occupation'] = occu_sat_model
+	# Build the profile model
+	sat_profile = gpf.IsotropicGalProf(
+		gal_type=sat_key, halo_prof_model=hpc.NFWProfile)
+	sat_model_dict['profile'] = sat_profile
+
+	model_blueprint = {
+		occu_cen_model.gal_type : cen_model_dict,
+		occu_sat_model.gal_type : sat_model_dict, 
+		'mock_factory' : mock_factories.HodMockFactory
+		}
+
+	return model_blueprint
 
 
 
--- halotools/empirical_models/test_empirical_models/test_hod_factory.py
+++ halotools/empirical_models/test_empirical_models/test_hod_factory.py
@@ -85,7 +85,56 @@ def test_alt_Zheng07_composites():
 	assert nsat3 == nsat4
 
 
+def test_Leauthaud11_composite():
+	""" Method to test the basic behavior of 
+	`~halotools.empirical_models.preloaded_models.Zheng07`, 
+	a specific pre-loaded model of 
+	`~halotools.empirical_models.model_factories.HodModelFactory`. 
 
+	The suite includes the following tests:
+
+		* Changes to ``self.param_dict`` properly propagate through to occupation component models. 
+
+		* Default behavior is recovered after calling the `~halotools.empirical_models.model_factories.HodModelFactory.restore_init_param_dict` method. 
+	"""
+	model = preloaded_models.Leauthaud11(threshold = 10.5)
+
+	# Verify that changes param_dict properly propagate
+	testmass1 = 5.e11
+	ncen1 = model.mean_occupation_centrals(prim_haloprop=testmass1)
+	nsat1 = model.mean_occupation_satellites(prim_haloprop=testmass1)
+	model.param_dict['n10_centrals'] *= 1.1
+	ncen2 = model.mean_occupation_centrals(prim_haloprop=testmass1)
+	nsat2 = model.mean_occupation_satellites(prim_haloprop=testmass1)
+	assert ncen2 > ncen1
+	assert nsat2 > nsat1
+
+	model.param_dict['n11_centrals'] *= 1.1
+	ncen3 = model.mean_occupation_centrals(prim_haloprop=testmass1)
+	nsat3 = model.mean_occupation_satellites(prim_haloprop=testmass1)
+	assert ncen3 == ncen2
+	assert nsat3 == nsat2
+
+	"""
+	orig_logMmin_centrals = model.param_dict['logMmin_centrals']
+	model.param_dict['logMmin_centrals'] = 11.5
+	cenocc_new = model.mean_occupation_centrals(prim_haloprop=testmass1)
+	assert cenocc_new < cenocc_orig
+
+	testmass2 = 5.e12
+	satocc_orig = model.mean_occupation_satellites(prim_haloprop=testmass2)
+	model.param_dict['logM0_satellites'] = 11.4
+	satocc_new = model.mean_occupation_satellites(prim_haloprop=testmass2)
+	assert satocc_new < satocc_orig
+
+	# Test that we can recover our initial behavior
+	model.restore_init_param_dict()
+	assert model.param_dict['logMmin_centrals'] == orig_logMmin_centrals
+	cenocc_restored = model.mean_occupation_centrals(prim_haloprop=testmass1)
+	assert cenocc_restored == cenocc_orig
+	satocc_restored = model.mean_occupation_satellites(prim_haloprop=testmass2)
+	assert satocc_restored == satocc_orig
+	"""
 
 
 
