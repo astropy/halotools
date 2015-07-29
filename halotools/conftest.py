c2a02ecdfcea51b825ed846af92e4e88e9f06855
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-28 23:49:30 -0400

    Attempted decorator solution to get pytest to work. Failed

--- halotools/conftest.py
+++ halotools/conftest.py
@@ -7,3 +7,26 @@ from astropy.tests.pytest_plugins import *
 ## Uncomment the following line to treat all DeprecationWarnings as
 ## exceptions
 # enable_deprecations_as_exceptions()
+
+
+def pytest_addoption_decorator(func):
+
+    def wrapper(parser):
+        baseline_behavior = func(parser)
+
+        parser.addoption('--slow', action='store_true', default=False,
+            help='Also run slow tests')
+
+    return wrapper
+pytest_addoption = pytest_addoption_decorator(pytest_addoption)
+
+# import pytest
+
+# def pytest_addoption(parser):
+#     parser.addoption('--slow', action='store_true', default=False,
+#                       help='Also run slow tests')
+
+# def pytest_runtest_setup(item):
+#     """Skip tests if they are marked as slow and --slow is not given"""
+#     if getattr(item.obj, 'slow', None) and not item.config.getvalue('slow'):
+#         pytest.skip('slow tests not requested')
