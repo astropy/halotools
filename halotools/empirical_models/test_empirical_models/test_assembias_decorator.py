44c952ebb5781a0b8a0f36a5ef8f7190709c8db2
Andrew Hearin <andrew.hearin@yale.edu>
2015-07-28 20:19:02 -0400

    Beefed up some of the assembias decorator test suite

--- halotools/empirical_models/test_empirical_models/test_assembias_decorator.py
+++ halotools/empirical_models/test_empirical_models/test_assembias_decorator.py
@@ -17,13 +17,47 @@ from ...utils.table_utils import SampleSelector
 class TestAssembiasDecorator(TestCase):
 
     def setup_class(self):
-    	pass
+    	Npts = 1e4
+    	mass = np.zeros(Npts) + 1e12
+    	zform = np.linspace(0, 10, Npts)
+    	d = {'halo_mvir': mass, 'halo_zform': zform}
+    	self.halo_table = Table(d)
 
     def test_initialization(self):
+    	"""
+    	"""
     	baseline_model = Zheng07Cens
+    	method_name_to_decorate='mean_occupation'
+
     	model = HeavisideAssembiasComponent(baseline_model=baseline_model, 
-    		method_name_to_decorate='mean_occupation', 
+    		method_name_to_decorate=method_name_to_decorate, 
     		lower_bound = 0, upper_bound = 1, 
     		)
+
+    	assert isinstance(model.baseline_model_instance, baseline_model)
+    	assert hasattr(model, '_split_abcissa')
+    	assert hasattr(model, '_split_ordinates')
+    	assert hasattr(model, '_assembias_strength_abcissa')
+    	assert hasattr(model, 'param_dict')
+    	param_key = method_name_to_decorate + '_assembias_param1'
+    	assert param_key in model.param_dict
+    	keys = [key for key in model.param_dict.keys() if method_name_to_decorate + '_assembias_param' in key]
+    	assert len(keys) == len(model._assembias_strength_abcissa)
+
+    	output_split = model.percentile_splitting_function(halo_table=self.halo_table)
+    	assert np.all(output_split == 0.5)
+
+    	model._split_ordinates = [0.75]
+    	output_split = model.percentile_splitting_function(halo_table=self.halo_table)
+    	assert np.all(output_split == 0.75)
+
+
+
+
+
+
+
+
+
 
 
