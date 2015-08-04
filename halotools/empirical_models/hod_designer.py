422a86c0bc9e41e7014847dd8b315340e1ddc938
Andrew Hearin <andrew.hearin@yale.edu>
2015-08-04 20:07:20 +0900

    fixed bug in hod designer

--- halotools/empirical_models/hod_designer.py
+++ halotools/empirical_models/hod_designer.py
@@ -10,28 +10,28 @@ from ..halotools_exceptions import HalotoolsError
 
 class HodModelArchitect(object):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	@staticmethod
-	def customize_model(*args, **kwargs):
+    @staticmethod
+    def customize_model(*args, **kwargs):
 
-		try:
-			baseline_model = kwargs['baseline_model']
-		except KeyError:
-			msg = ("\nThe customize_model method of HodModelArchitect "
-				"requires a baseline_model keyword argument\n")
-			raise HalotoolsError(msg)
-		baseline_blueprint = baseline_model.model_blueprint
+        try:
+            baseline_model = kwargs['baseline_model']
+        except KeyError:
+            msg = ("\nThe customize_model method of HodModelArchitect "
+                "requires a baseline_model keyword argument\n")
+            raise HalotoolsError(msg)
+        baseline_blueprint = baseline_model.model_blueprint
 
-		for component in args:
-			try:
-				gal_type = component.gal_type
-			except AttributeError:
-			msg = ("\nEvery argument of the customize_model method of HodModelArchitect "
-				"must be a model instance that has a ``gal_type`` attribute.\n")
-				raise HalotoolsError(msg)
-			
+        for component in args:
+            try:
+                gal_type = component.gal_type
+            except AttributeError:
+                msg = ("\nEvery argument of the customize_model method of HodModelArchitect "
+                    "must be a model instance that has a ``gal_type`` attribute.\n")
+                raise HalotoolsError(msg)
+
 
 
 
