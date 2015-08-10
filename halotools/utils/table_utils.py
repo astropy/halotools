7ca830c6797d8a5298c69d12c706d4b2f183c36a
Andrew Hearin <andrew.hearin@yale.edu>
2015-08-10 21:50:38 +0900

    Fixed bug in percentile calculator, though I still don't fully understand what was wrong with the original code. This commit introduces an error in the table_utils test suite, which is fine, because this suite wasn't catching the major bug anyway. Still need to revise test suite

--- halotools/utils/table_utils.py
+++ halotools/utils/table_utils.py
@@ -129,14 +129,16 @@ def compute_conditional_percentiles(**kwargs):
     for ibin in bins_in_halocat:
         indices_of_prim_haloprop_bin = np.where(prim_haloprop_bins == ibin)[0]
 
+        num_in_bin = len(sec_haloprop[indices_of_prim_haloprop_bin])
+
         # Find the indices that sort by the secondary property
         ind_sorted = np.argsort(sec_haloprop[indices_of_prim_haloprop_bin])
 
-        percentiles = (np.arange(len(sec_haloprop[indices_of_prim_haloprop_bin])) + 1.0) / \
-            float(len(sec_haloprop[indices_of_prim_haloprop_bin]))
+        percentiles = np.zeros(num_in_bin)
+        percentiles[ind_sorted] = (np.arange(num_in_bin) + 1.0) / float(num_in_bin)
         
         # place the percentiles into the catalog
-        output[indices_of_prim_haloprop_bin] = 1.0 - percentiles[ind_sorted][::-1]
+        output[indices_of_prim_haloprop_bin] = percentiles
 
     return output
 
