Purpose of commit is to demonstrate the single line of code responsible for the bug in the assembly bias testing. If I incorrectly remove the halo mass scaling, the assembly bias test suite passes. Not sure why this is yet.

halotools/empirical_models/hod_components.py
@@ -811,7 +811,7 @@ class Leauthaud11Sats(OccupationComponent):
 
         self._update_satellite_params()
 
-        mass *= self.littleh
+        # mass *= self.littleh
 
         mean_nsat = (
             np.exp(-self._mcut/mass)*