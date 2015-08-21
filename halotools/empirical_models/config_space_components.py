7f1d264fce01b0ad9b1f45384cb02a5161bdf3e1
Andrew Hearin <andrew.hearin@yale.edu>
2015-08-21 08:21:54 -0400

    Added new modules to contain the over-hauled class design for phase space modeling.

---
+++ halotools/empirical_models/config_space_components.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the components for 
+the intra-halo spatial positions of galaxies 
+within their halos. 
+"""
+
+__author__ = ['Andrew Hearin']
+
+
