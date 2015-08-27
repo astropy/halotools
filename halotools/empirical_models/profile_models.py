37dd830925ceedf5b5bf46e56b426c9266e2489e
Andrew Hearin <andrew.hearin@yale.edu>
2015-08-27 11:59:08 -0400

    Moved meta classes into the profile_models module

--- halotools/empirical_models/profile_models.py
+++ halotools/empirical_models/profile_models.py
@@ -6,7 +6,8 @@ inside their halos.
 """
 
 import numpy as np 
-from phase_space_metaclasses import *
+from astropy.extern import six 
+from abc import ABCMeta, abstractmethod
 from ..sim_manager import sim_defaults 
 from . import model_defaults
 from .conc_mass_models import ConcMass
@@ -18,6 +19,145 @@ from ..custom_exceptions import *
 __author__ = ['Andrew Hearin']
 
 __all__ = ['TrivialProfile', 'NFWProfile', 'BiasedNFWProfile']
+
+@six.add_metaclass(ABCMeta)
+class AnalyticDensityProf(object):
+    """ Container class for any radial profile model. 
+    """
+
+    def __init__(self, cosmology, redshift, mdef, **kwargs):
+        """
+        """
+        self.cosmology = cosmology
+        self.redshift = redshift
+        self.mdef = mdef
+
+        self.halo_boundary = model_defaults.halo_boundary_key(self.mdef)
+
+        self.density_threshold = density_threshold(
+            cosmology = self.cosmology, 
+            redshift = self.redshift, mdef = self.mdef)
+
+        self.prof_param_keys = []
+        self.publications = []
+        self.param_dict = {}
+
+
+    @abstractmethod
+    def mass_density(self, r, *args, **kwargs):
+        """
+        """
+        pass
+
+    def _enclosed_mass_integrand(self, y):
+        """
+        """
+        return self.mass_density(z) * 4.0 * np.pi * z**2
+
+    def enclosed_mass(self, x, rtol = 1E-5):
+        """
+        The mass enclosed within dimensionless radius :math:`x = r / R_{\\rm halo}`.
+
+        Parameters
+        -----------------
+        x: array_like
+            Halo-centric distance scaled by the halo boundary, such that :math:`0 < x < 1`. 
+            Can be a scalar or a numpy array.
+
+        rtol: float, optional
+            Relative tolerance of the integration accuracy. Default is 1e-5. 
+            
+        Returns
+        ----------
+        mass: array_like
+            The mass enclosed within radius r, in :math:`M_{\odot}/h`; 
+            has the same dimensions as the input ``x``.
+        """                
+        x = convert_to_ndarray(x)
+        mass = np.zeros_like(x)
+        for i in range(len(x)):
+            mass[i], _ = quad_integration(self._enclosed_mass_integrand, 0., x[i], epsrel = rtol)
+    
+        return mass
+
+    def cumulative_mass_PDF(self, x, rtol = 1E-5):
+        """
+        The fraction of the total mass enclosed within 
+        dimensionless radius :math:`x = r / R_{\\rm halo}`.
+
+        Parameters
+        -------------
+        x: array_like
+            Halo-centric distance scaled by the halo boundary, such that :math:`0 < x < 1`. 
+            Can be a scalar or a numpy array.
+
+        rtol: float, optional
+            Relative tolerance of the integration accuracy. Default is 1e-5. 
+            
+        Returns
+        -------------
+        p: array_like
+            The fraction of the total mass enclosed 
+            within radius x, in :math:`M_{\odot}/h`; 
+            has the same dimensions as the input ``x``.
+        """     
+        p = self.enclosed_mass(x)/self.enclosed_mass(1.0)
+        return p
+
+    def circular_velocity(self, x):
+        """
+        The circular velocity, :math:`v_c \\equiv \\sqrt{GM(<r)/r}`.
+
+        Parameters
+        --------------
+        x: array_like
+            Halo-centric distance scaled by the halo boundary, such that :math:`0 < x < 1`. 
+            Can be a scalar or a numpy array.
+            
+        Returns
+        ----------
+        vc: float
+            The circular velocity in km / s; has the same dimensions as r.
+
+        See also
+        ------------
+        Vmax: The maximum circular velocity, and the radius where it occurs.
+        """     
+    
+        M = self.enclosed_mass(x)
+        v = numpy.sqrt(newtonG.value * M / x)
+        
+        return v
+
+    def gravitational_potential_radial_gradient(self, x):
+        """
+        """
+        return newtonG.value * self.enclosed_mass(x) / x**2
+
+
+    ###############################################################################################
+
+    def vmax(self):
+        """
+        The maximum circular velocity, and the radius where it occurs.
+            
+        Returns
+        -----------------
+        vmax: float
+            The maximum circular velocity in km / s.
+        rmax: float
+            The radius where fmax occurs, in physical kpc/h.
+
+        """     
+        def _circular_velocity_negative(x):
+            return -self.circular_velocity(x)
+
+        x_guess = 0.1
+        res = scipy_minimize(_circular_velocity_negative, x_guess)
+        rmax = res.x[0]
+        vmax = self.circular_velocity(rmax)
+        
+        return vmax, rmax
 
 class TrivialProfile(AnalyticDensityProf):
     """ Profile of dark matter halos with all their mass concentrated at exactly the halo center. 
@@ -264,6 +404,22 @@ class BiasedNFWProfile(NFWProfile):
 
 
 ##################################################################################
+
+@six.add_metaclass(ABCMeta)
+class IsotropicJeansVelocity(object):
+    """ Orthogonal mixin class used to transform a configuration 
+    space model for the 1-halo term into a phase space model in which 
+    velocities solve the Jeans equation of the underlying potential. 
+    """
+    def __init__(self, **kwargs):
+        pass
+
+    def _unscaled_radial_velocity_dispersion(self, x):
+        """
+        Method returns the radial velocity dispersion as a function of the 
+        halo-centric distance. 
+        """
+        pass
 
 class NFWPhaseSpace(NFWProfile, IsotropicJeansVelocity):
     """ NFW halo profile, based on Navarro, Frenk and White (1999).
