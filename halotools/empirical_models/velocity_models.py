522a527d36cbcaca1897b2bc7d0044e9b4519667
Andrew Hearin <andrew.hearin@yale.edu>
2015-08-30 08:53:26 -0400

    Starting to develop class structure for jeans solutions

--- halotools/empirical_models/velocity_models.py
+++ halotools/empirical_models/velocity_models.py
@@ -7,4 +7,20 @@ relative to their halos.
 
 __author__ = ['Andrew Hearin']
 
-	
+__all__ = ['IsotropicJeansVelocity']
+
+@six.add_metaclass(ABCMeta)
+class IsotropicJeansVelocity(object):
+    """ Orthogonal mixin class used to transform a configuration 
+    space model for the 1-halo term into a phase space model  
+    by solving the Jeans equation of the underlying potential. 
+    """
+    def __init__(self, **kwargs):
+        pass
+
+    def _unscaled_radial_velocity_dispersion(self, x):
+        """
+        Method returns the radial velocity dispersion as a function of the 
+        halo-centric distance. 
+        """
+        pass
