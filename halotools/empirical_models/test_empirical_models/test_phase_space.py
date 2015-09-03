b6b4785609b576f50d34d29892193c9c99f808fb
Andrew Hearin <andrew.hearin@yale.edu>
2015-09-03 11:02:26 -0400

    Added test of the constructor class to ensure that the complex inheritance scheme of NFWPhaseSpace has been handled correctly

--- halotools/empirical_models/test_empirical_models/test_phase_space.py
+++ halotools/empirical_models/test_empirical_models/test_phase_space.py
@@ -7,22 +7,56 @@ from astropy.table import Table
 from ...sim_manager import HaloCatalog
 from ..phase_space_models import NFWPhaseSpace
 
-class TestAssembias(TestCase):
-    """
+__all__ = ['TestNFWPhaseSpace']
+
+class TestNFWPhaseSpace(TestCase):
+    """ Class used to test `~halotools.empirical_models.NFWPhaseSpace`. 
     """
 
     def setup_class(self):
+        """ Load the NFW model and build a coarse lookup table.
         """
-        """
-        self.halocat = HaloCatalog()
+        # self.halocat = HaloCatalog()
 
         self.nfw = NFWPhaseSpace()
-        cmin, cmax, dc = 1, 15, 15
+        cmin, cmax, dc = 1, 15, 25
         self.nfw._setup_lookup_tables((cmin, cmax, dc))
+        self.nfw.build_lookup_tables()
 
-    def test_nfw_phase_space(self):
-        pass
-        
+    def test_constructor(self):
+        """
+        """
+        ### MonteCarloGalProf attributes
+        assert hasattr(self.nfw, 'logradius_array')
+        assert hasattr(self.nfw, 'rad_prof_func_table')
+        assert hasattr(self.nfw, 'vel_prof_func_table')
+        assert hasattr(self.nfw, '_mc_dimensionless_radial_distance')
+
+        ### NFWPhaseSpace attributes
+        assert hasattr(self.nfw, 'assign_phase_space')
+        assert hasattr(self.nfw, 'column_keys_to_allocate')
+
+        ### AnalyticDensityProf attributes 
+        assert hasattr(self.nfw, 'circular_velocity')
+
+        ### NFWProfile attributes
+        assert hasattr(self.nfw, 'mass_density')
+        assert hasattr(self.nfw, 'prof_param_keys')
+
+        ### ConcMass
+        assert hasattr(self.nfw, 'conc_NFWmodel')
+        assert hasattr(self.nfw, 'conc_mass_model')
+
+
+
+
+    def test_mc_unit_sphere(self):
+        """ Method used to test `~halotools.empirical_models.NFWPhaseSpace.mc_unit_sphere`. 
+        """
+        x, y, z = self.nfw.mc_unit_sphere(100)
+        pos = np.vstack([x, y, z]).T 
+        norm = np.linalg.norm(pos, axis=1)
+        assert np.allclose(norm, 1, rtol=1e-4)
 
 
 
