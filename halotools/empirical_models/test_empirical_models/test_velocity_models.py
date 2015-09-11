e5c4e833c2a40efe3cec5125e0396145fa34b9d1
Andrew Hearin <andrew.hearin@yale.edu>
2015-09-11 09:58:58 -0400

    Added trivial module to test the velocity modules

---
+++ halotools/empirical_models/test_empirical_models/test_velocity_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import pytest
+from unittest import TestCase
+
+import numpy as np 
+from astropy.table import Table 
+from ...sim_manager import HaloCatalog
+from ..phase_space_models import NFWPhaseSpace
+from ...custom_exceptions import HalotoolsError
+from ..velocity_models import NFWJeansVelocity
+
+class TestNFWJeansVelocity(TestCase):
+    """ Class used to test `~halotools.empirical_models.NFWPhaseSpace`. 
+    """
+
+    def setup_class(self):
+        """ Load the NFW model and build a coarse lookup table.
+        """
+        pass
+
+    def test_velocity_dispersion(self):
+        """
+        """
+        pass
