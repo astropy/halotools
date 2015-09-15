77f2cb439402f1fe24a1fb480be11b9aa75cebdb
Andrew Hearin <andrew.hearin@yale.edu>
2015-09-15 08:53:51 -0400

    Mostly done implementing Tinker13Cens. Still need to decide how to implement quiescent_central_fraction method

--- halotools/empirical_models/tinker13.py
+++ halotools/empirical_models/tinker13.py
@@ -25,17 +25,17 @@ from .assembias import HeavisideAssembias
 from ..utils.array_utils import custom_len
 from ..utils.table_utils import compute_conditional_percentiles
 from ..  import sim_manager
-from ..custom_exceptions import HalotoolsModelInputError
+from ..custom_exceptions import *
 
 from astropy.extern import six
 from abc import ABCMeta, abstractmethod, abstractproperty
 import warnings
 
-from .hod_components import *
+from .hod_components import OccupationComponent
 
 class Tinker13Cens(OccupationComponent):
     """ HOD-style model for a central galaxy occupation that derives from 
-    two distinct active/passive stellar-to-halo-mass relations. 
+    two distinct active/quiescent stellar-to-halo-mass relations. 
     """
     def __init__(self, threshold = model_defaults.default_stellar_mass_threshold, 
         prim_haloprop_key=model_defaults.prim_haloprop_key,
@@ -73,22 +73,118 @@ class Tinker13Cens(OccupationComponent):
 
         for key, value in self.smhm_model.param_dict.iteritems():
             active_key = key + '_active'
-            passive_key = key + '_passive'
+            quiescent_key = key + '_quiescent'
             self.param_dict[active_key] = value
-            self.param_dict[passive_key] = value
+            self.param_dict[quiescent_key] = value
+
+        self.sfr_designation_key = 'sfr_designation'
 
         self.publications = ['arXiv:1308.2974', 'arXiv:1103.2077', 'arXiv:1104.0928']
 
+        # The _methods_to_inherit determines which methods will be directly callable 
+        # by the composite model built by the HodModelFactory
+        # Here we are overriding this attribute that is normally defined 
+        # in the OccupationComponent super class
+        self._methods_to_inherit = (
+            ['mc_occupation', 'mean_occupation', 'mean_occupation_active', 'mean_occupation_quiescent', 
+            'mean_stellar_mass_active', 'mean_stellar_mass_quiescent', 
+            'mean_log_halo_mass_active', 'mean_log_halo_mass_quiescent']
+            )
 
-    def passive_central_fraction(self, **kwargs):
+        # The _mock_generation_calling_sequence determines which methods 
+        # will be called during mock population, as well as in what order they will be called
+        self._mock_generation_calling_sequence = ['mc_sfr_designation', 'mc_occupation']
+        self._galprop_dtypes_to_allocate = np.dtype([
+            ('halo_num_'+ self.gal_type, 'i4'), 
+            ('central_sfr_designation', object), 
+            ])
+
+    def quiescent_central_fraction(self, **kwargs):
         """
         """
         pass
+
+    def mc_sfr_designation(self, **kwargs):
+        """
+        """
+        quiescent_fraction = self.quiescent_central_fraction(**kwargs)
+
+        if 'seed' in kwargs:
+            np.random.seed(seed=kwargs['seed'])
+        mc_generator = np.random.random(custom_len(quiescent_fraction))
+
+        result = np.where(mc_generator < quiescent_fraction, 'quiescent', 'active')
+        if 'halo_table' in kwargs:
+            kwargs['halo_table'][central_sfr_designation] = result
+        return result
 
     def mean_occupation(self, **kwargs):
+        """
+        """
+        if 'halo_table' in kwargs:
+            halo_table = kwargs['halo_table']
+            try:
+                prim_haloprop = halo_table[self.prim_haloprop_key]
+            except KeyError:
+                msg = ("The ``halo_table`` passed as a keyword argument to the mean_occupation method\n"
+                    "does not have the requested ``%s`` key")
+                raise HalotoolsError(msg % self.prim_haloprop_key)
+            try:
+                sfr_designation = halo_table[self.sfr_designation_key]
+            except KeyError:
+                msg = ("The ``halo_table`` passed as a keyword argument to the mean_occupation method\n"
+                    "does not have the requested ``%s`` key")
+                raise HalotoolsError(msg % self.sfr_designation_key)
+        else:
+            try:
+                prim_haloprop = kwargs['prim_haloprop']
+                sfr_designation = kwargs['sfr_designation']
+            except KeyError:
+                msg = ("If not passing a ``halo_table`` keyword argument to the mean_occupation method,\n"
+                    "you must pass both ``prim_haloprop`` and ``sfr_designation`` keyword arguments")
+
+        result = np.zeros(custom_len(prim_haloprop))
+
+        quiescent_central_idx = np.where(sfr_designation == 'quiescent')[0]
+        result[quiescent_central_idx] = self.mean_occupation_quiescent(
+            prim_haloprop = prim_haloprop[quiescent_central_idx])        
+
+        active_central_idx = np.invert(quiescent_central_idx)
+        result[active_central_idx] = self.mean_occupation_active(
+            prim_haloprop = prim_haloprop[active_central_idx])        
+
+        return result
+
+
+    def mean_occupation_active(self, **kwargs):
         """ 
         """
-        pass
+        self._update_smhm_param_dict('active')
+
+        logmstar = np.log10(self.smhm_model.mean_stellar_mass(
+            redshift = self.redshift, **kwargs))
+        logscatter = math.sqrt(2)*self.smhm_model.mean_scatter(**kwargs)
+
+        mean_ncen = 0.5*(1.0 - 
+            erf((self.threshold - logmstar)/logscatter))
+        mean_ncen *= (1. - self.quiescent_central_fraction(**kwargs))
+
+        return mean_ncen
+
+    def mean_occupation_quiescent(self, **kwargs):
+        """ 
+        """
+        self._update_smhm_param_dict('quiescent')
+
+        logmstar = np.log10(self.smhm_model.mean_stellar_mass(
+            redshift = self.redshift, **kwargs))
+        logscatter = math.sqrt(2)*self.smhm_model.mean_scatter(**kwargs)
+
+        mean_ncen = 0.5*(1.0 - 
+            erf((self.threshold - logmstar)/logscatter))
+        mean_ncen *= self.quiescent_central_fraction(**kwargs)
+
+        return mean_ncen
 
     def mean_stellar_mass_active(self, **kwargs):
         """ 
