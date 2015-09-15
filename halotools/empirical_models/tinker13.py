f7678a0d470071d85a79eed452cce2155ca8b01b
Andrew Hearin <andrew.hearin@yale.edu>
2015-09-15 13:50:30 -0400

    Added commented-out Tinker13Sats

--- halotools/empirical_models/tinker13.py
+++ halotools/empirical_models/tinker13.py
@@ -262,6 +262,51 @@ class Tinker13Cens(OccupationComponent):
 
 
 
+# class Tinker13Sats(OccupationComponent):
+#     """ HOD-style model for a central galaxy occupation that derives from 
+#     two distinct active/quiescent stellar-to-halo-mass relations. 
+#     """
+#     def __init__(self, **kwargs):
+#         """
+#         """
+#         upper_bound = float("inf")
+
+#         # Call the super class constructor, which binds all the 
+#         # arguments to the instance.  
+#         super(Tinker13Sats, self).__init__(
+#             gal_type='satellites', threshold=threshold, 
+#             upper_bound=upper_bound, 
+#             prim_haloprop_key = prim_haloprop_key, **kwargs)
+#         self.redshift = redshift
+
+#         self.smhm_model = smhm_components.Behroozi10SmHm(
+#             prim_haloprop_key = prim_haloprop_key, **kwargs)
+
+#         self._initialize_param_dict(**kwargs)
+
+#         self.sfr_designation_key = 'sfr_designation'
+
+#         self.publications = ['arXiv:1308.2974', 'arXiv:1103.2077', 'arXiv:1104.0928']
+
+#         # The _methods_to_inherit determines which methods will be directly callable 
+#         # by the composite model built by the HodModelFactory
+#         # Here we are overriding this attribute that is normally defined 
+#         # in the OccupationComponent super class
+#         self._methods_to_inherit = (
+#             ['mc_occupation', 'mean_occupation', 'mean_occupation_active', 'mean_occupation_quiescent', 
+#             'mean_stellar_mass_active', 'mean_stellar_mass_quiescent', 
+#             'mean_log_halo_mass_active', 'mean_log_halo_mass_quiescent']
+#             )
+
+#         # The _mock_generation_calling_sequence determines which methods 
+#         # will be called during mock population, as well as in what order they will be called
+#         self._mock_generation_calling_sequence = ['mc_sfr_designation', 'mc_occupation']
+#         self._galprop_dtypes_to_allocate = np.dtype([
+#             ('halo_num_'+ self.gal_type, 'i4'), 
+#             ('central_sfr_designation', object), 
+#             ])
+
+
 
 
 
