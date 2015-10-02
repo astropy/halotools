a94e22d949b53fc16ff74cc3bafb382f1830b8a5
Andrew Hearin <andrew.hearin@yale.edu>
2015-10-02 11:30:50 -0400

    Added stand-alone convenience function mc_generate_phase_space_points to NFWPhaseSpace.

--- halotools/empirical_models/phase_space_models.py
+++ halotools/empirical_models/phase_space_models.py
@@ -15,6 +15,8 @@ from .monte_carlo_phase_space import *
 from . import model_defaults
 from ..sim_manager import sim_defaults
 
+from astropy.table import Table
+
 class NFWPhaseSpace(NFWProfile, NFWJeansVelocity, MonteCarloGalProf):
     """ NFW halo profile, based on Navarro, Frenk and White (1999).
 
@@ -84,6 +86,70 @@ class NFWPhaseSpace(NFWProfile, NFWJeansVelocity, MonteCarloGalProf):
         """
         self.mc_pos(halo_table = halo_table)
         self.mc_vel(halo_table = halo_table)
+
+
+    def mc_generate_phase_space_points(self, Ngals = 1e4, conc=5, mass = 1e12):
+        """ Stand-alone convenience function for returning a Monte Carlo 
+        realization of NFW phase space.
+
+        Parameters 
+        -----------
+        Ngals : int, optional 
+            Number of galaxies in the Monte Carlo realization of the 
+            phase space distribution. Default is 1e4. 
+
+        conc : float, optional 
+            Concentration of the NFW profile being realized. 
+            Default is 5.
+
+        mass : float, optional 
+            Mass of the halo whose phase space distribution is being realized. 
+            Default is 1e12. 
+
+        Returns 
+        --------
+        t : table 
+            `~astropy.table.Table` containing the Monte Carlo realization of the 
+            phase space distribution. 
+            Keys are 'x', 'y', 'z', 'vx', 'vy', 'vz', 'radial_position', 'radial_velocity'.
+
+        Examples 
+        ---------
+        >>> nfw = NFWPhaseSpace()
+        >>> data = nfw.mc_generate_phase_space_points(Ngals = 1e2) # doctest: +SKIP
+        >>> data = nfw.mc_generate_phase_space_points(Ngals = 1e3, mass = 1e15) # doctest: +SKIP
+
+        """
+
+        m = np.zeros(Ngals) + mass
+        c = np.zeros(Ngals) + conc
+        vvir = self.virial_velocity(total_mass = m)
+        rvir = self.halo_mass_to_halo_radius(total_mass = m)
+
+        x, y, z = self.mc_halo_centric_pos(
+            profile_params = [c], halo_radius = rvir)
+        r = np.sqrt(x**2 + y**2 + z**2)
+
+        vrad = self.mc_radial_velocity(x = r/rvir, 
+            virial_velocities = vvir, profile_params = [c])
+        vx = self.mc_radial_velocity(x = r/rvir, 
+            virial_velocities = vvir, profile_params = [c])
+        vy = self.mc_radial_velocity(x = r/rvir, 
+            virial_velocities = vvir, profile_params = [c])
+        vz = self.mc_radial_velocity(x = r/rvir, 
+            virial_velocities = vvir, profile_params = [c])
+
+        t = Table()
+        t['x'] = x 
+        t['y'] = y
+        t['z'] = z 
+        t['vx'] = vx 
+        t['vy'] = vy
+        t['vz'] = vz 
+
+        t['radial_position'] = r 
+        t['radial_velocity'] = vrad 
+        return t
 
 
 class TrivialPhaseSpace(object):
