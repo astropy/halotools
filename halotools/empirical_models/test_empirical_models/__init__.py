aa9e16ecaf96dd07005df4906b4b39c87ee01358
Andrew Hearin <andrew.hearin@yale.edu>
2015-10-19 13:21:09 -0400

    fixed import problem in test_conc_mass

--- halotools/empirical_models/test_empirical_models/__init__.py
+++ halotools/empirical_models/test_empirical_models/__init__.py
@@ -3,7 +3,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from .test_hod_components import *
-from .test_conc_mass import *
 from .test_profile_models import *
 from .test_mock_factories import *
 from .test_hod_factory import *
