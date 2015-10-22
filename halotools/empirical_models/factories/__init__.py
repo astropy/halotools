0b6447475f12c1fdbbc8ba04c8509eb64d144b2e
Andrew Hearin <andrew.hearin@yale.edu>
2015-10-21 20:37:22 -0400

    Moved factories into dedicated sub-package

---
+++ halotools/empirical_models/factories/__init__.py
@@ -0,0 +1,7 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+
+from .mock_factories import *
+from .model_factories import *
