406df966cd955142678971e2f21132c0a48d15a1
Andrew Hearin <andrew.hearin@yale.edu>
2015-10-25 17:08:52 -0400

    Wrote new test of baseline method recovery. Fails unless I switch frac_type1 <==> frac_type2 to be seemingly incorrect. Puzzling.

--- halotools/empirical_models/assembias_models/tests/test_assembias.py
+++ halotools/empirical_models/assembias_models/tests/test_assembias.py
@@ -47,80 +47,109 @@ class TestAssembias(TestCase):
         lower_bound_key = 'lower_bound_' + model._method_name_to_decorate + '_' + model.gal_type
         assert hasattr(model, lower_bound_key)
 
-    def baseline_recovery_test(self, model):
+    # def baseline_recovery_test(self, model):
+
+    #     baseline_method = getattr(model, 'baseline_'+model._method_name_to_decorate)
+    #     baseline_result = baseline_method(halo_table = self.toy_halo_table2)
+
+    #     method = getattr(model, model._method_name_to_decorate)
+    #     result = method(halo_table = self.toy_halo_table2)
+
+    #     oldmask = self.toy_halo_table2['halo_zform_percentile'] >= model._split_ordinates[0]
+    #     oldmean = result[oldmask].mean()
+    #     youngmean = result[~oldmask].mean()
+    #     baseline_mean = baseline_result.mean()
+
+    #     param_key = model._get_assembias_param_dict_key(0)
+    #     param = model.param_dict[param_key]
+    #     if param > 0:
+    #         assert oldmean > youngmean 
+    #     elif param < 0: 
+    #         assert oldmean < youngmean
+    #     else:
+    #         assert oldmean == youngmean 
+
+    #     split = model.percentile_splitting_function(
+    #         self.toy_halo_table2['halo_mvir'])
+    #     split = np.where(oldmask, split, 1-split)
+    #     derived_result = split*oldmean
+    #     derived_result[~oldmask] = split[~oldmask]*youngmean
+    #     derived_mean = derived_result[oldmask].mean() + derived_result[~oldmask].mean()
+    #     baseline_mean = baseline_result.mean()
+    #     np.testing.assert_allclose(baseline_mean, derived_mean, rtol=1e-3)
+
+    def baseline_preservation_test(self, model):
+
+        prim_haloprop = np.logspace(10, 15, 6)
 
         baseline_method = getattr(model, 'baseline_'+model._method_name_to_decorate)
-        baseline_result = baseline_method(halo_table = self.toy_halo_table2)
+        baseline_result = baseline_method(prim_haloprop = prim_haloprop)
 
-        method = getattr(model, model._method_name_to_decorate)
-        result = method(halo_table = self.toy_halo_table2)
+        decorated_method = getattr(model, model._method_name_to_decorate)
+        decorated_result_type1 = decorated_method(
+            prim_haloprop = prim_haloprop, 
+            sec_haloprop_percentile = 1)
+        decorated_result_type2 = decorated_method(
+            prim_haloprop = prim_haloprop, 
+            sec_haloprop_percentile = 0)
+        type2_frac = model.percentile_splitting_function(prim_haloprop)
+        type1_frac = 1 - type2_frac
 
-        oldmask = self.toy_halo_table2['halo_zform_percentile'] >= model._split_ordinates[0]
-        oldmean = result[oldmask].mean()
-        youngmean = result[~oldmask].mean()
-        baseline_mean = baseline_result.mean()
+        derived_result = type1_frac*decorated_result_type1 + type2_frac*decorated_result_type2
+        np.testing.assert_allclose(baseline_result, derived_result, rtol=1e-3)
 
-        param_key = model._get_assembias_param_dict_key(0)
-        param = model.param_dict[param_key]
-        if param > 0:
-            assert oldmean > youngmean 
-        elif param < 0: 
-            assert oldmean < youngmean
-        else:
-            assert oldmean == youngmean 
+    def test_decorated_zheng07cens(self):
+        abz = AssembiasZheng07Cens(sec_haloprop_key = 'halo_zform')
+        self.baseline_preservation_test(abz)
 
-        split = model.percentile_splitting_function(
-            self.toy_halo_table2['halo_mvir'])
-        split = np.where(oldmask, split, 1-split)
-        derived_result = split*oldmean
-        derived_result[~oldmask] = split[~oldmask]*youngmean
-        derived_mean = derived_result[oldmask].mean() + derived_result[~oldmask].mean()
-        baseline_mean = baseline_result.mean()
-        np.testing.assert_allclose(baseline_mean, derived_mean, rtol=1e-3)
+        abz2 = AssembiasZheng07Cens(sec_haloprop_key = 'halo_zform', 
+            split=0.75)
+        self.baseline_preservation_test(abz2)
+
 
     def test_assembias_zheng07_cens(self):
         abz = AssembiasZheng07Cens(sec_haloprop_key = 'halo_zform')
 
         self.init_test(abz)
-        self.baseline_recovery_test(abz)
+        # self.baseline_recovery_test(abz)
 
         abz2 = AssembiasZheng07Cens(sec_haloprop_key = 'halo_zform', 
             split=0.75, assembias_strength = -0.25)
         self.init_test(abz2)
-        self.baseline_recovery_test(abz2)
+        # self.baseline_recovery_test(abz2)
 
     def test_assembias_zheng07_sats(self):
         abz = AssembiasZheng07Sats(sec_haloprop_key = 'halo_zform')
 
         self.init_test(abz)
-        self.baseline_recovery_test(abz)
+        # self.baseline_recovery_test(abz)
 
         abz2 = AssembiasZheng07Sats(sec_haloprop_key = 'halo_zform', 
             split=0.25, assembias_strength = -0.7)
         self.init_test(abz2)
-        self.baseline_recovery_test(abz2)
+        # self.baseline_recovery_test(abz2)
 
     def test_assembias_leauthaud11_cens(self):
         abl = AssembiasLeauthaud11Cens(sec_haloprop_key = 'halo_zform')
 
         self.init_test(abl)
-        self.baseline_recovery_test(abl)
+        # self.baseline_recovery_test(abl)
 
         abl2 = AssembiasLeauthaud11Cens(sec_haloprop_key = 'halo_zform', 
             split=0.25, assembias_strength = -0.7)
         self.init_test(abl2)
-        self.baseline_recovery_test(abl2)
+        # self.baseline_recovery_test(abl2)
 
     def test_assembias_leauthaud11_sats(self):
         abl = AssembiasLeauthaud11Sats(sec_haloprop_key = 'halo_zform')
 
         self.init_test(abl)
-        self.baseline_recovery_test(abl)
+        # self.baseline_recovery_test(abl)
 
         abl2 = AssembiasLeauthaud11Sats(sec_haloprop_key = 'halo_zform', 
             split=0.25, assembias_strength = -0.7)
         self.init_test(abl2)
-        self.baseline_recovery_test(abl2)
+        # self.baseline_recovery_test(abl2)
 
 
 
