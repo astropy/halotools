Fixed test suite sign problems. HeavisideAssembias is now cleanly debugged. Still need to implement mock population integration testing

halotools/empirical_models/assembias_models/tests/test_assembias.py
@@ -37,6 +37,11 @@ class TestAssembias(TestCase):
         fakesim = FakeSim()
         self.fake_halo_table = fakesim.halo_table
 
+        self.model_class_list = (AssembiasZheng07Cens, AssembiasZheng07Sats, 
+            AssembiasLeauthaud11Cens, AssembiasLeauthaud11Sats)
+
+        self.prim_haloprop = np.logspace(10, 15, 6)
+
     def init_test(self, model):
 
         assert hasattr(model, 'prim_haloprop_key')
@@ -47,36 +52,23 @@ class TestAssembias(TestCase):
         lower_bound_key = 'lower_bound_' + model._method_name_to_decorate + '_' + model.gal_type
         assert hasattr(model, lower_bound_key)
 
-    # def baseline_recovery_test(self, model):
+    def assembias_sign_effect(self, model):
 
-    #     baseline_method = getattr(model, 'baseline_'+model._method_name_to_decorate)
-    #     baseline_result = baseline_method(halo_table = self.toy_halo_table2)
+        decorated_method = getattr(model, model._method_name_to_decorate)
+        decorated_result_type1 = decorated_method(
+            prim_haloprop = self.prim_haloprop, 
+            sec_haloprop_percentile = 0)
+        decorated_result_type2 = decorated_method(
+            prim_haloprop = self.prim_haloprop, 
+            sec_haloprop_percentile = 1)
 
-    #     method = getattr(model, model._method_name_to_decorate)
-    #     result = method(halo_table = self.toy_halo_table2)
-
-    #     oldmask = self.toy_halo_table2['halo_zform_percentile'] >= model._split_ordinates[0]
-    #     oldmean = result[oldmask].mean()
-    #     youngmean = result[~oldmask].mean()
-    #     baseline_mean = baseline_result.mean()
-
-    #     param_key = model._get_assembias_param_dict_key(0)
-    #     param = model.param_dict[param_key]
-    #     if param > 0:
-    #         assert oldmean > youngmean 
-    #     elif param < 0: 
-    #         assert oldmean < youngmean
-    #     else:
-    #         assert oldmean == youngmean 
-
-    #     split = model.percentile_splitting_function(
-    #         self.toy_halo_table2['halo_mvir'])
-    #     split = np.where(oldmask, split, 1-split)
-    #     derived_result = split*oldmean
-    #     derived_result[~oldmask] = split[~oldmask]*youngmean
-    #     derived_mean = derived_result[oldmask].mean() + derived_result[~oldmask].mean()
-    #     baseline_mean = baseline_result.mean()
-    #     np.testing.assert_allclose(baseline_mean, derived_mean, rtol=1e-3)
+        assembias_sign = model.assembias_strength(self.prim_haloprop)
+        positive_assembias = assembias_sign > 0
+        negative_assembias = assembias_sign < 0
+        diff = decorated_result_type1 - decorated_result_type2
+        assert np.all(diff[positive_assembias] <= 0)            
+        assert np.all(diff[negative_assembias] >= 0)
+        assert np.any(diff != 0)
 
     def baseline_preservation_test(self, model):
 
@@ -88,67 +80,32 @@ class TestAssembias(TestCase):
         decorated_method = getattr(model, model._method_name_to_decorate)
         decorated_result_type1 = decorated_method(
             prim_haloprop = prim_haloprop, 
-            sec_haloprop_percentile = 1)
+            sec_haloprop_percentile = 0)
         decorated_result_type2 = decorated_method(
             prim_haloprop = prim_haloprop, 
-            sec_haloprop_percentile = 0)
+            sec_haloprop_percentile = 1)
         type2_frac = model.percentile_splitting_function(prim_haloprop)
         type1_frac = 1 - type2_frac
 
         derived_result = type1_frac*decorated_result_type1 + type2_frac*decorated_result_type2
         np.testing.assert_allclose(baseline_result, derived_result, rtol=1e-3)
 
-    def test_decorated_zheng07cens(self):
-        abz = AssembiasZheng07Cens(sec_haloprop_key = 'halo_zform')
-        self.baseline_preservation_test(abz)
+    def model_variation_generator(self, model_class):
+        yield model_class()
+        yield model_class(split=0.75)
+        yield model_class(split=0.25, assembias_strength = -0.5)
+        yield model_class(split_abcissa = [1e10, 1e15], 
+            split = [-0.25, 1.75], 
+            assembias_strength_abcissa = [1e10, 1e13, 1e15],
+            assembias_strength = [-1.25, 0.25, -5.75])
 
-        abz2 = AssembiasZheng07Cens(sec_haloprop_key = 'halo_zform', split=0.75)
-        self.baseline_preservation_test(abz2)
+    def test_preloaded_assembiased_occupation_models(self):
 
-
-    def test_assembias_zheng07_cens(self):
-        abz = AssembiasZheng07Cens(sec_haloprop_key = 'halo_zform')
-
-        self.init_test(abz)
-        # self.baseline_recovery_test(abz)
-
-        abz2 = AssembiasZheng07Cens(sec_haloprop_key = 'halo_zform', 
-            split=0.75, assembias_strength = -0.25)
-        self.init_test(abz2)
-        # self.baseline_recovery_test(abz2)
-
-    def test_assembias_zheng07_sats(self):
-        abz = AssembiasZheng07Sats(sec_haloprop_key = 'halo_zform')
-
-        self.init_test(abz)
-        # self.baseline_recovery_test(abz)
-
-        abz2 = AssembiasZheng07Sats(sec_haloprop_key = 'halo_zform', 
-            split=0.25, assembias_strength = -0.7)
-        self.init_test(abz2)
-        # self.baseline_recovery_test(abz2)
-
-    def test_assembias_leauthaud11_cens(self):
-        abl = AssembiasLeauthaud11Cens(sec_haloprop_key = 'halo_zform')
-
-        self.init_test(abl)
-        # self.baseline_recovery_test(abl)
-
-        abl2 = AssembiasLeauthaud11Cens(sec_haloprop_key = 'halo_zform', 
-            split=0.25, assembias_strength = -0.7)
-        self.init_test(abl2)
-        # self.baseline_recovery_test(abl2)
-
-    def test_assembias_leauthaud11_sats(self):
-        abl = AssembiasLeauthaud11Sats(sec_haloprop_key = 'halo_zform')
-
-        self.init_test(abl)
-        # self.baseline_recovery_test(abl)
-
-        abl2 = AssembiasLeauthaud11Sats(sec_haloprop_key = 'halo_zform', 
-            split=0.25, assembias_strength = -0.7)
-        self.init_test(abl2)
-        # self.baseline_recovery_test(abl2)
+        for model_class in self.model_class_list:
+            for model in self.model_variation_generator(model_class):
+                self.init_test(model)
+                self.assembias_sign_effect(model)
+                self.baseline_preservation_test(model)
 
 
 