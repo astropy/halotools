7eca736a7b9cde1d8de20a0497dab1f10265001b
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-10-26 16:20:19 -0400

    updated test for tpcf_jackknife

--- halotools/mock_observables/test_clustering/test_tpcf_jackknife.py
+++ halotools/mock_observables/test_clustering/test_tpcf_jackknife.py
@@ -17,11 +17,11 @@ def test_tpcf_jackknife():
     Npts=100
     sample1 = np.random.random((Npts,3))
     randoms = np.random.random((Npts*10,3))
-    period = np.array([1,1,1])
-    Lbox = np.array([1,1,1])
-    rbins = np.linspace(0.0,0.1,5)
+    period = np.array([1.0,1.0,1.0])
+    Lbox = np.array([1.0,1.0,1.0])
+    rbins = np.linspace(0.0,0.3,5).astype(float)
     
-    result_1,err = tpcf_jackknife(sample1, randoms, rbins, Nsub=5, Lbox=Lbox, period = period, num_threads=1)
+    result_1,err = tpcf_jackknife(sample1, randoms, rbins, Nsub=5, period = period, num_threads=1)
     result_2 = tpcf(sample1, rbins, randoms=randoms, period = period, num_threads=1)
     
     print(result_1)
@@ -36,12 +36,12 @@ def test_tpcf_jackknife_cov_matrix():
     Npts=100
     sample1 = np.random.random((Npts,3))
     randoms = np.random.random((Npts*10,3))
-    period = np.array([1,1,1])
-    Lbox = np.array([1,1,1])
-    rbins = np.linspace(0.0,0.1,5)
+    period = np.array([1.0,1.0,1.0])
+    Lbox = np.array([1.0,1.0,1.0])
+    rbins = np.linspace(0.0,0.3,5)
     nbins = len(rbins)-1
     
-    result_1,err = tpcf_jackknife(sample1, randoms, rbins, Nsub=5, Lbox=Lbox, period = period, num_threads=1)
+    result_1,err = tpcf_jackknife(sample1, randoms, rbins, Nsub=5, period = period, num_threads=1)
     
     print(err)
     assert np.shape(err)==(nbins,nbins), "correlation functions do not match"
