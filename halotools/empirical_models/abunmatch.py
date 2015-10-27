121a1f00064be7a5969777cab24c3d7c41078d5a
Andrew Hearin <andrew.hearin@yale.edu>
2015-10-27 13:56:43 -0400

    Deleted docstring to abunmatch that was causing problems

--- halotools/empirical_models/abunmatch.py
+++ halotools/empirical_models/abunmatch.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+
 """
 Module containing classes used to perform abundance matching (SHAM)
-and conditional abundance matching (CAM). 
+
 """
 
 import numpy as np
@@ -26,56 +27,12 @@ from ..utils import array_utils
 __all__ = ['AbunMatchSmHm']
 
 class AbunMatchSmHm(PrimGalpropModel):
-    """ Stellar-to-halo-mass relation based on traditional abundance matching. 
+    """
     """
 
     def __init__(self, galprop_name, galaxy_abundance_abcissa, galaxy_abundance_ordinates, 
         scatter_level = 0.2, **kwargs):
-        """
-        Parameters 
-        ----------
-        galprop_name : string  
-            Name of the galaxy property being assigned. Default is ``stellar mass``, 
-            though another common case may be ``luminosity``. 
-
-        galaxy_abundance_ordinates : array_like
-            Length-Ng array storing the comoving number density of galaxies 
-            The value ``galaxy_abundance_ordinates[i]`` gives the comoving number density 
-            of galaxies evaluated at the galaxy property stored in ``galaxy_abundance_abcissa[i]``. 
-            The most common two cases are where ``galaxy_abundance_abcissa`` stores either 
-            stellar mass or luminosity, in which case ``galaxy_abundance_ordinates`` would 
-            simply be the stellar mass function or the luminosity function, respectively. 
-
-        galaxy_abundance_abcissa : array_like
-            Length-Ng array storing the property of the galaxies for which the 
-            abundance has been tabulated. 
-             The value ``galaxy_abundance_ordinates[i]`` gives the comoving number density 
-            of galaxies evaluated at the galaxy property stored in ``galaxy_abundance_abcissa[i]``. 
-            The most common two cases are where ``galaxy_abundance_abcissa`` stores either 
-            stellar mass or luminosity, in which case ``galaxy_abundance_ordinates`` would 
-            simply be the stellar mass function or the luminosity function, respectively. 
-
-        subhalo_abundance_ordinates : array_like, optional  
-            Length-Nh array storing the comoving number density of subhalo_table.
-            The value ``subhalo_abundance_ordinates[i]`` gives the comoving number density 
-            of subhalo_table of property ``subhalo_abundance_abcissa[i]``. 
-            If keyword arguments ``subhalo_abundance_ordinates`` 
-            and ``subhalo_abundance_abcissa`` are not passed, 
-            then keyword arguments ``prim_haloprop_key`` and ``halo_table`` must be passed. 
-
-        subhalo_abundance_abcissa : array_like, optional  
-            Length-Nh array storing the stellar mass of subhalo_table. 
-            The value ``subhalo_abundance_ordinates[i]`` gives the comoving number density 
-            of subhalo_table of property ``subhalo_abundance_abcissa[i]``. 
-            If keyword arguments ``subhalo_abundance_ordinates`` 
-            and ``subhalo_abundance_abcissa`` are not passed, 
-            then keyword arguments ``prim_haloprop_key`` and ``halo_table`` must be passed. 
-
-        scatter_level : float, optional  
-            Level of constant scatter in dex. Default is 0.2. 
-
-        """
-
+    
         kwargs['scatter_model'] = LogNormalScatterModel
         kwargs['scatter_abcissa'] = [12]
         kwargs['scatter_ordinates'] = [scatter_level]
