0a639d6190d00066efe9bb6a0963ffbf8662711d
Andrew Hearin <andrew.hearin@yale.edu>
2015-10-27 14:11:31 -0400

    Removed abunmatch.py entirely from repo

--- halotools/empirical_models/__init__.py
+++ halotools/empirical_models/__init__.py
@@ -5,7 +5,6 @@ from __future__ import (absolute_import, division, print_function,
 
 
 from .model_defaults import *
-from .abunmatch import *
 from .model_helpers import *
 
 from .factories import *
