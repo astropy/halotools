7c70ac3e492b9072dcaf124f42d9dfdf64c6f2f5
Andrew Hearin <andrew.hearin@yale.edu>
2015-10-27 10:07:52 -0400

    Added test of redshift safety behavior

---
+++ halotools/empirical_models/smhm_models/tests/test_redshift_defensiveness.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import numpy as np 
+from astropy.table import Table 
+from astropy.io.ascii import read as astropy_ascii_read
+from astropy.utils.data import get_pkg_data_filename, get_pkg_data_fileobj
+from astropy.tests.helper import remote_data, pytest
+from unittest import TestCase
+from copy import copy
+
+
+from ...smhm_models import *
+
+from ... import model_defaults
+from ....sim_manager import sim_defaults
+
+def test_behroozi10_redshift_safety():
+	"""
+	"""
+	model = Behroozi10SmHm()
+	result0 = model.mean_log_halo_mass(11)
+	result1 = model.mean_log_halo_mass(11, redshift = 4)
+	result2 = model.mean_log_halo_mass(11, redshift = sim_defaults.default_redshift)
+	assert result0 == result2
+	assert result0 != result1
+
+	result0 = model.mean_stellar_mass(prim_haloprop = 1e12)
+	result1 = model.mean_stellar_mass(prim_haloprop = 1e12, redshift = 4)
+	result2 = model.mean_stellar_mass(prim_haloprop = 1e12, redshift = sim_defaults.default_redshift)
+	assert result0 == result2
+	assert result0 != result1
+
