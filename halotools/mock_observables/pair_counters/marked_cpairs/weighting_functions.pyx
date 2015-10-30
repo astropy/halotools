3d79bcd1e96bb04db3a06cf9050d0a1f8e82e430
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-10-30 14:20:49 -0400

    fixed bigs and updated doc strings

--- halotools/mock_observables/pair_counters/marked_cpairs/weighting_functions.pyx
+++ halotools/mock_observables/pair_counters/marked_cpairs/weighting_functions.pyx
@@ -19,6 +19,7 @@ cdef double mweights(np.float64_t* w1, np.float64_t* w2):
     multiplicative weights
     return w1[0]*w2[0]
     id: 1
+    expects length 1 arrays
     """
     return w1[0]*w2[0]
 
@@ -28,6 +29,7 @@ cdef double sweights(np.float64_t* w1, np.float64_t* w2):
     summed weights
     return w1[0]+w2[0]
     id: 2
+    expects length 1 arrays
     """
     return w1[0]+w2[0]
 
@@ -37,6 +39,7 @@ cdef double eqweights(np.float64_t* w1, np.float64_t* w2):
     equality weights
     return w1[1]*w2[1] if w1[0]==w2[0]
     id: 3
+    expects length 2 arrays
     """
     if w1[0]==w2[0]: return w1[1]*w2[1]
     else: return 0.0
@@ -47,6 +50,7 @@ cdef double gweights(np.float64_t* w1, np.float64_t* w2):
     greater than weights
     return w1[1]*w2[1] if w2[0]>w1[0]
     id: 4
+    expects length 2 arrays
     """
     if w2[0]>w1[0]: return w1[1]*w2[1]
     else: return 0.0
@@ -57,6 +61,7 @@ cdef double lweights(np.float64_t* w1, np.float64_t* w2):
     less than weights
     return w1[1]*w2[1] if w2[0]<w1[0]
     id: 5
+    expects length 2 arrays
     """
     if w2[0]<w1[0]: return w1[1]*w2[1]
     else: return 0.0
@@ -67,6 +72,7 @@ cdef double tgweights(np.float64_t* w1, np.float64_t* w2):
     greater than tolerance weights
     return w2[1] if w2[0]>(w1[0]+w1[1])
     id: 6
+    expects length 2 arrays
     """
     if w2[0]>(w1[0]+w1[1]): return w2[1]
     else: return 0.0
@@ -77,6 +83,7 @@ cdef double tlweights(np.float64_t* w1, np.float64_t* w2):
     less than tolerance weights
     return w2[1] if w2[0]<(w1[0]-w1[1])
     id: 7
+    expects length 2 arrays
     """
     if w2[0]<(w1[0]+w1[1]): return w2[1]
     else: return 0.0
@@ -87,6 +94,7 @@ cdef double tweights(np.float64_t* w1, np.float64_t* w2):
     tolerance weights
     return w2[1] if |w1[0]-w2[0]|<w1[1]
     id: 8
+    expects length 2 arrays
     """
     if fabs(w1[0]-w2[0])<w1[1]: return w2[1]
     else: return 0.0
@@ -97,6 +105,7 @@ cdef double exweights(np.float64_t* w1, np.float64_t* w2):
     exclusion weights
     return w2[1] if |w1[0]-w2[0]|>w1[1]
     id: 9
+    expects length 2 arrays
     """
     if fabs(w1[0]-w2[0])>w1[1]: return w2[1]
     else: return 0.0
