ba1044151e025dda64e169a0d6833facd02de289
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-10-30 14:12:45 -0400

    changed function calls to weighting functions

--- halotools/mock_observables/pair_counters/marked_cpairs/marked_cpairs.pyx
+++ halotools/mock_observables/pair_counters/marked_cpairs/marked_cpairs.pyx
@@ -13,29 +13,27 @@ import numpy as np
 cimport numpy as np
 from libc.math cimport fabs, fmin
 
-from .objective_weights cimport *
+from .weighting_functions cimport *
 
 
 __author__ = ['Duncan Campbell']
-__all__ = ['obj_wnpairs_no_pbc', 'obj_wnpairs_pbc']
+__all__ = ['marked_npairs_no_pbc', 'marked_npairs_pbc']
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
-def obj_wnpairs_no_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
-                       np.ndarray[np.float64_t, ndim=1] y_icell1,
-                       np.ndarray[np.float64_t, ndim=1] z_icell1,
-                       np.ndarray[np.float64_t, ndim=1] x_icell2,
-                       np.ndarray[np.float64_t, ndim=1] y_icell2,
-                       np.ndarray[np.float64_t, ndim=1] z_icell2,
-                       np.ndarray[np.float64_t, ndim=1] w_icell1,
-                       np.ndarray[np.float64_t, ndim=1] w_icell2,
-                       np.ndarray[np.float64_t, ndim=1] r_icell1,
-                       np.ndarray[np.float64_t, ndim=1] r_icell2,
-                       np.ndarray[np.float64_t, ndim=1] rbins,
-                       np.int_t weight_func_id
-                       ):
+def marked_npairs_no_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
+                         np.ndarray[np.float64_t, ndim=1] y_icell1,
+                         np.ndarray[np.float64_t, ndim=1] z_icell1,
+                         np.ndarray[np.float64_t, ndim=1] x_icell2,
+                         np.ndarray[np.float64_t, ndim=1] y_icell2,
+                         np.ndarray[np.float64_t, ndim=1] z_icell2,
+                         np.ndarray[np.float64_t, ndim=2] w_icell1,
+                         np.ndarray[np.float64_t, ndim=2] w_icell2,
+                         np.ndarray[np.float64_t, ndim=1] rbins,
+                         np.int_t weight_func_id
+                         ):
     """
     weighted real-space pair counter without periodic boundary conditions (no PBCs).
     Calculate the weighted number of pairs with separations less than or equal to 
@@ -51,6 +49,9 @@ def obj_wnpairs_no_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
     cdef int Ni = len(x_icell1)
     cdef int Nj = len(x_icell2)
     
+    #choose weighting function
+    wfunc = return_weighting_function(weight_func_id)
+    
     #loop over points in grid1's cell
     for i in range(0,Ni):
                 
@@ -61,47 +62,9 @@ def obj_wnpairs_no_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
             d = square_distance(x_icell1[i],y_icell1[i],z_icell1[i],\
                                 x_icell2[j],y_icell2[j],z_icell2[j])
             
-            #calculate counts in bins
-            if weight_func_id==0:
-                radial_wbinning_0(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==1:
-                radial_wbinning_1(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==2:
-                radial_wbinning_2(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==3:
-                radial_wbinning_3(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==4:
-                radial_wbinning_4(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==5:
-                radial_wbinning_5(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==6:
-                radial_wbinning_6(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==7:
-                radial_wbinning_7(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==8:
-                radial_wbinning_8(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==9:
-                radial_wbinning_9(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
+            radial_wbinning(<np.float64_t*>counts.data,\
+                            <np.float64_t*>rbins.data, d, nbins_minus_one,\
+                            w_icell1[i,:], w_icell2[j,:], wfunc)
     
     return counts
 
@@ -109,19 +72,17 @@ def obj_wnpairs_no_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
-def obj_wnpairs_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
-                   np.ndarray[np.float64_t, ndim=1] y_icell1,
-                   np.ndarray[np.float64_t, ndim=1] z_icell1,
-                   np.ndarray[np.float64_t, ndim=1] x_icell2,
-                   np.ndarray[np.float64_t, ndim=1] y_icell2,
-                   np.ndarray[np.float64_t, ndim=1] z_icell2,
-                   np.ndarray[np.float64_t, ndim=1] w_icell1,
-                   np.ndarray[np.float64_t, ndim=1] w_icell2,
-                   np.ndarray[np.float64_t, ndim=1] r_icell1,
-                   np.ndarray[np.float64_t, ndim=1] r_icell2,
-                   np.ndarray[np.float64_t, ndim=1] rbins,
-                   np.ndarray[np.float64_t, ndim=1] period,
-                   np.int_t weight_func_id):
+def marked_npairs_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
+                      np.ndarray[np.float64_t, ndim=1] y_icell1,
+                      np.ndarray[np.float64_t, ndim=1] z_icell1,
+                      np.ndarray[np.float64_t, ndim=1] x_icell2,
+                      np.ndarray[np.float64_t, ndim=1] y_icell2,
+                      np.ndarray[np.float64_t, ndim=1] z_icell2,
+                      np.ndarray[np.float64_t, ndim=2] w_icell1,
+                      np.ndarray[np.float64_t, ndim=2] w_icell2,
+                      np.ndarray[np.float64_t, ndim=1] rbins,
+                      np.ndarray[np.float64_t, ndim=1] period,
+                      np.int_t weight_func_id):
     """
     weighted real-space pair counter with periodic boundary conditions (PBCs).
     Calculate the weighted number of pairs with separations less than or equal to 
@@ -137,6 +98,9 @@ def obj_wnpairs_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
     cdef int Ni = len(x_icell1)
     cdef int Nj = len(x_icell2)
     
+    #choose weighting function
+    wfunc = return_weighting_function(weight_func_id)
+    
     #loop over points in grid1's cell
     for i in range(0,Ni):
                 
@@ -148,47 +112,9 @@ def obj_wnpairs_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
                                          x_icell2[j],y_icell2[j],z_icell2[j],\
                                          <np.float64_t*>period.data)
                     
-            #calculate counts in bins
-            if weight_func_id==0:
-                radial_wbinning_0(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==1:
-                radial_wbinning_1(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==2:
-                radial_wbinning_2(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==3:
-                radial_wbinning_3(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==4:
-                radial_wbinning_4(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==5:
-                radial_wbinning_5(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==6:
-                radial_wbinning_6(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==7:
-                radial_wbinning_7(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==8:
-                radial_wbinning_8(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
-            elif weight_func_id==9:
-                radial_wbinning_9(<np.float64_t*>counts.data,\
-                                  <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                                  w_icell1[i], w_icell2[j], r_icell1[i], r_icell2[j])
+            radial_wbinning(<np.float64_t*>counts.data,\
+                            <np.float64_t*>rbins.data, d, nbins_minus_one,\
+                            w_icell1[i,:], w_icell2[j,:], wfunc)
     
     return counts
 
@@ -230,143 +156,45 @@ cdef inline double square_distance(np.float64_t x1, np.float64_t y1, np.float64_
     return dx*dx+dy*dy+dz*dz
 
 
-cdef inline radial_wbinning_0(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
+cdef inline radial_wbinning(np.float64_t* counts, np.float64_t* bins,\
+                            np.float64_t d, np.int_t k,\
+                            np.float64_t w1, np.float64_t w2, wfunc):
     """
     real space radial weighted binning function
     """
     
     while d<=bins[k]:
-        counts[k] += 0
+        counts[k] += wfunc(w1, w2)
         k=k-1
         if k<0: break
 
 
-cdef inline radial_wbinning_1(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
+return_weighting_function(weight_func_id):
     """
-    real space radial weighted binning function
+    returns a pointer to the user-specified weighting function
     """
     
-    while d<=bins[k]:
-        counts[k] += mweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
+    if weight_func_id==0:
+        return custom_func
+    elif weight_func_id==1:
+        return mweights
+    elif weight_func_id==2:
+        return sweights
+    elif weight_func_id==3:
+        return eqweights
+    elif weight_func_id==4:
+        return gweights
+    elif weight_func_id==5:
+        return lweights
+    elif weight_func_id==6:
+        return tgweights
+    elif weight_func_id==7:
+        return tlweights
+    elif weight_func_id==8:
+        return tweights
+    elif weight_func_id==9:
+        return exweights
+    else:
+        raise ValueError('weighting function does not exist')
 
 
-cdef inline radial_wbinning_2(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
-    """
-    real space radial weighted binning function
-    """
-    
-    while d<=bins[k]:
-        counts[k] += sweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
-
-
-cdef inline radial_wbinning_3(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
-    """
-    real space radial weighted binning function
-    """
-    
-    while d<=bins[k]:
-        counts[k] += eqweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
-
-
-cdef inline radial_wbinning_4(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
-    """
-    real space radial weighted binning function
-    """
-    
-    while d<=bins[k]:
-        counts[k] += gweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
-
-
-cdef inline radial_wbinning_5(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
-    """
-    real space radial weighted binning function
-    """
-    
-    while d<=bins[k]:
-        counts[k] += lweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
-    
-
-cdef inline radial_wbinning_6(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
-    """
-    real space radial weighted binning function
-    """
-    
-    while d<=bins[k]:
-        counts[k] += tgweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
-    
-
-cdef inline radial_wbinning_7(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
-    """
-    real space radial weighted binning function
-    """
-    
-    while d<=bins[k]:
-        counts[k] += tlweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
-    
-
-cdef inline radial_wbinning_8(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
-    """
-    real space radial weighted binning function
-    """
-    
-    while d<=bins[k]:
-        counts[k] += tweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
-
-
-cdef inline radial_wbinning_9(np.float64_t* counts, np.float64_t* bins,\
-                             np.float64_t d, np.int_t k,\
-                             np.float64_t w1, np.float64_t w2,
-                             np.float64_t r1, np.float64_t r2):
-    """
-    real space radial weighted binning function
-    """
-    
-    while d<=bins[k]:
-        counts[k] += exweights(w1,w2,r1,r2)
-        k=k-1
-        if k<0: break
-    
-    
