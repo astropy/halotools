44420a6c523daf8056908a203f7c360c524320e3
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-10-30 15:38:00 -0400

    marked_cpairs is comiling!

--- halotools/mock_observables/pair_counters/marked_cpairs/marked_cpairs.pyx
+++ halotools/mock_observables/pair_counters/marked_cpairs/marked_cpairs.pyx
@@ -15,7 +15,7 @@ from libc.math cimport fabs, fmin
 
 from .weighting_functions cimport *
 from .custom_weighting_func cimport *
-
+ctypedef double (*f_type)(np.float64_t* w1, np.float64_t* w2)
 
 __author__ = ['Duncan Campbell']
 __all__ = ['marked_npairs_no_pbc', 'marked_npairs_pbc']
@@ -43,12 +43,18 @@ def marked_npairs_no_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
     
     #c definitions
     cdef int nbins = len(rbins)
+    cdef int n_weights1 = np.shape(w_icell1)[1]
+    cdef int n_weights2 = np.shape(w_icell2)[1]
     cdef int nbins_minus_one = len(rbins) -1
     cdef np.ndarray[np.float64_t, ndim=1] counts = np.zeros((nbins,), dtype=np.float64)
     cdef double d
     cdef int i, j
     cdef int Ni = len(x_icell1)
     cdef int Nj = len(x_icell2)
+    cdef f_type wfunc
+    
+    cdef np.ndarray[np.float64_t, ndim=1] subw1 = np.zeros((n_weights1,), dtype=np.float64)
+    cdef np.ndarray[np.float64_t, ndim=1] subw2 = np.zeros((n_weights2,), dtype=np.float64)
     
     #choose weighting function
     wfunc = return_weighting_function(weight_func_id)
@@ -63,9 +69,14 @@ def marked_npairs_no_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
             d = square_distance(x_icell1[i],y_icell1[i],z_icell1[i],\
                                 x_icell2[j],y_icell2[j],z_icell2[j])
             
+            #get the weighting vectors for each point
+            subw1 = w_icell1[i,:]
+            subw2 = w_icell2[j,:]
+            
             radial_wbinning(<np.float64_t*>counts.data,\
                             <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                            w_icell1[i,:], w_icell2[j,:], wfunc)
+                            <np.float64_t*>subw1.data, <np.float64_t*>subw2.data,\
+                            <f_type>wfunc)
     
     return counts
 
@@ -92,12 +103,18 @@ def marked_npairs_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
     
     #c definitions
     cdef int nbins = len(rbins)
+    cdef int n_weights1 = np.shape(w_icell1)[1]
+    cdef int n_weights2 = np.shape(w_icell2)[1]
     cdef int nbins_minus_one = len(rbins) -1
     cdef np.ndarray[np.float64_t, ndim=1] counts = np.zeros((nbins,), dtype=np.float64)
     cdef double d
     cdef int i, j
     cdef int Ni = len(x_icell1)
     cdef int Nj = len(x_icell2)
+    cdef f_type wfunc
+    
+    cdef np.ndarray[np.float64_t, ndim=1] subw1 = np.zeros((n_weights1,), dtype=np.float64)
+    cdef np.ndarray[np.float64_t, ndim=1] subw2 = np.zeros((n_weights2,), dtype=np.float64)
     
     #choose weighting function
     wfunc = return_weighting_function(weight_func_id)
@@ -112,10 +129,15 @@ def marked_npairs_pbc(np.ndarray[np.float64_t, ndim=1] x_icell1,
             d = periodic_square_distance(x_icell1[i],y_icell1[i],z_icell1[i],\
                                          x_icell2[j],y_icell2[j],z_icell2[j],\
                                          <np.float64_t*>period.data)
-                    
+            
+            #get the weighting vectors for each point
+            subw1 = w_icell1[i,:]
+            subw2 = w_icell2[j,:]
+            
             radial_wbinning(<np.float64_t*>counts.data,\
                             <np.float64_t*>rbins.data, d, nbins_minus_one,\
-                            w_icell1[i,:], w_icell2[j,:], wfunc)
+                            <np.float64_t*>subw1.data, <np.float64_t*>subw2.data,\
+                            <f_type>wfunc)
     
     return counts
 
@@ -159,7 +181,7 @@ cdef inline double square_distance(np.float64_t x1, np.float64_t y1, np.float64_
 
 cdef inline radial_wbinning(np.float64_t* counts, np.float64_t* bins,\
                             np.float64_t d, np.int_t k,\
-                            np.float64_t w1, np.float64_t w2, wfunc):
+                            np.float64_t* w1, np.float64_t* w2, f_type wfunc):
     """
     real space radial weighted binning function
     """
@@ -170,7 +192,7 @@ cdef inline radial_wbinning(np.float64_t* counts, np.float64_t* bins,\
         if k<0: break
 
 
-def return_weighting_function(weight_func_id):
+cdef f_type return_weighting_function(weight_func_id):
     """
     returns a pointer to the user-specified weighting function
     """
