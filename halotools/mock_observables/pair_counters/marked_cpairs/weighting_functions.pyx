88326bd6d705477e4be2abc5e327beefb2106b7d
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-10-30 14:18:39 -0400

    fixed bigs and updated doc strings

--- halotools/mock_observables/pair_counters/marked_cpairs/weighting_functions.pyx
+++ halotools/mock_observables/pair_counters/marked_cpairs/weighting_functions.pyx
@@ -17,7 +17,7 @@ __author__ = ["Duncan Campbell"]
 cdef double mweights(np.float64_t* w1, np.float64_t* w2):
     """
     multiplicative weights
-    return w1*w2
+    return w1[0]*w2[0]
     id: 1
     """
     return w1[0]*w2[0]
@@ -26,7 +26,7 @@ cdef double mweights(np.float64_t* w1, np.float64_t* w2):
 cdef double sweights(np.float64_t* w1, np.float64_t* w2):
     """
     summed weights
-    return w1+w2
+    return w1[0]+w2[0]
     id: 2
     """
     return w1[0]+w2[0]
@@ -35,17 +35,17 @@ cdef double sweights(np.float64_t* w1, np.float64_t* w2):
 cdef double eqweights(np.float64_t* w1, np.float64_t* w2):
     """
     equality weights
-    return r1*r2 if w1==w2
+    return w1[1]*w2[1] if w1[0]==w2[0]
     id: 3
     """
-    if w1[0]==w2[1]: return w1[1]*w2[1]
+    if w1[0]==w2[0]: return w1[1]*w2[1]
     else: return 0.0
 
 
 cdef double gweights(np.float64_t* w1, np.float64_t* w2):
     """
     greater than weights
-    return r1*r2 if w2>w1
+    return w1[1]*w2[1] if w2[0]>w1[0]
     id: 4
     """
     if w2[0]>w1[0]: return w1[1]*w2[1]
@@ -55,7 +55,7 @@ cdef double gweights(np.float64_t* w1, np.float64_t* w2):
 cdef double lweights(np.float64_t* w1, np.float64_t* w2):
     """
     less than weights
-    return r1*r2 if w2<w1
+    return w1[1]*w2[1] if w2[0]<w1[0]
     id: 5
     """
     if w2[0]<w1[0]: return w1[1]*w2[1]
@@ -65,40 +65,40 @@ cdef double lweights(np.float64_t* w1, np.float64_t* w2):
 cdef double tgweights(np.float64_t* w1, np.float64_t* w2):
     """
     greater than tolerance weights
-    return r2 if w2>(w1+r1)
+    return w2[1] if w2[0]>(w1[0]+w1[1])
     id: 6
     """
-    if w2[0]>(w1[0]+w1[0]): return w2[0]
+    if w2[0]>(w1[0]+w1[1]): return w2[1]
     else: return 0.0
 
 
 cdef double tlweights(np.float64_t* w1, np.float64_t* w2):
     """
     less than tolerance weights
-    return r2 if w2<(w1-r1)
+    return w2[1] if w2[0]<(w1[0]-w1[1])
     id: 7
     """
-    if w2[0]<(w1[0]+w1[0]): return w2[0]
+    if w2[0]<(w1[0]+w1[1]): return w2[1]
     else: return 0.0
 
 
 cdef double tweights(np.float64_t* w1, np.float64_t* w2):
     """
     tolerance weights
-    return r2 if |w1-w2|<r1
+    return w2[1] if |w1[0]-w2[0]|<w1[1]
     id: 8
     """
-    if fabs(w1[0]-w2[0])<w1[0]: return w2[0]
+    if fabs(w1[0]-w2[0])<w1[1]: return w2[1]
     else: return 0.0
 
 
 cdef double exweights(np.float64_t* w1, np.float64_t* w2):
     """
     exclusion weights
-    return r2 if |w1-w2|>r1
+    return w2[1] if |w1[0]-w2[0]|>w1[1]
     id: 9
     """
-    if fabs(w1[0]-w2[0])>w1[0]: return w2[0]
+    if fabs(w1[0]-w2[0])>w1[1]: return w2[1]
     else: return 0.0
 
 
