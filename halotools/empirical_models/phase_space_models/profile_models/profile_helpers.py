a9c36970792a8e4cd5e0ae703b266efa68250fd9
Andrew Hearin <andrew.hearin@yale.edu>
2015-11-02 12:36:59 -0500

    Improved exception handling of profile_helper

--- halotools/empirical_models/phase_space_models/profile_models/profile_helpers.py
+++ halotools/empirical_models/phase_space_models/profile_models/profile_helpers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """ Module containing functions related to halo mass definitions, 
 the relations between halo mass and radius, and the variation of these 
-relations with cosmology and redshift. 
+relations with cosmology_obj and redshift. 
 
 The functions contained in this module borrow heavily from the Colossus 
 package developed by Benedikt Diemer, http://bdiemer.bitbucket.org. 
@@ -13,18 +13,20 @@ import numpy as np
 from astropy import cosmology
 from astropy import units as u
 
+from ....custom_exceptions import *
+
 __all__ = (['density_threshold', 'delta_vir', 
 	'halo_mass_to_halo_radius', 'halo_radius_to_halo_mass'])
 
 __author__ = ['Benedikt Diemer', 'Andrew Hearin']
 
-def density_threshold(cosmology, redshift, mdef):
+def density_threshold(cosmology_obj, redshift, mdef):
 	"""
 	The threshold density for a given spherical-overdensity mass definition.
 	
 	Parameters
 	--------------
-	cosmology : object 
+	cosmology_obj : object 
 		Instance of an `~astropy.cosmology` object. 
 
 	redshift: array_like
@@ -43,38 +45,69 @@ def density_threshold(cosmology, redshift, mdef):
 	----------
 	delta_vir: The virial overdensity in units of the critical density.
 	"""
-	rho_crit = cosmology.critical_density(redshift)
-	rho_crit = rho_crit.to(u.Msun/u.Mpc**3).value/cosmology.h**2
+
+	try:
+		assert isinstance(cosmology_obj, cosmology.core.FLRW)
+	except AssertionError:
+		msg = ("\nYour input ``cosmology_obj`` must be an instance of "
+			"`~astropy.cosmology.core.FLRW`\n")
+		raise HalotoolsError(msg)
+
+	mdef_msg = ("\nYour input mdef = ``%s`` is not recognized.\n\n"
+		"The string formatting of the ``mdef`` input must be one of the following:\n"
+		"\n1. A positive integer followed by the letter ``m``," 
+		"for the case where you wish to specify the integer multiple of the mean matter density,\n"
+		"\n2. A positive integer followed by the letter ``c``," 
+		"for the case where you wish to specify the integer multiple of the critical density,\n"
+		"\n3. The string ``vir``, for the virial overdensity defined by Bryan & Norman (1998)\n")
+
+	try:
+		delta_multiple = int(mdef[:-1])
+		if delta_multiple <= 0:
+			raise HalotoolsError("\nYour density threshold must be a positive integer\n")
+	except:
+		if mdef != 'vir':
+			raise HalotoolsError(mdef_msg)
+
+	final_char = mdef[-1]
+	try:
+		assert final_char in ('c', 'm')
+	except AssertionError:
+		if mdef != 'vir':
+			raise HalotoolsError(mdef_msg)
+
+
+	rho_crit = cosmology_obj.critical_density(redshift)
+	rho_crit = rho_crit.to(u.Msun/u.Mpc**3).value/cosmology_obj.h**2
 
 	if mdef[-1] == 'c':
 		delta = int(mdef[:-1])
-		rho_treshold = rho_crit * delta
+		rho_threshold = rho_crit * delta
 
 	elif mdef[-1] == 'm':
-		rho_crit0 = cosmology.critical_density0
-		rho_crit0 = rho_crit0.to(u.Msun/u.Mpc**3).value/cosmology.h**2
+		rho_crit0 = cosmology_obj.critical_density0
+		rho_crit0 = rho_crit0.to(u.Msun/u.Mpc**3).value/cosmology_obj.h**2
 		delta = int(mdef[:-1])
-		rho_m = cosmology.Om(redshift)*rho_crit0
-		rho_treshold = delta * rho_m
+		rho_m = cosmology_obj.Om(redshift)*rho_crit0
+		rho_threshold = delta * rho_m
 
 	elif mdef == 'vir':
-		delta = delta_vir(cosmology, redshift)
-		rho_treshold = rho_crit * delta
+		delta = delta_vir(cosmology_obj, redshift)
+		rho_threshold = rho_crit * delta
 
 	else:
-		msg = 'Invalid mass definition, %s.' % mdef
-		raise Exception(msg)
+		raise HalotoolsError(mdef_msg)
 
-	return rho_treshold
+	return rho_threshold
 
-def delta_vir(cosmology, redshift):
+def delta_vir(cosmology_obj, redshift):
 	"""
 	The virial overdensity in units of the critical density, 
 	using the fitting formula of Bryan & Norman 1998.
 	
 	Parameters
 	--------------
-	cosmology : object 
+	cosmology_obj : object 
 		Instance of an `~astropy.cosmology` object. 
 
 	redshift: array_like
@@ -90,12 +123,12 @@ def delta_vir(cosmology, redshift):
 	density_threshold: The threshold density for a given mass definition.
 	"""
 	
-	x = cosmology.Om(redshift) - 1.0
+	x = cosmology_obj.Om(redshift) - 1.0
 	delta = 18 * np.pi**2 + 82.0 * x - 39.0 * x**2
 
 	return delta
 
-def halo_mass_to_halo_radius(mass, cosmology, redshift, mdef):
+def halo_mass_to_halo_radius(mass, cosmology_obj, redshift, mdef):
 	"""
 	Spherical overdensity radius as a function of the input mass. 
 
@@ -106,7 +139,7 @@ def halo_mass_to_halo_radius(mass, cosmology, redshift, mdef):
 	mass: array_like
 		Total halo mass in :math:`M_{\odot}/h`; can be a number or a numpy array.
 
-	cosmology : object 
+	cosmology_obj : object 
 		Instance of an `~astropy.cosmology` object. 
 
 	redshift: array_like
@@ -125,12 +158,12 @@ def halo_mass_to_halo_radius(mass, cosmology, redshift, mdef):
 	halo_radius_to_halo_mass: Spherical overdensity radius from mass.
 	"""
 	
-	rho = density_threshold(cosmology, redshift, mdef)
+	rho = density_threshold(cosmology_obj, redshift, mdef)
 	radius = (mass * 3.0 / 4.0 / np.pi / rho)**(1.0 / 3.0)
 
 	return radius
 
-def halo_radius_to_halo_mass(radius, cosmology, redshift, mdef):
+def halo_radius_to_halo_mass(radius, cosmology_obj, redshift, mdef):
 	"""
 	Spherical overdensity mass as a function of the input radius. 
 
@@ -141,7 +174,7 @@ def halo_radius_to_halo_mass(radius, cosmology, redshift, mdef):
 	radius: array_like
 		Halo radius in physical Mpc/h; can be a scalar or a numpy array.
 
-	cosmology : object 
+	cosmology_obj : object 
 		Instance of an `~astropy.cosmology` object. 
 
 	redshift: array_like
@@ -157,7 +190,7 @@ def halo_radius_to_halo_mass(radius, cosmology, redshift, mdef):
 
 	"""
 	
-	rho = density_threshold(cosmology, redshift, mdef)
+	rho = density_threshold(cosmology_obj, redshift, mdef)
 	mass = 4.0 / 3.0 * np.pi * rho * radius**3
 	return mass
 
