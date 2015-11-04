37b05f5662018d974082ffffba538bc6ae448db9
Andrew Hearin <andrew.hearin@yale.edu>
2015-11-04 07:52:27 -0500

    Added test_mass_density of nfw

--- halotools/empirical_models/phase_space_models/profile_models/tests/test_nfw_profile.py
+++ halotools/empirical_models/phase_space_models/profile_models/tests/test_nfw_profile.py
@@ -35,6 +35,8 @@ class TestNFWProfile(TestCase):
         self.wmap9_nfw = NFWProfile(cosmology = WMAP9)
         self.m200_nfw = NFWProfile(mdef = '200m')
 
+        self.model_list = [self.default_nfw, self.wmap9_nfw, self.m200_nfw]
+
     def test_instance_attrs(self):
         """
         """
@@ -50,78 +52,27 @@ class TestNFWProfile(TestCase):
         assert hasattr(self.wmap9_nfw, 'mdef')
         assert hasattr(self.m200_nfw, 'mdef')
 
-    # The following tests are useful but need to be rewritten according to 
-    # the changes made by the prof_overhaul branch 
+    def test_mass_density(self):
+        """ Require the returned value of the 
+        `~halotools.empirical_models.phase_space_models.profile_models.NFWProfile.mass_density` 
+        function to be self-consistent with the 
+        returned value of the 
+        `~halotools.empirical_models.phase_space_models.profile_models.NFWProfile.dimensionless_mass_density` 
+        function. 
+        """
+        Npts = 100
+        radius = np.logspace(-2, -1, Npts)
+        mass = np.zeros(Npts) + 1e12
+        conc = 5
 
-    # # Check that the initialized attributes are correct
-    # model_instance = hpc.NFWProfile()
-    # assert hasattr(model_instance, 'cosmology')
-    # assert isinstance(model_instance.cosmology, cosmology.FlatLambdaCDM)
+        for model in self.model_list:
+            result = model.mass_density(radius, mass, conc)
 
-
-    # # Check that the lookup table attributes are correct
-    # model_instance.build_inv_cumu_lookup_table()
-
-    # assert np.all(model_instance.NFWmodel_conc_lookup_table_bins > 0)
-    # assert np.all(model_instance.NFWmodel_conc_lookup_table_bins < 1000)
-
-
-    # assert len(model_instance.NFWmodel_conc_lookup_table_bins) >= 10
-    # assert (len(model_instance.cumu_inv_func_table) == 
-    #     len(model_instance.func_table_indices) )
-
-    # # Verify accuracy of lookup table
-    # def get_lookup_table_frac_error(model, conc, test_radius):
-    #     """ Function returns the fractional difference between the 
-    #     exact value of the inverse cumulative mass PDF, and the 
-    #     value inferred from the discretized lookup table. 
-    #     """
-    #     exact_result = model.cumulative_mass_PDF(test_radius, conc)
-    #     conc_table = model.NFWmodel_conc_lookup_table_bins        
-    #     digitized_conc_index = np.digitize(np.array([conc]), conc_table)
-    #     digitized_conc = conc_table[digitized_conc_index]
-    #     func = model.cumu_inv_func_table[digitized_conc_index[0]]
-    #     approximate_result = 10.**func(np.log10(exact_result))
-    #     fracdiff = abs((approximate_result - test_radius)/test_radius)
-    #     return fracdiff
-    # # Now we will verify that the lookup table method attains 
-    # # better than 0.1% accuracy at all relevant radii and concentrations
-    # radius = np.logspace(-3, 0, 15)
-    # test_conc_array = np.linspace(1, 25, 5)
-    # for test_conc in test_conc_array:
-    #     frac_error = get_lookup_table_frac_error(
-    #         model_instance, test_conc, radius)
-    #     assert np.allclose(frac_error, 0, rtol = 1e-3, atol = 1e-3)
-
-    # # The lookup table should adjust properly when passed an input_dict
-    # initial_NFWmodel_conc_lookup_table_min = copy(model_instance.NFWmodel_conc_lookup_table_min)
-    # initial_NFWmodel_conc_lookup_table_max = copy(model_instance.NFWmodel_conc_lookup_table_max)
-    # initial_NFWmodel_conc_lookup_table_spacing = copy(model_instance.NFWmodel_conc_lookup_table_spacing)
-    # initial_NFWmodel_conc_lookup_table_bins = copy(model_instance.NFWmodel_conc_lookup_table_bins)
-
-    # model_instance.NFWmodel_conc_lookup_table_min -= 0.05
-    # model_instance.NFWmodel_conc_lookup_table_min += 0.05
-    # model_instance.NFWmodel_conc_lookup_table_spacing *= 0.9
-    # model_instance.build_inv_cumu_lookup_table()
-    # assert model_instance.NFWmodel_conc_lookup_table_bins != initial_NFWmodel_conc_lookup_table_bins
-
-    # model_instance.NFWmodel_conc_lookup_table_min = initial_NFWmodel_conc_lookup_table_min
-    # model_instance.NFWmodel_conc_lookup_table_max = initial_NFWmodel_conc_lookup_table_max
-    # model_instance.NFWmodel_conc_lookup_table_spacing = initial_NFWmodel_conc_lookup_table_spacing
-    # model_instance.build_inv_cumu_lookup_table()
-    # assert np.all(model_instance.NFWmodel_conc_lookup_table_bins == initial_NFWmodel_conc_lookup_table_bins)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            halo_radius = model.halo_mass_to_halo_radius(mass)
+            scaled_radius = radius/halo_radius
+            derived_result = (
+                model.density_threshold * 
+                model.dimensionless_mass_density(scaled_radius, conc)
+                )
+            assert np.allclose(derived_result, result, rtol = 1e-4)
 
