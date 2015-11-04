9bcb037ba45d4dfe6d2466dcede4b09e940219b6
Andrew Hearin <andrew.hearin@yale.edu>
2015-11-04 07:52:27 -0500

    Added test_mass_density of nfw

--- halotools/empirical_models/phase_space_models/tests/test_phase_space.py
+++ halotools/empirical_models/phase_space_models/tests/test_phase_space.py
@@ -228,6 +228,74 @@ class TestNFWPhaseSpace(TestCase):
         self.nfw.mc_vel(self._dummy_halo_table)
 
 
+    ### OLD TESTS OF THE NFW PROFILE MODEL
+    ### THESE ARE STILL RELEVANT BUT NEED TO BE REVAMPED TO THE NEW SYNTAX
+    # # Check that the lookup table attributes are correct
+    # model_instance.build_inv_cumu_lookup_table()
+
+    # assert np.all(model_instance.NFWmodel_conc_lookup_table_bins > 0)
+    # assert np.all(model_instance.NFWmodel_conc_lookup_table_bins < 1000)
+
+
+    # assert len(model_instance.NFWmodel_conc_lookup_table_bins) >= 10
+    # assert (len(model_instance.cumu_inv_func_table) == 
+    #     len(model_instance.func_table_indices) )
+
+    # # Verify accuracy of lookup table
+    # def get_lookup_table_frac_error(model, conc, test_radius):
+    #     """ Function returns the fractional difference between the 
+    #     exact value of the inverse cumulative mass PDF, and the 
+    #     value inferred from the discretized lookup table. 
+    #     """
+    #     exact_result = model.cumulative_mass_PDF(test_radius, conc)
+    #     conc_table = model.NFWmodel_conc_lookup_table_bins        
+    #     digitized_conc_index = np.digitize(np.array([conc]), conc_table)
+    #     digitized_conc = conc_table[digitized_conc_index]
+    #     func = model.cumu_inv_func_table[digitized_conc_index[0]]
+    #     approximate_result = 10.**func(np.log10(exact_result))
+    #     fracdiff = abs((approximate_result - test_radius)/test_radius)
+    #     return fracdiff
+    # # Now we will verify that the lookup table method attains 
+    # # better than 0.1% accuracy at all relevant radii and concentrations
+    # radius = np.logspace(-3, 0, 15)
+    # test_conc_array = np.linspace(1, 25, 5)
+    # for test_conc in test_conc_array:
+    #     frac_error = get_lookup_table_frac_error(
+    #         model_instance, test_conc, radius)
+    #     assert np.allclose(frac_error, 0, rtol = 1e-3, atol = 1e-3)
+
+    # # The lookup table should adjust properly when passed an input_dict
+    # initial_NFWmodel_conc_lookup_table_min = copy(model_instance.NFWmodel_conc_lookup_table_min)
+    # initial_NFWmodel_conc_lookup_table_max = copy(model_instance.NFWmodel_conc_lookup_table_max)
+    # initial_NFWmodel_conc_lookup_table_spacing = copy(model_instance.NFWmodel_conc_lookup_table_spacing)
+    # initial_NFWmodel_conc_lookup_table_bins = copy(model_instance.NFWmodel_conc_lookup_table_bins)
+
+    # model_instance.NFWmodel_conc_lookup_table_min -= 0.05
+    # model_instance.NFWmodel_conc_lookup_table_min += 0.05
+    # model_instance.NFWmodel_conc_lookup_table_spacing *= 0.9
+    # model_instance.build_inv_cumu_lookup_table()
+    # assert model_instance.NFWmodel_conc_lookup_table_bins != initial_NFWmodel_conc_lookup_table_bins
+
+    # model_instance.NFWmodel_conc_lookup_table_min = initial_NFWmodel_conc_lookup_table_min
+    # model_instance.NFWmodel_conc_lookup_table_max = initial_NFWmodel_conc_lookup_table_max
+    # model_instance.NFWmodel_conc_lookup_table_spacing = initial_NFWmodel_conc_lookup_table_spacing
+    # model_instance.build_inv_cumu_lookup_table()
+    # assert np.all(model_instance.NFWmodel_conc_lookup_table_bins == initial_NFWmodel_conc_lookup_table_bins)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
