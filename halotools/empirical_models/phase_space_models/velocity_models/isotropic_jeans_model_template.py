ebcee268a6c6073e6a93ee4cba33d595390169da
Andrew Hearin <andrew.hearin@yale.edu>
2015-11-04 15:45:14 -0500

    Moved IsotropicJeansVelocity into a dedicated module

--- halotools/empirical_models/phase_space_models/velocity_models/isotropic_jeans_model_template.py
+++ halotools/empirical_models/phase_space_models/velocity_models/isotropic_jeans_model_template.py
@@ -13,20 +13,22 @@ from scipy.special import spence
 from astropy.extern import six 
 from abc import ABCMeta, abstractmethod
 
-from .isotropic_jeans_model_template import IsotropicJeansVelocity
-
 from ....utils.array_utils import convert_to_ndarray
 
 __author__ = ['Andrew Hearin']
 
-__all__ = ['NFWJeansVelocity']
+__all__ = ['IsotropicJeansVelocity']
 
-class NFWJeansVelocity(IsotropicJeansVelocity):
-    """ Orthogonal mix-in class providing the solution to the Jeans equation 
-    for galaxies orbiting in an isotropic NFW profile with no spatial bias. 
+
+
+@six.add_metaclass(ABCMeta)
+class IsotropicJeansVelocity(object):
+    """ Orthogonal mix-in class used to transform a configuration 
+    space model for the 1-halo term into a phase space model  
+    by solving the Jeans equation of the underlying potential. 
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, velocity_bias = False, **kwargs):
         """
         Parameters 
         -----------
@@ -37,60 +39,32 @@ class NFWJeansVelocity(IsotropicJeansVelocity):
             Jeans solution for the halo radial velocity dispersion by an overall factor. 
             Default is False. 
         """
-        IsotropicJeansVelocity.__init__(self, **kwargs)
+        if velocity_bias is True:
+            self.param_dict['velbias_satellites'] = 1.
 
-    def _jeans_integrand_term1(self, y):
+    @abstractmethod
+    def dimensionless_velocity_dispersion(self, x, *args):
         """
-        """
-        return np.log(1+y)/(y**3*(1+y)**2)
+        Method returns the radial velocity dispersion scaled by 
+        the virial velocity, as a function of the 
+        halo-centric distance scaled by the halo radius.
 
-    def _jeans_integrand_term2(self, y):
-        """
-        """
-        return 1/(y**2*(1+y)**3)
-
-    def dimensionless_velocity_dispersion(self, x, conc):
-        """
         Parameters 
-        -----------
+        ----------
         x : array_like 
             Halo-centric distance scaled by the halo boundary, so that 
             :math:`0 <= x <= 1`. Can be a scalar or numpy array
 
-        conc : float 
-            Concentration of the halo.
+        args : sequence, optional 
+            Any additional parameters necessary to specify the shape of the radial profile, 
+            e.g., halo concentration.         
 
         Returns 
         -------
         result : array_like 
             Radial velocity dispersion profile scaled by the virial velocity. 
             The returned result has the same dimension as the input ``x``. 
+
         """
-        x = convert_to_ndarray(x)
-        x = x.astype(float)
-        result = np.zeros_like(x)
-
-        prefactor = conc*(conc*x)*(1. + conc*x)**2/self.g(conc)
-
-        lower_limit = conc*x
-        upper_limit = float("inf")
-        for i in range(len(x)):
-            term1, _ = quad_integration(self._jeans_integrand_term1, 
-                lower_limit[i], upper_limit, epsrel=1e-5)
-            term2, _ = quad_integration(self._jeans_integrand_term2, 
-                lower_limit[i], upper_limit, epsrel=1e-5)
-            result[i] = term1 - term2 
-
-        return np.sqrt(result*prefactor)
-
-
-
-
-
-
-
-
-
-
-
+        pass
 
