acc6102844c3f72cf0dac770e5b69088a3435072
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-11-23 17:17:19 -0500

    added import statements for tpcf_one_two_halo_decomp and delta_sogma

--- halotools/mock_observables/__init__.py
+++ halotools/mock_observables/__init__.py
@@ -1,12 +1,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-                        
+
 from .tpcf import *
+from .tpcf_jackknife import *
+from .tpcf_one_two_halo_decomp import *
+from .marked_tpcf import *
 from .redshift_space_tpcf import *
 from .wp import *
-from .tpcf_jackknife import *
 from .s_mu_tpcf import *
-from .marked_tpcf import *
+from .delta_sigma import *
 from .groups import *
 from .mock_survey import *
