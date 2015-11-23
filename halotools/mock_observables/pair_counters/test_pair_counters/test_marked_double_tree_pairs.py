8f74f4280f8098439384f79a2a8852030d0308b4
Andrew Hearin <andrew.hearin@yale.edu>
2015-11-23 13:37:03 -0500

    Added test verifying that when the correct signature is passed for wfunc, no exception is raised, and that an incorrect raises a HalotoolsError

--- halotools/mock_observables/pair_counters/test_pair_counters/test_marked_double_tree_pairs.py
+++ halotools/mock_observables/pair_counters/test_pair_counters/test_marked_double_tree_pairs.py
@@ -3,10 +3,14 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
+import pytest 
+
 from ..pairs import wnpairs as pure_python_weighted_pairs
 from ..marked_double_tree_pairs import marked_npairs
+from ..marked_double_tree_helpers import _func_signature_int_from_wfunc
 from ..double_tree_pairs import npairs
 
+from ....custom_exceptions import HalotoolsError
 
 def test_marked_npairs_periodic():
     
@@ -19,15 +23,15 @@ def test_marked_npairs_periodic():
     z = np.random.uniform(0, Lbox[2], Npts)
     data1 = np.vstack((x,y,z)).T
     weights1 = np.random.random(Npts)
-    #weights1 = np.ones(Npts)
     
     rbins = np.array([0.0,0.1,0.2,0.3])
 
-    result = marked_npairs(data1, data1, rbins, period=period, weights1=weights1, weights2=weights1, wfunc=1)
+    result = marked_npairs(data1, data1, 
+        rbins, period=period, weights1=weights1, weights2=weights1, wfunc=1)
     
-    test_result = pure_python_weighted_pairs(data1, data1, rbins, period=period, weights1=weights1, weights2=weights1)
+    test_result = pure_python_weighted_pairs(data1, data1, rbins, 
+        period=period, weights1=weights1, weights2=weights1)
 
-    print(test_result,result)
     assert np.allclose(test_result,result,rtol=1e-09), "pair counts are incorrect"
 
 
@@ -46,35 +50,48 @@ def test_marked_npairs_nonperiodic():
     
     rbins = np.array([0.0,0.1,0.2,0.3])
 
-    result = marked_npairs(data1, data1, rbins, period=None, weights1=weights1, weights2=weights1, wfunc=1)
+    result = marked_npairs(data1, data1, rbins, period=None, 
+        weights1=weights1, weights2=weights1, wfunc=1)
     
-    test_result = pure_python_weighted_pairs(data1, data1, rbins, period=None, weights1=weights1, weights2=weights1)
+    test_result = pure_python_weighted_pairs(data1, data1, rbins, 
+        period=None, weights1=weights1, weights2=weights1)
     
-    print(test_result,result)
     assert np.allclose(test_result,result,rtol=1e-09), "pair counts are incorrect"
 
 
-def test_marked_npairs_wfuncs():
+def test_marked_npairs_wfuncs_signatures():
+    """ Loop over all wfuncs and ensure that the wfunc signature is handled correctly. 
+    """
     
     Npts = 1e3
     Lbox = [1.0,1.0,1.0]
     period = np.array(Lbox)
-    
+
     x = np.random.uniform(0, Lbox[0], Npts)
     y = np.random.uniform(0, Lbox[1], Npts)
     z = np.random.uniform(0, Lbox[2], Npts)
     data1 = np.vstack((x,y,z)).T
-    weights1 = np.random.random(Npts)-0.5
-    #weights1 = np.ones(Npts)
     
     rbins = np.array([0.0,0.1,0.2,0.3])
 
-    result = marked_npairs(data1, data1, rbins, period=period, weights1=weights1, weights2=weights1, wfunc=1)
-    test_result = pure_python_weighted_pairs(data1, data1, rbins, period=period, weights1=weights1, weights2=weights1)
-    
-    print(result)
-    print(test_result)
-    print(result/test_result)
-    
-    assert np.all(np.isclose(result,test_result,rtol=0.0001)), "wfunc 1 returned incorrect results"
+    for wfunc_index in xrange(1, 12):
+        signature = _func_signature_int_from_wfunc(wfunc_index)
+        weights = np.random.random(Npts*signature).reshape(Npts, signature) - 0.5
+        result = marked_npairs(data1, data1, rbins, period=period, 
+            weights1=weights, weights2=weights, wfunc=wfunc_index)
+
+        with pytest.raises(HalotoolsError):
+            signature = _func_signature_int_from_wfunc(wfunc_index) + 1
+            weights = np.random.random(Npts*signature).reshape(Npts, signature) - 0.5
+            result = marked_npairs(data1, data1, rbins, period=period, 
+                weights1=weights, weights2=weights, wfunc=wfunc_index)
+
+
+
+
+
+
+
+
+
 
