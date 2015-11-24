f8a621af28966ced002aa53b081995b8d5577255
Andrew Hearin <andrew.hearin@yale.edu>
2015-11-24 18:27:35 -0500

    Added some new testing classes for the trees

---
+++ halotools/mock_observables/pair_counters/test_trees/test_double_tree.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function)
+
+from unittest import TestCase
+import pytest 
+
+import numpy as np 
+from copy import copy 
+
+
+__all__ = ['TestFlatRectanguloidDoubleTree']
+
+class TestFlatRectanguloidDoubleTree(TestCase):
+    """ Class providing tests of the `~halotools.mock_observables.pair_counters.FlatRectanguloidDoubleTree`. 
+    """
+
+    def setup_class(self):
+        """ Pre-load various arrays into memory for use by all tests. 
+        """
+        pass
