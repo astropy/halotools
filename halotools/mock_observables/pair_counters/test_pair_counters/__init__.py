df3952f3539e700646b6d1a12d18fe747586430f
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-11-24 19:32:36 -0500

    removed objectice pairs

--- halotools/mock_observables/pair_counters/test_pair_counters/__init__.py
+++ halotools/mock_observables/pair_counters/test_pair_counters/__init__.py
@@ -2,3 +2,5 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from test_double_tree_pairs import *
+from test_marked_double_tree_pairs import *
