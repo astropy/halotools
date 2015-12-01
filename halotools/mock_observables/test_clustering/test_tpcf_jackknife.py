cd96af00034eaf857e94c1e26311dc365cc09c48
Andrew Hearin <andrew.hearin@yale.edu>
2015-12-01 08:49:18 -0500

    Sped up many tests using approx_cell1_size = [rmax, rmax, rmax]

--- halotools/mock_observables/test_clustering/test_tpcf_jackknife.py
+++ halotools/mock_observables/test_clustering/test_tpcf_jackknife.py
@@ -20,6 +20,7 @@ sample1 = np.random.random((Npts,3))
 randoms = np.random.random((Npts*10,3))
 period = np.array([1.0,1.0,1.0])
 rbins = np.linspace(0.0,0.3,5).astype(float)
+rmax = rbins.max()
 
 @pytest.mark.slow
 def test_tpcf_jackknife_corr_func():
@@ -27,8 +28,11 @@ def test_tpcf_jackknife_corr_func():
     test the correlation function
     """
     
-    result_1,err = tpcf_jackknife(sample1, randoms, rbins, Nsub=5, period = period, num_threads=1)
-    result_2 = tpcf(sample1, rbins, randoms=randoms, period = period, num_threads=1)
+    result_1,err = tpcf_jackknife(sample1, randoms, rbins, 
+        Nsub=5, period = period, num_threads=1)
+
+    result_2 = tpcf(sample1, rbins,
+        randoms=randoms, period = period, num_threads=1)
     
     assert np.allclose(result_1,result_2,rtol=1e-09), "correlation functions do not match"
 
