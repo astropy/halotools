70218debc2aa9143cdbd342e5db73dfa30d730e2
Andrew Hearin <andrew.hearin@yale.edu>
2015-12-07 14:45:01 -0500

    Developing new HaloCatalog api

--- halotools/sim_manager/halo_catalog.py
+++ halotools/sim_manager/halo_catalog.py
@@ -15,7 +15,7 @@ except ImportError:
 from . import sim_defaults, catalog_manager
 
 from ..utils.array_utils import custom_len
-from ..custom_exceptions import *
+from ..custom_exceptions import HalotoolsError
 
 
 __all__ = ('HaloCatalog',)
@@ -26,11 +26,75 @@ class HaloCatalog(object):
     Container class for halo catalogs and particle data.  
     """
 
-    def __init__(self, simname=sim_defaults.default_simname, 
-        halo_finder=sim_defaults.default_halo_finder, 
-        redshift = sim_defaults.default_redshift, dz_tol = 0.05, 
-        preload_halo_table = False):
+    def __init__(self, **kwargs):
+        """
+        """
+
+        try:
+            import h5py
+        except ImportError:
+            raise HalotoolsError("Must have h5py package installed to use this feature")
+        self._catman = catalog_manager.CatalogManager()
+
+        self._process_constructor_inputs(**kwargs):
+
+        self.fname_halo_table = self._catman.get_halo_table_fname(
+            simname = simname, halo_finder = halo_finder, 
+            redshift = redshift, version_name = version_name, 
+            dz_tol = dz_tol)
+
+        self._check_metadata_consistency(
+            simname = simname, halo_finder = halo_finder, 
+            redshift = redshift, version_name = version_name, 
+            )
+
+        self._load_halo_table()
+        self._bind_metadata()
+
+    def _process_constructor_inputs(self, **kwargs):
+        """
+        """
         pass
+
+    def _load_halo_table(self):
+        """ Retrieve the halo_table from ``self.fname_halo_table`` and update the cache log. 
+        """
+        pass
+
+    def _bind_metadata(self):
+        """ Create convenience bindings of all metadata to the `HaloCatalog` instance. 
+        """
+        f = h5py.File(self.fname_halo_table)
+        for attr_key in f.attrs.keys():
+            setattr(self, attr_key, f.attrs[attr])
+
+    def _check_metadata_consistency(self, **kwargs):
+        """ Check that any metadata passed to the constructor 
+        agrees with any metadata bound to the hdf5 file storing the table. 
+        """
+        pass
+
+    def store_halocat_in_cache(self, halo_table_fname, overwrite = False, 
+        neglect_supplementary_metadata = False, store_ptcl_table = True, **kwargs):
+        """ 
+        Parameters 
+        ------------
+        fname_halo_table : string 
+            String providing the absolute path to the halo table. 
+
+        overwrite : bool, optional 
+
+        store_ptcl_table : bool, optional 
+
+        neglect_supplementary_metadata : bool, optional 
+        """
+        pass
+
+
+
+
+
+
 
 
 
