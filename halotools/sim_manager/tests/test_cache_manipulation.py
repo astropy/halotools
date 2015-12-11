1bf646b4d675acc7a729ab484abf5b01b3c701a0
Andrew Hearin <andrew.hearin@yale.edu>
2015-12-11 13:46:00 -0500

    Still generalizing TestCacheManipulation

--- halotools/sim_manager/tests/test_cache_manipulation.py
+++ halotools/sim_manager/tests/test_cache_manipulation.py
@@ -36,55 +36,86 @@ class TestCacheManipulation(TestCase):
         """ Pre-load various arrays into memory for use by all tests. 
         """
 
+        self.cache_log_tables_to_test = []
         simname_list = ['bolshoi', 'bolshoi']
         redshift_list = [0, 1]
         halo_finder_list = ['rockstar', 'rockstar']
         version_name_list = ['beta_v0', 'beta_v0']
         fname_list = ['whatever_fname1', 'whatever_fname2']
-        self.create_dummy_cache_log('temp_dummy_cache_dirname', 
+        self.cache_log_tables_to_test.append(
+            self.create_dummy_cache_log('temp_dummy_cache_dirname', 
             simname_list, redshift_list,
             halo_finder_list, version_name_list, fname_list)
+            )
+
+        for name, table in zip(self.dummy_cache_log_fnames, self.cache_log_tables_to_test):
+            self.create_dummy_cache_directories(name, table)
 
     def create_dummy_cache_log(self, tmp_name, simname_list, redshift_list, 
         halo_finder_list, version_name_list, fname_list):
 
-        self.temp_dirname = os.path.join(detected_home, 
-            'Desktop', tmp_name)
+        new_cache_name = os.path.join(detected_home, 'Desktop', tmp_name)
+        setattr(self, tmp_name, new_cache_name)
 
-        if os.path.isdir(self.temp_dirname) is False:
-            os.mkdir(self.temp_dirname)
+        try:
+            self.temp_cache_dirnames.append(new_cache_name)
+        except AttributeError:
+            self.temp_cache_dirnames = [new_cache_name]
+
+        if os.path.isdir(getattr(self, tmp_name)) is False:
+            os.mkdir(new_cache_name)
         else:
-            os.system('rm -rf ' + self.temp_dirname)
-            os.mkdir(self.temp_dirname)
+            os.system('rm -rf ' + new_cache_name)
+            os.mkdir(new_cache_name)
 
-        self.dummy_cache_log_fname = os.path.join(self.temp_dirname, 'dummy_cache_log.txt')
+        new_cache_log_fname = os.path.join(new_cache_name, 'dummy_cache_log.txt')
+        try:
+            self.dummy_cache_log_fnames.append(new_cache_log_fname)
+        except AttributeError:
+            self.dummy_cache_log_fnames = [new_cache_log_fname]
 
-        self.halo_table_cache_log_table1 = Table(
-            {'simname': simname_list, 
-            'redshift': redshift_list, 
-            'halo_finder': halo_finder_list, 
-            'version_name': version_name_list, 
+        return Table({'simname': simname_list, 'redshift': redshift_list, 
+            'halo_finder': halo_finder_list, 'version_name': version_name_list, 
             'fname': fname_list}
             )
 
-    def create_dummy_cache_directories(self):
-        pass
+    def create_dummy_cache_directories(self, basename, table):
+        """
+        """
+        basedir = os.path.dirname(basename)
+        for entry in table:
+            simname = entry['simname']
+            new_simname_path = os.path.join(basedir, simname)
+            try:
+                os.mkdir(new_simname_path)
+            except OSError:
+                pass
+            halo_finder = entry['halo_finder']
+            new_halo_finder_path = os.path.join(new_simname_path, halo_finder)
+            try:
+                os.mkdir(new_halo_finder_path)
+            except OSError:
+                pass
+
 
 
 
     @pytest.mark.skipif('not APH_MACHINE')
     def test_read_write(self):
         
-        manipulate_cache_log.overwrite_halo_table_cache_log(
-            self.halo_table_cache_log_table1, cache_fname=self.dummy_cache_log_fname)
+        for cache_fname, table in zip(
+            self.dummy_cache_log_fnames, self.cache_log_tables_to_test):
 
-        table2 = manipulate_cache_log.read_halo_table_cache_log(cache_fname=self.dummy_cache_log_fname)
+            manipulate_cache_log.overwrite_halo_table_cache_log(
+                table, cache_fname=cache_fname)
 
-        assert set(self.halo_table_cache_log_table1.keys()).issubset(set(table2.keys()))
-        assert not set(table2.keys()).issubset(set(self.halo_table_cache_log_table1.keys()))
+            table2 = manipulate_cache_log.read_halo_table_cache_log(cache_fname=cache_fname)
 
-        for key in self.halo_table_cache_log_table1.keys():
-            assert np.all(self.halo_table_cache_log_table1[key] == table2[key])
+            assert set(table.keys()).issubset(set(table2.keys()))
+            assert not set(table2.keys()).issubset(set(table.keys()))
+
+            for key in table.keys():
+                assert np.all(table[key] == table2[key])
 
     @pytest.mark.xfail
     def test_update_halo_table_cache_log(self):
@@ -113,7 +144,8 @@ class TestCacheManipulation(TestCase):
         raise HalotoolsError("The erase_halo_table_cache_log_entry function is not implemented yet.")
 
     def tearDown(self):
-        os.system('rm -rf ' + self.temp_dirname)
+        for name in self.temp_cache_dirnames:
+            os.system('rm -rf ' + name)
 
 
 
