ba9f47939dad227f8de3978afd539333c105ed05
Andrew Hearin <andrew.hearin@yale.edu>
2015-12-14 11:56:16 -0500

    Added a few new empty test modules

---
+++ halotools/sim_manager/tests/test_update_halo_table_cache_log.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function)
+
+from unittest import TestCase
+import pytest 
+import warnings, os
+
+import numpy as np 
+from copy import copy, deepcopy 
+
+from astropy.table import Table
+from astropy.config.paths import _find_home 
+
+from .. import manipulate_cache_log
+
+from ...custom_exceptions import HalotoolsError
+
+### Determine whether the machine is mine
+# This will be used to select tests whose 
+# returned values depend on the configuration 
+# of my personal cache directory files
+aph_home = u'/Users/aphearin'
+detected_home = _find_home()
+if aph_home == detected_home:
+    APH_MACHINE = True
+else:
+    APH_MACHINE = False
+
+__all__ = ('TestUpdateHaloTableCacheLog', )
+
+class TestUpdateHaloTableCacheLog(TestCase):
+    """ 
+    """
+
+    def setUp(self):
+        """ Pre-load various arrays into memory for use by all tests. 
+        """
+        pass
+        
