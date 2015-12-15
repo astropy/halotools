414c5f8375d7c7e0dedf5515953ba05beb73de32
Andrew Hearin <andrew.hearin@yale.edu>
2015-12-15 17:06:31 -0500

    Added test function that fails to demonstrate where I left off. testmod halotools/sim_manager/tests/test_store_new_halo_table_in_cache.py  See the log for notes about where I left off.

---
+++ halotools/sim_manager/tests/test_store_new_halo_table_in_cache.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function)
+
+from unittest import TestCase
+import pytest 
+import warnings, os
+
+import numpy as np 
+from copy import copy, deepcopy 
+
+from astropy.table import Table
+from astropy.table import vstack as table_vstack
+
+from astropy.config.paths import _find_home 
+
+from . import helper_functions 
+
+from .. import manipulate_cache_log, halo_catalog
+
+from ...custom_exceptions import HalotoolsError
+
+### Determine whether the machine is mine
+# This will be used to select tests whose 
+# returned values depend on the configuration 
+# of my personal cache directory files
+aph_home = u'/Users/aphearin'
+detected_home = _find_home()
+if aph_home == detected_home:
+    APH_MACHINE = True
+else:
+    APH_MACHINE = False
+
+__all__ = ('TestStoreNewHaloTable' )
+
+
+class TestStoreNewHaloTable(TestCase):
+    """ Class to test manipulate_cache_log.store_new_halo_table_in_cache
+    """
+
+    def setUp(self):
+        """ Pre-load various arrays into memory for use by all tests. 
+        """
+        self.dummy_cache_baseloc = helper_functions.dummy_cache_baseloc
+
+        try:
+            os.system('rm -rf ' + self.dummy_cache_baseloc)
+        except OSError:
+            pass
+
+    def test_basic_behavior(self):
+        raise HalotoolsError("Pick up here next time")
+        
