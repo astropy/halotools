c1afbeb586b939fc8c908b4e036a7656ea9639b9
Andrew Hearin <andrew.hearin@yale.edu>
2015-12-15 13:23:08 -0500

    Reworked OverhauledHaloCatalog object to use the manipulate_cache_log module

--- halotools/sim_manager/halo_catalog.py
+++ halotools/sim_manager/halo_catalog.py
@@ -2,6 +2,7 @@ import numpy as np
 import os, sys, urllib2, fnmatch
 from warnings import warn 
 
+from copy import deepcopy 
 from astropy import cosmology
 from astropy import units as u
 from astropy.table import Table
@@ -12,7 +13,7 @@ except ImportError:
     warn("Most of the functionality of the sim_manager sub-package requires h5py to be installed,\n"
         "which can be accomplished either with pip or conda")
 
-from . import sim_defaults, catalog_manager
+from . import sim_defaults, catalog_manager, manipulate_cache_log
 
 from ..utils.array_utils import custom_len, convert_to_ndarray
 from ..custom_exceptions import HalotoolsError
@@ -26,40 +27,52 @@ class OverhauledHaloCatalog(object):
     Container class for halo catalogs and particle data.  
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, preload_halo_table = False, **kwargs):
         """
         """
 
         try:
             import h5py
         except ImportError:
-            raise HalotoolsError("Must have h5py package installed to use this feature")
-        self._catman = catalog_manager.CatalogManager()
+            raise HalotoolsError("Must have h5py package installed "
+                "to use OverhauledHaloCatalog objects")
 
-        self._process_constructor_inputs(**kwargs)
+        self.fname_halo_table = self._retrieve_fname_halo_table(**kwargs)
 
-        self.fname_halo_table = self._catman.get_fname_halo_table(
-            simname = simname, halo_finder = halo_finder, 
-            redshift = redshift, version_name = version_name, 
-            dz_tol = dz_tol)
+        if preload_halo_table is True:
+            self._load_halo_table()
 
-        self._check_metadata_consistency(
-            simname = simname, halo_finder = halo_finder, 
-            redshift = redshift, version_name = version_name, 
-            )
-
-        self._load_halo_table()
         self._bind_metadata()
 
-    def _process_constructor_inputs(self, **kwargs):
+    def _retrieve_fname_halo_table(self, **kwargs):
         """
         """
-        pass
+        if 'fname' in kwargs:
+            fname = kwargs['fname']
+            del kwargs['fname']
+            return (
+                manipulate_cache_log.return_halo_table_fname_after_verification(
+                    fname, **kwargs)
+                )
+        else:
+            return (
+                manipulate_cache_log.return_halo_table_fname_from_simname_inputs(**kwargs)
+                )
+
+
+    @property 
+    def halo_table(self):
+        """
+        `~astropy.table.Table` object storing a catalog of dark matter halos. 
+        """
+        if not hasattr(self, '_halo_table'):
+            self._load_halo_table()
+        return self._halo_table
 
     def _load_halo_table(self):
         """ Retrieve the halo_table from ``self.fname_halo_table`` and update the cache log. 
         """
-        pass
+        self._halo_table = Table.read(self.fname_halo_table, path='data')
 
     def _bind_metadata(self):
         """ Create convenience bindings of all metadata to the `HaloCatalog` instance. 
@@ -67,12 +80,7 @@ class OverhauledHaloCatalog(object):
         f = h5py.File(self.fname_halo_table)
         for attr_key in f.attrs.keys():
             setattr(self, attr_key, f.attrs[attr])
-
-    def _check_metadata_consistency(self, **kwargs):
-        """ Check that any metadata passed to the constructor 
-        agrees with any metadata bound to the hdf5 file storing the table. 
-        """
-        pass
+        f.close()
 
     def store_halocat_in_cache(self, fname_halo_table, overwrite = False, 
         neglect_supplementary_metadata = False, store_ptcl_table = True, **kwargs):
