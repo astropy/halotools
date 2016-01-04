2c341ee2d4737628e58e9601bc3b464206c63f03
Andrew Hearin <andrew.hearin@yale.edu>
2016-01-04 17:28:25 -0500

    Finished first draft of rewrite of rebuild_halo_table_cache_log.py. Still entirely untested

--- scripts/rebuild_halo_table_cache_log.py
+++ scripts/rebuild_halo_table_cache_log.py
@@ -15,16 +15,14 @@ except ImportError:
 from halotools.sim_manager import manipulate_cache_log
 from halotools.custom_exceptions import HalotoolsError
 from halotools.sim_manager.halo_table_cache import HaloTableCache
+from halotools.sim_manager.log_entry import HaloTableCacheLogEntry
 
-halo_table_cache = HaloTableCache()
-if os.path.isfile(halo_table_cache.cache_log_fname):
-    has_existing_log = True
-else:
-    has_existing_log = False
+old_cache = HaloTableCache()
+old_cache_log_exists = os.path.isfile(old_cache.cache_log_fname)
 
-cache_log_dirname = os.path.dirname(halo_table_cache.cache_log_fname)
-corrupted_cache_log_fname = 'corrupted_halo_table_cache_log.txt'
-corrupted_cache_log_fname = os.path.join(cache_log_dirname, corrupted_cache_log_fname)
+cache_log_dirname = os.path.dirname(old_cache.cache_log_fname)
+corrupted_cache_log_basename = 'corrupted_halo_table_cache_log.txt'
+corrupted_cache_log_fname = os.path.join(cache_log_dirname, corrupted_cache_log_basename)
 
 rejected_filename_log_fname = 'rejected_halo_table_filenames.txt'
 rejected_filename_log_fname = os.path.join(cache_log_dirname, rejected_filename_log_fname)
@@ -41,11 +39,11 @@ if os.path.isfile(corrupted_cache_log_fname):
         "so here is how to proceed.\n"
         "Use a text editor to manually compare the "
         "corrupted and working copies of the cache log:\n\n"
-        + halo_table_cache.cache_log_fname + "\n"
+        + old_cache.cache_log_fname + "\n"
         + corrupted_cache_log_fname + "\n\n"
         "For any row of the corrupted log corresponding to a halo catalog \n"
         "that you would like to be recognized in your cache,\n"
-        "copy this row into a new row of " + os.path.basename(halo_table_cache.cache_log_fname) + "\n"
+        "copy this row into a new row of " + os.path.basename(old_cache.cache_log_fname) + "\n"
         "Depending on the state of the corrupted log, "
         "you may need to enter in the metadata columns manually.\n"
         "When you have finished, delete the " + os.path.basename(corrupted_cache_log_fname) + "file \n"
@@ -53,7 +51,7 @@ if os.path.isfile(corrupted_cache_log_fname):
         "Once the corrupted log has been removed from \n" + os.path.dirname(corrupted_cache_log_fname) + ",\n"
         "you can run the rebuild_halo_table_cache_log.py again.\n"
         "This script will then repeate the verification process on all entries of " 
-        + os.path.basename(halo_table_cache.cache_log_fname) + "\n\n\n"
+        + os.path.basename(old_cache.cache_log_fname) + "\n\n\n"
         )
     raise HalotoolsError(msg)
 
@@ -62,7 +60,7 @@ def fnames_in_existing_log():
     """ If there is an existing log, try and extract a list of filenames from it. 
     """
     try:
-        names = [entry.fname for entry in halo_table_cache.log]
+        names = [entry.fname for entry in old_cache.log]
         existing_log_is_corrupted = False
         return names
     except:
@@ -74,7 +72,7 @@ def halo_table_fnames_in_standard_cache():
     $HOME/.astropy/cache/halotools/halo_catalogs and yield the absolute path 
     to any file with a .hdf5 extension. 
     """
-    standard_loc = os.path.join(os.path.dirname(halo_table_cache.cache_log_fname), 'halo_catalogs')
+    standard_loc = os.path.join(os.path.dirname(old_cache.cache_log_fname), 'halo_catalogs')
     if os.path.exists(standard_loc):
         for path, dirlist, filelist in os.walk(standard_loc):
             for name in fnmatch.filter(filelist, '*.hdf5'):
@@ -95,89 +93,72 @@ potential_fnames.extend(list(fnames_in_rejected_filename_log()))
 # remove any possibly duplicated entries
 potential_fnames = list(set(potential_fnames))
 
-def verified_fname_generator():
-    """
-    """
-    for fname in potential_fnames:
-        try:
-            verified_fname = manipulate_cache_log.verify_file_storing_unrecognized_halo_table(fname)
-            yield verified_fname
-        except:
-            pass
+rejected_fnames = []
+potential_log_entries = []
+for fname in potential_fnames:
+    result = old_cache.determine_log_entry_from_fname(fname)
+    if type(result) is HaloTableCacheLogEntry:
+        potential_log_entries.append(result)
+    else:
+        rejected_fnames.append((fname, result))
+potential_log_entries = list(set(potential_log_entries))
 
-def rejected_fname_generator():
-    """
-    """
-    for fname in potential_fnames:
-        try:
-            verified_fname = manipulate_cache_log.verify_file_storing_unrecognized_halo_table(fname)
-        except:
-            yield fname
+new_cache = HaloTableCache(read_log_from_standard_loc = False)
+for log_entry in potential_log_entries:
+    if log_entry.safe_for_cache == True:
+        new_cache.add_entry_to_cache_log(log_entry, update_ascii = False)
+    else:
+        rejected_fnames.append((log_entry.fname, log_entry._cache_safety_message))
 
-verified_fnames = list(verified_fname_generator())
+
 print("\nNumber of files passing verification tests = " 
-    + str(len(verified_fnames)) + "\n")
+    + str(len(new_cache.log)) + "\n")
 
-rejected_fnames = list(rejected_fname_generator())
 print("\nNumber of files that fail verification tests = " 
     + str(len(rejected_fnames)) + "\n")
+
 # We are now done with the existing rejected_fnames file. 
 if os.path.isfile(rejected_filename_log_fname):
     os.system('rm ' + rejected_filename_log_fname)
 
-new_log = Table()
-new_log['fname'] = verified_fnames
-new_log['simname'] = object
-new_log['halo_finder'] = object
-new_log['version_name'] = object
-new_log['redshift'] = 0.
-
-for ii, entry in enumerate(new_log):
-    fname = entry['fname']
-    f = h5py.File(fname)
-    new_log['simname'][ii] = str(f.attrs['simname'])
-    new_log['halo_finder'][ii] = str(f.attrs['halo_finder'])
-    new_log['version_name'][ii] = str(f.attrs['version_name'])
-    new_log['redshift'][ii] = np.round(float(f.attrs['redshift']), 4)
-    f.close()
-
-
-if has_existing_log is True:
-    os.system('mv ' + halo_table_cache.cache_log_fname + ' ' + corrupted_cache_log_fname)
+if old_cache_log_exists:
+    os.system('mv ' + old_cache.cache_log_fname + ' ' + corrupted_cache_log_fname)
 
 if len(new_log) > 0:
-    manipulate_cache_log.overwrite_halo_table_cache_log(new_log, cache_fname=halo_table_cache.cache_log_fname)
+    new_cache._overwrite_log_ascii(new_cache.log)
 
-    print("\n")
-    print("\n")
-    print("The following filenames have been verified and will be added to your new cache log:\n")
-    for entry in new_log:
-        print(entry['fname'])
-    print("\n")
-    print("The new cache log is stored in the following location:\n" + halo_table_cache.cache_log_fname)
-
-if len(rejected_fnames) > 0:
-    print("The following filenames have been detected but rejected and will NOT be added to your new cache log:\n")
-    for entry in rejected_fnames:
+    print("\n\n")
+    print("The following log entries have been verified "
+        "and added to your new cache log:\n")
+    for entry in new_cache.log:
         print(entry)
     print("\n")
+    print("The new cache log is stored "
+        "in the following location:\n" + new_cache.cache_log_fname)
+
+if len(rejected_fnames) > 0:
+    print("There were some filenames that were "
+        "rejected and will NOT be added to your new cache log.\n"
+        "The reason for the rejection appears below each filename.")
+    for ii, entry in enumerate(rejected_fnames):
+        print("Rejected file #" + str(ii) + "\n")
+        print(entry[0])
+        print("\n")
+        print(entry[1])
+
+    print("\n")
+
     with open(rejected_filename_log_fname, 'w') as f:
         for name in rejected_fnames:
             f.write(name + "\n")
     print("These rejected filenames are now stored in the following location:\n"
         + rejected_filename_log_fname + "\n")
-    if has_existing_log is True:
+
+    if old_cache_log_exists:
         print("Before running this script, you already had an existing cache log.\n"
-            "This log was not readable by Halotools.\n"
-            "This file has been saved and is now stored in the following location:\n" + corrupted_cache_log_fname)
-    print("\n")
-    print("For any file in " + os.path.basename(rejected_filename_log_fname) + ",\n"
-    "you can find out why the file was rejected by reading the traceback triggered by the following syntax:\n\n"
-        ">>> from halotools.sim_manager import manipulate_cache_log \n"
-        ">>> manipulate_cache_log.verify_file_storing_unrecognized_halo_table(fname) \n")
-    print("In many cases, the reason for rejection is simple and can be easily rectified.\n"
-        "For example, you may only need to add a little metadata to the .hdf5 file\n"
-        "When you are done repairing any inconsistencies in the file, you can simply run this script again\n")
+            "This file has been saved "
+            "and is now stored in the following location:\n" 
+            + corrupted_cache_log_fname)
     print("\n")
 
 
