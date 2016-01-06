981a6e1a22c749fb50639879f627ae3fe0e54adf
Andrew Hearin <andrew.hearin@yale.edu>
2016-01-06 13:50:11 -0500

    Improved reporthook in io_utils.download_file_from_url

--- halotools/utils/io_utils.py
+++ halotools/utils/io_utils.py
@@ -3,7 +3,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-
+from time import time
 import sys, urllib
 
 __all__ = ['file_len', 'download_file_from_url']
@@ -29,16 +29,24 @@ def download_file_from_url(url, fname):
         Location and filename to store the downloaded file, e.g., 
         ``/Users/username/dirname/possibly_new_filename.txt``
     """
-
+    start = time()
     print("\n... Downloading data from the following location: \n%s\n" % url)
     print(" ... Saving the data with the following filename: \n%s\n" % fname) 
 
-    def reporthook(a,b,c): 
-        print("% 3.1f%% of %d bytes\r" % (min(100, float(a * b) / c * 100), c),)
+    def reporthook(blocks_thus_far, bytes_per_block, file_size_in_bytes): 
+        try:
+            blocks_in_file = int(round(file_size_in_bytes/bytes_per_block))
+            printout_condition = int(round(blocks_in_file/20))
+            if (blocks_thus_far % printout_condition == 0) & (blocks_thus_far > 0):
+                frac_complete = blocks_thus_far / float(blocks_in_file)
+                runtime = time() - start
+                print("{0:.0f}% complete, elapsed time = {1:.0f} seconds".format(frac_complete*100, runtime))
+
+        except ZeroDivisionError:
+            pass
         sys.stdout.flush()
 
     urllib.urlretrieve(url, fname, reporthook)
-
 
 
 
