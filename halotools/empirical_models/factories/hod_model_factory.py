25411b26ecf267452c7d7fcee0915ad90644b7d7
Andrew Hearin <andrew.hearin@yale.edu>
2016-01-21 09:43:25 -0500

    Added list_of_haloprops_needed feature to factories

--- halotools/empirical_models/factories/hod_model_factory.py
+++ halotools/empirical_models/factories/hod_model_factory.py
@@ -738,6 +738,8 @@ class HodModelFactory(ModelFactory):
                 haloprop_list.append(component_model.prim_haloprop_key)
             if hasattr(component_model, 'sec_haloprop_key'):
                 haloprop_list.append(component_model.sec_haloprop_key)
+            if hasattr(component_model, 'list_of_haloprops_needed'):
+                haloprop_list.extend(component_model.list_of_haloprops_needed)
 
         self._haloprop_list = list(set(haloprop_list))
 
