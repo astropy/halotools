62f81413bac46a15f1de3896edd7e86b26596d1b
Andrew Hearin <andrew.hearin@yale.edu>
2016-01-29 10:51:06 -0500

    Still developing tutorial2. Added teesting function for the rst file

--- docs/quickstart_and_tutorials/model_building/composing_models/hod_modeling/hod_modeling_tutorial2.rst
+++ docs/quickstart_and_tutorials/model_building/composing_models/hod_modeling/hod_modeling_tutorial2.rst
@@ -5,3 +5,54 @@ Example 2: An HOD-style model with additional features
 ****************************************************************
 
 .. currentmodule:: halotools.empirical_models
+
+In this section of the :ref:`hod_modeling_tutorial0`, 
+we'll build a composite model that is not part of the Halotools code base 
+by composing together a collection of Halotools-provided features. 
+Before reading on, be sure you have read and understood 
+:ref:`hod_modeling_tutorial1`. 
+
+Overview of the new model
+=============================
+
+The model we'll build will be based on the ``leauthaud11`` HOD, 
+but we'll add an additional component model that governs whether or not 
+our centrals and satellites are quiescent or star-forming. 
+The new component model we'll use is `HaloMassInterpolQuenching`. 
+In this model, galaxies are assigned a boolean designation as to whether or 
+not they are quiescent. Briefly, the way the model works is that you specify 
+what the quiescent fraction is at a set of input control points in halo mass, 
+and the model interpolates between these control points to calculate the 
+quiescent fraction at any mass. See the `HaloMassInterpolQuenching` docstring for details. 
+
+Source code for the new model 
+===============================
+
+.. code:: python
+
+	from halotools.empirical_models import HodModelFactory
+
+	from halotools.empirical_models import TrivialPhaseSpace, Leauthaud11Cens
+	another_cens_occ_model =  Leauthaud11Cens()
+	another_cens_prof_model = TrivialPhaseSpace()
+
+	from halotools.empirical_models import NFWPhaseSpace, Leauthaud11Sats
+	another_sats_occ_model =  Leauthaud11Sats()
+	another_sats_prof_model = NFWPhaseSpace()
+
+	from halotools.empirical_models import HaloMassInterpolQuenching
+	sat_quenching = HaloMassInterpolQuenching('halo_mvir', [1e12, 1e13, 1e14, 1e15], [0.35, 0.5, 0.6, 0.9], gal_type = 'satellites')
+	cen_quenching = HaloMassInterpolQuenching('halo_mvir', [1e12, 1e15], [0.25, 0.95], gal_type = 'centrals')
+
+	model_instance = HodModelFactory(
+		centrals_occupation = another_cens_occ_model, 
+		centrals_profile = another_cens_prof_model, 
+		satellites_occupation = another_sats_occ_model, 
+		satellites_profile = another_sats_prof_model, 
+		centrals_quenching = cen_quenching, 
+		satellites_quenching = sat_quenching
+		)
+
+
+
+
--- halotools/tests/docs_code_block_tests/test_hod_model_factory_tutorial.py
+++ halotools/tests/docs_code_block_tests/test_hod_model_factory_tutorial.py
@@ -40,6 +40,57 @@ class TestHodModelFactoryTutorial(TestCase):
         # that is generated on-the-fly, but you can use the populate_mock 
         # method with any Halotools-formatted catalog 
 
+    @pytest.mark.slow
+    def test_hod_modeling_tutorial2(self):
+
+        from ...empirical_models import HodModelFactory
+
+        from ...empirical_models import TrivialPhaseSpace, Leauthaud11Cens
+        another_cens_occ_model =  Leauthaud11Cens()
+        another_cens_prof_model = TrivialPhaseSpace()
+
+        from ...empirical_models import NFWPhaseSpace, Leauthaud11Sats
+        another_sats_occ_model =  Leauthaud11Sats()
+        another_sats_prof_model = NFWPhaseSpace()
+
+        from ...empirical_models import HaloMassInterpolQuenching
+        sat_quenching = HaloMassInterpolQuenching('halo_mvir', [1e12, 1e13, 1e14, 1e15], [0.35, 0.5, 0.6, 0.9], gal_type = 'satellites')
+        cen_quenching = HaloMassInterpolQuenching('halo_mvir', [1e12, 1e15], [0.25, 0.95], gal_type = 'centrals')
+
+        model_instance = HodModelFactory(
+            centrals_occupation = another_cens_occ_model, 
+            centrals_profile = another_cens_prof_model, 
+            satellites_occupation = another_sats_occ_model, 
+            satellites_profile = another_sats_prof_model, 
+            centrals_quenching = cen_quenching, 
+            satellites_quenching = sat_quenching
+            )
+
+        assert hasattr(model_instance, 'mean_quiescent_fraction_centrals')
+        assert hasattr(model_instance, 'mean_quiescent_fraction_satellites')
+
+        assert 'centrals_quiescent_ordinates_param1' in model_instance.param_dict.keys()
+        assert 'centrals_quiescent_ordinates_param2' in model_instance.param_dict.keys()
+        assert 'satellites_quiescent_ordinates_param1' in model_instance.param_dict.keys()
+        assert 'satellites_quiescent_ordinates_param4' in model_instance.param_dict.keys()
+
+        model_instance.populate_mock(simname = 'fake')
+
+        assert 'quiescent' in model_instance.mock.galaxy_table.keys()
+        assert set(model_instance.mock.galaxy_table['quiescent']) == {True, False}
+
+        cenmask = model_instance.mock.galaxy_table['gal_type'] == 'centrals'
+        cens = model_instance.mock.galaxy_table[cenmask]
+        assert set(cens['quiescent']) == {True, False}
+
+        satmask = model_instance.mock.galaxy_table['gal_type'] == 'satellites'
+        sats = model_instance.mock.galaxy_table[satmask]
+        assert set(sats['quiescent']) == {True, False}
+
+
+
+
+
 
 
 
