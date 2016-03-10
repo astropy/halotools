2a8898f68e21f88a0c3bb163fc69695d0859ffb7
Andrew Hearin <andrew.hearin@yale.edu>
2016-03-10 10:25:57 -0500

    Added large_scale_density_spherical_annulus function

--- halotools/mock_observables/large_scale_density_helpers.py
+++ halotools/mock_observables/large_scale_density_helpers.py
@@ -8,7 +8,8 @@ import numpy as np
 from ..utils.array_utils import convert_to_ndarray, array_is_monotonic
 from ..custom_exceptions import *
 
-__all__ = ('_large_scale_density_spherical_volume_process_args', )
+__all__ = ('_large_scale_density_spherical_volume_process_args', 
+    '_large_scale_density_spherical_annulus_process_args')
 
 
 def _large_scale_density_spherical_volume_process_args(
@@ -44,6 +45,43 @@ def _large_scale_density_spherical_volume_process_args(
 
     return sample, tracers, rbins, period, sample_volume, num_threads, approx_cell1_size
 
+def _large_scale_density_spherical_annulus_process_args(
+    sample, tracers, inner_radius, outer_radius, 
+    period, sample_volume, num_threads, approx_cell1_size):
+    """
+    """
+    sample = convert_to_ndarray(sample)
+    tracers = convert_to_ndarray(tracers)
+
+    try:
+        assert outer_radius > inner_radius
+    except AssertionError:
+        msg = ("Input ``outer_radius`` must be larger than input ``inner_radius``")
+        raise HalotoolsError(msg)
+    rbins = np.array([inner_radius, outer_radius])
+
+    if period is None:
+        if sample_volume is None:
+            msg = ("If period is None, you must pass in ``sample_volume``.")
+            raise HalotoolsError(msg)
+        else:
+            sample_volume = float(sample_volume)
+    else:
+        period = convert_to_ndarray(period)
+        if len(period) == 1:
+            period = np.array([period, period, period])
+        elif len(period) == 3:
+            pass
+        else:
+            msg = ("\nInput ``period`` must either be a float or length-3 sequence")
+            raise HalotoolsError(msg)
+        if sample_volume is None:
+            sample_volume = period.prod()
+        else:
+            msg = ("If period is not None, do not pass in sample_volume")
+            raise HalotoolsError(msg)
+
+    return sample, tracers, rbins, period, sample_volume, num_threads, approx_cell1_size
 
 
 
