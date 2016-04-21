f2a7abe62cb8c5830bdd1337d4024fc01449f865
Andrew Hearin <andrew.hearin@yale.edu>
2016-04-21 16:16:41 -0400

    Added many new tests of npairs_projected. 10 of 10 tests in test_npairs_projected pass.

---
+++ halotools/mock_observables/pair_counters/test_pair_counters/test_npairs_projected.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function,
+    unicode_literals)
+
+import numpy as np
+import pytest 
+
+from ..npairs_projected import npairs_projected
+from ..pairs import xy_z_npairs as pure_python_brute_force_npairs_projected
+
+from ...tests.cf_helpers import generate_locus_of_3d_points
+from ...tests.cf_helpers import generate_3d_regular_mesh
+
+__all__ = ('test_rectangular_mesh_pairs_tight_locus_xy1', )
+
+def test_rectangular_mesh_pairs_tight_locus_xy1():
+    """ Verify that `halotools.mock_observables.npairs_projected` returns 
+    the correct counts for two tight loci of points. 
+
+    In this test, PBCs are irrelevant
+    """
+    npts1, npts2 = 100, 90
+    data1 = generate_locus_of_3d_points(npts1, xc=0.1, yc=0.1, zc=0.1)
+    data2 = generate_locus_of_3d_points(npts2, xc=0.1, yc=0.2, zc=0.1)
+
+    rp_bins = np.array((0.05, 0.15, 0.3))
+    pi_max = np.max(rp_bins)
+
+    result = npairs_projected(data1, data2, rp_bins, pi_max, period=1)
+    assert np.all(result == [0, npts1*npts2, npts1*npts2])
+
+def test_rectangular_mesh_pairs_tight_locus_z1():
+    """ Verify that `halotools.mock_observables.npairs_projected` returns 
+    the correct counts for two tight loci of points. 
+
+    In this test, PBCs are irrelevant
+    """
+    npts1, npts2 = 100, 110
+    data1 = generate_locus_of_3d_points(npts1, xc=0.1, yc=0.1, zc=0.1)
+    data2 = generate_locus_of_3d_points(npts2, xc=0.1, yc=0.1, zc=0.2)
+
+    rp_bins = np.array((0.05, 0.15, 0.3))
+
+    pi_max = 0.05
+    result = npairs_projected(data1, data2, rp_bins, pi_max, period=1)
+    assert np.all(result == [0, 0, 0])
+
+    pi_max = 0.15
+    result = npairs_projected(data1, data2, rp_bins, pi_max, period=1)
+    assert np.all(result == [npts1*npts2, npts1*npts2, npts1*npts2])
+
+def test_rectangular_mesh_pairs_tight_locus_xy2():
+    """ Verify that `halotools.mock_observables.npairs_projected` returns 
+    the correct counts for two tight loci of points. 
+
+    In this test, PBCs are important.
+    """
+    npts1, npts2 = 100, 300
+    data1 = generate_locus_of_3d_points(npts1, xc=0.1, yc=0.05, zc=0.1)
+    data2 = generate_locus_of_3d_points(npts2, xc=0.1, yc=0.95, zc=0.1)
+
+    rp_bins = np.array((0.05, 0.15, 0.3))
+    pi_max = 0.1
+
+    result = npairs_projected(data1, data2, rp_bins, pi_max, period=1)
+    assert np.all(result == [0, npts1*npts2, npts1*npts2])
+
+    result = npairs_projected(data1, data2, rp_bins, pi_max)
+    assert np.all(result == [0, 0, 0])
+
+def test_rectangular_mesh_pairs_tight_locus_z2():
+    """ Verify that `halotools.mock_observables.npairs_projected` returns 
+    the correct counts for two tight loci of points. 
+
+    In this test, PBCs are important.
+    """
+    npts1, npts2 = 100, 101
+    data1 = generate_locus_of_3d_points(npts1, xc=0.1, yc=0.1, zc=0.05)
+    data2 = generate_locus_of_3d_points(npts2, xc=0.1, yc=0.2, zc=0.95)
+
+    rp_bins = np.array((0.05, 0.15, 0.3))
+
+    pi_max = 0.2
+    result = npairs_projected(data1, data2, rp_bins, pi_max, period=1)
+    assert np.all(result == [0, npts1*npts2, npts1*npts2])
+
+    pi_max = 0.05
+    result = npairs_projected(data1, data2, rp_bins, pi_max, period=1)
+    assert np.all(result == [0, 0, 0])
+
+    pi_max = 0.2
+    result = npairs_projected(data1, data2, rp_bins, pi_max)
+    assert np.all(result == [0, 0, 0])
+
+    pi_max = 0.05
+    result = npairs_projected(data1, data2, rp_bins, pi_max)
+    assert np.all(result == [0, 0, 0])
+
+def test_rectangular_mesh_pairs_tight_locus_cell1_sizes():
+    """ Verify that the pair counters return the correct results
+    when operating on a tight locus of points.
+
+    For this test, PBCs have no impact.
+    """
+    npts1, npts2 = 100, 90
+    data1 = generate_locus_of_3d_points(npts1, xc=0.1, yc=0.1, zc=0.1)
+    data2 = generate_locus_of_3d_points(npts2, xc=0.1, yc=0.2, zc=0.1)
+
+    rp_bins = np.array((0.05, 0.15, 0.3))
+    pi_max = np.max(rp_bins)
+
+    result1 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = 0.1)
+    assert np.all(result1 == [0, npts1*npts2, npts1*npts2])
+
+    result2 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = [0.1, 0.1, 0.1])
+    assert np.all(result2 == [0, npts1*npts2, npts1*npts2])
+
+    result3 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = [0.3, 0.3, 0.3])
+    assert np.all(result3 == [0, npts1*npts2, npts1*npts2])
+
+    result4 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = [0.1, 0.3, 0.3])
+    assert np.all(result4 == [0, npts1*npts2, npts1*npts2])
+
+    result5 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = [0.1, 0.2, 0.3])
+    assert np.all(result5 == [0, npts1*npts2, npts1*npts2])
+
+
+def test_rectangular_mesh_pairs_tight_locus_cell2_sizes():
+    """ Verify that the pair counters return the correct results
+    when operating on a tight locus of points.
+
+    For this test, PBCs have no impact.
+    """
+    npts1, npts2 = 100, 90
+    data1 = generate_locus_of_3d_points(npts1, xc=0.1, yc=0.1, zc=0.1)
+    data2 = generate_locus_of_3d_points(npts2, xc=0.1, yc=0.2, zc=0.1)
+
+    rp_bins = np.array((0.05, 0.15, 0.3))
+    pi_max = np.max(rp_bins)
+
+    result1 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell2_size = 0.1)
+    assert np.all(result1 == [0, npts1*npts2, npts1*npts2])
+
+    result2 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell2_size = [0.1, 0.1, 0.1])
+    assert np.all(result2 == [0, npts1*npts2, npts1*npts2])
+
+    result3 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell2_size = [0.3, 0.3, 0.3])
+    assert np.all(result3 == [0, npts1*npts2, npts1*npts2])
+
+    result4 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell2_size = [0.1, 0.3, 0.3])
+    assert np.all(result4 == [0, npts1*npts2, npts1*npts2])
+
+    result5 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell2_size = [0.1, 0.2, 0.3])
+    assert np.all(result5 == [0, npts1*npts2, npts1*npts2])
+
+def test_rectangular_mesh_pairs_tight_locus_cell1_cell2_sizes():
+    """ Verify that the pair counters return the correct results
+    when operating on a tight locus of points.
+
+    For this test, PBCs have no impact.
+    """
+    npts1, npts2 = 100, 90
+    data1 = generate_locus_of_3d_points(npts1, xc=0.1, yc=0.1, zc=0.1)
+    data2 = generate_locus_of_3d_points(npts2, xc=0.1, yc=0.2, zc=0.1)
+
+    rp_bins = np.array((0.05, 0.15, 0.3))
+    pi_max = np.max(rp_bins)
+
+    result1 = npairs_projected(data1, data2, rp_bins, pi_max, period=1,
+        approx_cell1_size = 0.1, approx_cell2_size = 0.1)
+    assert np.all(result1 == [0, npts1*npts2, npts1*npts2])
+
+    result1 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = 0.2, approx_cell2_size = 0.1)
+    assert np.all(result1 == [0, npts1*npts2, npts1*npts2])
+
+    result1 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = 0.1, approx_cell2_size = 0.2)
+    assert np.all(result1 == [0, npts1*npts2, npts1*npts2])
+
+    result2 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = [0.1, 0.1, 0.1], approx_cell2_size = [0.1, 0.1, 0.1])
+    assert np.all(result2 == [0, npts1*npts2, npts1*npts2])
+
+    result2 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = [0.1, 0.1, 0.1], approx_cell2_size = [0.2, 0.2, 0.2])
+    assert np.all(result2 == [0, npts1*npts2, npts1*npts2])
+
+    result2 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = [0.2, 0.2, 0.2], approx_cell2_size = [0.1, 0.1, 0.1])
+    assert np.all(result2 == [0, npts1*npts2, npts1*npts2])
+
+    result3 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell2_size = [0.3, 0.3, 0.3])
+    assert np.all(result3 == [0, npts1*npts2, npts1*npts2])
+
+    result3 = npairs_projected(data1, data2, rp_bins, pi_max, period=1, 
+        approx_cell1_size = [0.1, 0.2, 0.3], approx_cell2_size = [0.23, 0.32, 0.11])
+    assert np.all(result3 == [0, npts1*npts2, npts1*npts2])
+
+def test_rectangular_mesh_pairs1():
+    """ Verify that `halotools.mock_observables.npairs_projected` returns 
+    the correct counts for two regularly spaced grids of points in the limit 
+    where pi_max << grid_spacing
+    """
+    npts_per_dim = 10
+    data1 = generate_3d_regular_mesh(npts_per_dim)
+    data2 = generate_3d_regular_mesh(npts_per_dim)
+    grid_spacing = 1./npts_per_dim
+    Lbox = 1.
+    r1 = grid_spacing/100.
+    epsilon = 0.0001
+    r2 = grid_spacing + epsilon
+    r3 = grid_spacing*np.sqrt(2) + epsilon
+    r4 = grid_spacing*np.sqrt(3) + epsilon
+    rp_bins = np.array([r1, r2, r3, r4])
+    pi_max = grid_spacing/100.
+    result = npairs_projected(data1, data2, rp_bins, pi_max, 
+        period=Lbox, approx_cell1_size=0.1)
+    assert np.all(result == 
+        [npts_per_dim**3, 5*npts_per_dim**3, 9*npts_per_dim**3, 9*npts_per_dim**3])
+
+def test_rectangular_mesh_pairs2():
+    """ Verify that `halotools.mock_observables.npairs_projected` returns 
+    the correct counts for two regularly spaced grids of points in the limit 
+    where grid_spacing < pi_max < 2*grid_spacing exceeds the spacing of the grid
+    """
+    npts_per_dim = 10
+    data1 = generate_3d_regular_mesh(npts_per_dim)
+    data2 = generate_3d_regular_mesh(npts_per_dim)
+    grid_spacing = 1./npts_per_dim
+    Lbox = 1.
+    r1 = grid_spacing/100.
+    epsilon = 0.0001
+    r2 = grid_spacing + epsilon
+    r3 = grid_spacing*np.sqrt(2) + epsilon
+    r4 = grid_spacing*np.sqrt(3) + epsilon
+    rp_bins = np.array([r1, r2, r3, r4])
+    pi_max = 1.1*grid_spacing
+    result = npairs_projected(data1, data2, rp_bins, pi_max, 
+        period=Lbox, approx_cell1_size=0.1)
+    assert np.all(result == 
+        [3*npts_per_dim**3, 15*npts_per_dim**3, 27*npts_per_dim**3, 27*npts_per_dim**3])
+
+def test_parallel():
+    """ Verify that `halotools.mock_observables.npairs_projected` returns 
+    identical counts whether it is run in serial or parallel. 
+    """
+    npts_per_dim = 10
+    data1 = generate_3d_regular_mesh(npts_per_dim)
+    data2 = generate_3d_regular_mesh(npts_per_dim)
+    grid_spacing = 1./npts_per_dim
+    Lbox = 1.
+    r1 = grid_spacing/100.
+    epsilon = 0.0001
+    r2 = grid_spacing + epsilon
+    r3 = grid_spacing*np.sqrt(2) + epsilon
+    r4 = grid_spacing*np.sqrt(3) + epsilon
+    rp_bins = np.array([r1, r2, r3, r4])
+    pi_max = 0.1
+
+    serial_result = npairs_projected(data1, data2, rp_bins, pi_max, 
+        period=Lbox, approx_cell1_size=0.1)
+    parallel_result2 = npairs_projected(data1, data2, rp_bins, pi_max, 
+        period=Lbox, approx_cell1_size=0.1, num_threads = 2)
+    parallel_result7 = npairs_projected(data1, data2, rp_bins, pi_max, 
+        period=Lbox, approx_cell1_size=0.1, num_threads = 7)
+    assert np.all(serial_result == parallel_result2)
+    assert np.all(serial_result == parallel_result7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
