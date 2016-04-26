b90c81238c2cfe926f43a93a7b825d8e910bfa9d
Andrew Hearin <andrew.hearin@yale.edu>
2016-04-26 09:51:45 -0400

    cpairs.pyx has been removed from the repo

--- halotools/mock_observables/pair_counters/cpairs/setup_package.py
+++ halotools/mock_observables/pair_counters/cpairs/setup_package.py
@@ -2,7 +2,7 @@ from distutils.extension import Extension
 import os
 
 PATH_TO_PKG = os.path.relpath(os.path.dirname(__file__))
-SOURCES = ("cpairs.pyx", "distances.pyx", "pairwise_distances.pyx",
+SOURCES = ("distances.pyx", "pairwise_distances.pyx",
     "per_object_cpairs.pyx", "npairs_3d_engine.pyx", "npairs_projected_engine.pyx", 
     "npairs_xy_z_engine.pyx", "npairs_jackknife_3d_engine.pyx", "npairs_s_mu_engine.pyx")
 THIS_PKG_NAME = '.'.join(__name__.split('.')[:-1])
