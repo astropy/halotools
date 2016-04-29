c1bc7384939d99f49f5820d61166e3da071bf6ef
Andrew Hearin <andrew.hearin@yale.edu>
2016-04-29 16:14:44 -0400

    Added in two new tests of mean_radial_velocity_vs_r

--- halotools/mock_observables/tests/test_mean_radial_velocity_vs_r.py
+++ halotools/mock_observables/tests/test_mean_radial_velocity_vs_r.py
@@ -6,8 +6,8 @@ from astropy.tests.helper import pytest
 from ..pairwise_velocity_stats import mean_radial_velocity_vs_r
 from .cf_helpers import generate_locus_of_3d_points
 
-__all__ = ('test_mean_radial_velocity_vs_r_auto_consistency', 
-    'test_mean_radial_velocity_vs_r_cross_consistency')
+__all__ = ('test_mean_radial_velocity_vs_r_correctness1a', 
+    'test_mean_radial_velocity_vs_r_correctness1b')
 
 
 
@@ -76,36 +76,69 @@ def test_mean_radial_velocity_vs_r_correctness1b():
     assert np.allclose(s1s1[1], -50, rtol=0.01)
 
 
-# @pytest.mark.slow
-# def test_mean_radial_velocity_vs_r_correctness_pbc():
-#     """ Create two tight localizations of points, 
-#     one at (0.5, 0.5, 0.1), the other at (0.5, 0.5, 0.2). 
-#     The first set of points is moving at -50 in the z-direction, 
-#     the second set of points is at rest. 
-#     Verify that mean_radial_velocity_vs_r returns -50 
-#     for the outer bin. 
-#     Same test as test_mean_radial_velocity_vs_r_correctness, 
-#     only here we apply PBC, which should not matter in this case. 
-#     """
-#     np.random.seed(43)
+@pytest.mark.slow
+def test_mean_radial_velocity_vs_r_correctness2a():
+    """ Create two tight localizations of points, 
+    one at (0.5, 0.5, 0.1), the other at (0.5, 0.5, 0.2). 
+    The first set of points is moving at -50 in the z-direction, 
+    the second set of points is at rest. 
 
-#     npts = 200
-#     sample1 = generate_locus_of_3d_points(npts, 
-#         xc=0.5, yc=0.5, zc=0.1, epsilon = 0.0001)
-#     velocities1 = np.zeros(npts*3).reshape(npts, 3)
-#     velocities1[:,2] = 50.
+    In this example PBCs are important and we pass in a sample2
+    """
+    np.random.seed(43)
 
-#     sample2 = generate_locus_of_3d_points(npts, 
-#         xc=0.5, yc=0.5, zc=0.25, epsilon = 0.0001)
-#     velocities2 = np.zeros(npts*3).reshape(npts, 3)
+    npts = 200
+    sample1 = generate_locus_of_3d_points(npts, 
+        xc=0.5, yc=0.5, zc=0.1, epsilon = 0.0001)
+    velocities1 = np.zeros(npts*3).reshape(npts, 3)
+    velocities1[:,2] = -50.
 
-#     rbins = np.array([0, 0.1, 0.3])
-#     s1s1, s1s2, s2s2 = mean_radial_velocity_vs_r(sample1, velocities1, rbins, 
-#         sample2 = sample2, velocities2 = velocities2, period=1., num_threads = 'max')
+    sample2 = generate_locus_of_3d_points(npts, 
+        xc=0.5, yc=0.5, zc=0.95, epsilon = 0.0001)
+    velocities2 = np.zeros(npts*3).reshape(npts, 3)
 
-#     assert np.allclose(s1s1[0], 0, rtol=0.01)
-#     assert np.allclose(s2s2[0], 0, rtol=0.01)
-#     assert np.allclose(s1s2[1], -50, rtol=0.01)
+    rbins = np.array([0, 0.1, 0.3])
+    s1s1, s1s2, s2s2 = mean_radial_velocity_vs_r(sample1, velocities1, rbins, 
+        sample2 = sample2, velocities2 = velocities2, period=1)
+
+    assert np.allclose(s1s1[0], 0, rtol=0.01)
+    assert np.allclose(s1s2[0], 0, rtol=0.01)
+    assert np.allclose(s2s2[0], 0, rtol=0.01)
+
+    assert np.allclose(s1s1[1], 0, rtol=0.01)
+    assert np.allclose(s1s2[1], -50, rtol=0.01)
+    assert np.allclose(s2s2[1], 0, rtol=0.01)
+
+@pytest.mark.slow
+def test_mean_radial_velocity_vs_r_correctness2b():
+    """ Create two tight localizations of points, 
+    one at (0.5, 0.5, 0.1), the other at (0.5, 0.5, 0.2). 
+    The first set of points is moving at -50 in the z-direction, 
+    the second set of points is at rest. These two sets of points 
+    are concatenated together into a single sample1.
+
+    In this example PBCs are important and we only pass in a sample1. 
+    """
+    np.random.seed(43)
+
+    npts = 200
+    sample1 = generate_locus_of_3d_points(npts, 
+        xc=0.5, yc=0.5, zc=0.1, epsilon = 0.0001)
+    velocities1 = np.zeros(npts*3).reshape(npts, 3)
+    velocities1[:,2] = -50.
+
+    sample2 = generate_locus_of_3d_points(npts, 
+        xc=0.5, yc=0.5, zc=0.95, epsilon = 0.0001)
+    velocities2 = np.zeros(npts*3).reshape(npts, 3)
+
+    sample = np.concatenate((sample1, sample2))
+    velocities = np.concatenate((velocities1, velocities2))
+
+    rbins = np.array([0, 0.1, 0.3])
+    s1s1 = mean_radial_velocity_vs_r(sample, velocities, rbins, period=1)
+
+    assert np.allclose(s1s1[0], 0, rtol=0.01)
+    assert np.allclose(s1s1[1], -50, rtol=0.01)
 
 # @pytest.mark.slow
 # def test_mean_radial_velocity_vs_r_correctness_pbc2():
