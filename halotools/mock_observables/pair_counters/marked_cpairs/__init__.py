e947e510aa265c5c59fa3ac4c0b19e7ff3c45f7a
Andrew Hearin <andrew.hearin@yale.edu>
2016-05-04 09:22:38 -0400

    Added new marked npairs engines

--- halotools/mock_observables/pair_counters/marked_cpairs/__init__.py
+++ halotools/mock_observables/pair_counters/marked_cpairs/__init__.py
@@ -4,3 +4,5 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 
 from .marked_cpairs import *
 from .conditional_pairwise_distances import *
+from .marked_npairs_3d_engine import marked_npairs_3d_engine
+from .marked_npairs_xy_z_engine import marked_npairs_xy_z_engine
--- halotools/mock_observables/pair_counters/marked_cpairs/setup_package.py
+++ halotools/mock_observables/pair_counters/marked_cpairs/setup_package.py
@@ -5,7 +5,11 @@ PATH_TO_PKG = os.path.relpath(os.path.dirname(__file__))
 SOURCES = ("marked_cpairs.pyx", "weighting_functions.pyx", 
     "custom_weighting_func.pyx",
     "pairwise_velocity_funcs.pyx","distances.pyx", 
-    "conditional_pairwise_distances.pyx")
+    "conditional_pairwise_distances.pyx", 
+    "marked_npairs_3d_engine.pyx", 
+    "marked_npairs_xy_z_engine.pyx", 
+    "custom_marking_func.pyx", 
+    "marking_functions.pyx")
 THIS_PKG_NAME = '.'.join(__name__.split('.')[:-1])
 
 def get_extensions():
