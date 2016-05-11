c70c98a0915af5479446b33d6314b98908e95811
Andrew Hearin <andrew.hearin@yale.edu>
2016-05-11 11:44:26 -0400

    Finished testing radial profile code

--- halotools/mock_observables/radial_profiles/tests/test_radial_profile_3d.py
+++ halotools/mock_observables/radial_profiles/tests/test_radial_profile_3d.py
@@ -87,7 +87,7 @@ def test_radial_profile_3d_test2():
     assert np.all(counts == (npts1/2.)*(npts2/2.))
     assert np.all(result == [0.5, 1.5])
 
-def test_absolute_normalized_agreement():
+def test_absolute_vs_normalized_agreement():
     npts1, npts2 = 100, 90
     with NumpyRNGContext(fixed_seed):
         sample1 = np.random.random((npts1, 3))
@@ -98,10 +98,8 @@ def test_absolute_normalized_agreement():
     fixed_rvir = 0.1
     rbins_normalized = rbins_absolute/fixed_rvir
 
-    print("rbins_absolute case")
     result1, counts1 = radial_profile_3d(sample1, sample2, quantity2, 
         rbins_absolute=rbins_absolute, return_counts=True, period=1)
-    print("rbins_normalized case")
     result2, counts2 = radial_profile_3d(sample1, sample2, quantity2, 
         rbins_normalized=rbins_normalized, 
         normalize_rbins_by = np.zeros(npts1) + fixed_rvir, 
@@ -109,8 +107,6 @@ def test_absolute_normalized_agreement():
     assert np.all(counts1 == counts2)
     assert np.allclose(result1, result2, rtol = 0.001)
 
-
-@pytest.mark.xfail
 def test_radial_profile_3d_test3():
     """ Create a regular mesh of ``sample1`` points and two concentric rings around 
     two different points in the mesh. Give random uniform weights to the rings, 
@@ -120,7 +116,7 @@ def test_radial_profile_3d_test3():
     npts1 = 100
     sample1 = generate_3d_regular_mesh(4) # coords = 0.125, 0.375, 0.625, 0.875
 
-    rbins = np.array([0.04, 0.06, 0.1])
+    rbins_absolute = np.array([0.04, 0.06, 0.1])
     r1, r2 = 0.05, 0.09
 
     xca1, yca1, zca1 = 0.125, 0.125, 0.125
@@ -138,12 +134,12 @@ def test_radial_profile_3d_test3():
 
     quantity = np.concatenate([inner_ring_values, outer_ring_values])
 
-    result, counts = radial_profile_3d(sample1, sample2, quantity, rbins, period=1, return_counts=True)
+    result, counts = radial_profile_3d(sample1, sample2, quantity, 
+        rbins_absolute = rbins_absolute, period=1, return_counts=True)
 
     assert np.all(counts == npts2/2)
     assert np.allclose(result, [np.mean(inner_ring_values), np.mean(outer_ring_values)], rtol = 0.001)
 
-@pytest.mark.xfail
 def test_radial_profile_3d_test4():
     """ For two tight localizations of sample1 points each surrounded by two concentric 
     shells of sample2 points, verify that both the counts and the primary result 
@@ -163,7 +159,7 @@ def test_radial_profile_3d_test4():
 
     rvir = 0.013
     rvir_array = np.zeros(npts1) + rvir
-    rbins = np.array([0.5, 1, 2])
+    rbins_normalized = np.array([0.5, 1, 2])
     r1, r2 = 0.75*rvir, 1.5*rvir
 
     sample2_p1_r1 = generate_thin_shell_of_3d_points(npts1, r1, xca1, yca1, zca1, seed=fixed_seed, Lbox=1)
@@ -176,7 +172,7 @@ def test_radial_profile_3d_test4():
     quantity_a, quantity_b = np.zeros(npts2/2) + 0.5, np.zeros(npts2/2) + 1.5
     quantity = np.concatenate([quantity_a, quantity_b])
 
-    result, counts = radial_profile_3d(sample1, sample2, quantity, rbins, 
+    result, counts = radial_profile_3d(sample1, sample2, quantity, rbins_normalized=rbins_normalized, 
         return_counts=True, period=1, normalize_rbins_by=rvir_array)
     correct_counts = (npts1/2.)*(npts2/2.)
     assert np.all(counts == correct_counts)
