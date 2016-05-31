0f856f04ddc0617dcf035f356b0a49779a0d4d49
Andrew Hearin <andrew.hearin@yale.edu>
2016-05-31 16:13:25 -0400

    Added skeleton module for testing pairwise_velocities_helpers module

---
+++ halotools/mock_observables/pairwise_velocities/tests/test_pairwise_velocity_helpers.py
@@ -0,0 +1,10 @@
+"""
+"""
+from __future__ import (absolute_import, division, print_function)
+
+import numpy as np
+from astropy.tests.helper import pytest
+
+from ..pairwise_velocities_helpers import _pairwise_velocity_stats_process_args
+
+
