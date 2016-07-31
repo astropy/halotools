c43a86d5d0427719472363030adc9d851d246d6f
Andrew Hearin <andrew.hearin@yale.edu>
2016-07-31 14:08:42 -0400

    Discovered bug revealed by test_s_mu_auto_consistency

--- halotools/mock_observables/two_point_clustering/tests/test_s_mu_tpcf.py
+++ halotools/mock_observables/two_point_clustering/tests/test_s_mu_tpcf.py
@@ -46,3 +46,53 @@ def test_s_mu_tpcf_auto_periodic():
         max_sample_size=int(1e4), estimator='Natural')
 
     assert result_1.ndim == 2, "correlation function returned has wrong dimension."
+
+
+def test_s_mu_cross_consistency():
+    """
+    """
+    Npts1, Npts2 = 100, 200
+
+    with NumpyRNGContext(fixed_seed):
+        sample1 = np.random.random((Npts1, 3))
+        sample2 = np.random.random((Npts2, 3))
+    period = np.array([1.0, 1.0, 1.0])
+    s_bins = np.linspace(0.001, 0.3, 5)
+    mu_bins = np.linspace(0, 1.0, 10)
+
+    result_11a, result_12a, result_22a = s_mu_tpcf(
+        sample1, s_bins, mu_bins, sample2=sample2,
+        period=period, do_auto=True, do_cross=True)
+
+    result_12b = s_mu_tpcf(
+        sample1, s_bins, mu_bins, sample2=sample2,
+        period=period, do_auto=False, do_cross=True)
+
+    assert np.allclose(result_12a, result_12b)
+
+
+def test_s_mu_auto_consistency():
+    """
+    """
+    Npts1, Npts2 = 100, 200
+
+    with NumpyRNGContext(fixed_seed):
+        sample1 = np.random.random((Npts1, 3))
+        sample2 = np.random.random((Npts2, 3))
+    period = np.array([1.0, 1.0, 1.0])
+    s_bins = np.linspace(0.001, 0.3, 5)
+    mu_bins = np.linspace(0, 1.0, 10)
+
+    result_11a, result_12a, result_22a = s_mu_tpcf(
+        sample1, s_bins, mu_bins, sample2=sample2,
+        period=period, do_auto=True, do_cross=True)
+
+    result_11b, result_22b = s_mu_tpcf(
+        sample1, s_bins, mu_bins, sample2=sample2,
+        period=period, do_auto=True, do_cross=False)
+
+    assert np.allclose(result_11a, result_11b)
+    assert np.allclose(result_22a, result_22b)
+
+
+
