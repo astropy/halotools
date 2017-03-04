f247cc0ee465f2e967e85b67ea54a98741ef34d9
Andrew Hearin <andrew.hearin@yale.edu>
2017-03-04 10:45:46 -0500

    Refactored test_one_two_halo_decomposition_on_mock so that it can be run in any environment with the bolshoi z=0 catalog, not just on APH_MACHINE

--- halotools/empirical_models/factories/tests/test_hod_mock_factory.py
+++ halotools/empirical_models/factories/tests/test_hod_mock_factory.py
@@ -2,9 +2,7 @@
 """
 from __future__ import (absolute_import, division, print_function)
 
-from unittest import TestCase
 from astropy.tests.helper import pytest
-from astropy.config.paths import _find_home
 
 import numpy as np
 from copy import deepcopy
@@ -16,12 +14,6 @@ from ....sim_manager.fake_sim import FakeSimHalosNearBoundaries
 from ..prebuilt_model_factory import PrebuiltHodModelFactory
 from ....custom_exceptions import HalotoolsError
 
-aph_home = '/Users/aphearin'
-detected_home = _find_home()
-if aph_home == detected_home:
-    APH_MACHINE = True
-else:
-    APH_MACHINE = False
 
 __all__ = ('test_estimate_ngals1', )
 
@@ -240,13 +232,15 @@ def test_satellite_positions1():
 
 
 @pytest.mark.slow
-@pytest.mark.skipif('not APH_MACHINE')
 def test_one_two_halo_decomposition_on_mock():
     """ Enforce that the one-halo term is exactly zero
     on sufficiently large scales.
     """
     model = PrebuiltHodModelFactory('zheng07', threshold=-21)
-    bolshoi_halocat = CachedHaloCatalog(simname='bolshoi')
+    try:
+        bolshoi_halocat = CachedHaloCatalog(simname='bolshoi')
+    except:
+        return
     model.populate_mock(bolshoi_halocat)
     gals = model.mock.galaxy_table
     pos = return_xyz_formatted_array(gals['x'], gals['y'], gals['z'])
@@ -256,37 +250,3 @@ def test_one_two_halo_decomposition_on_mock():
     xi_1h, xi_2h = tpcf_one_two_halo_decomp(pos, halo_hostid, rbins,
         period=model.mock.Lbox, num_threads='max')
     assert xi_1h[-1] == -1
-
-##################################################
-
-
-##################################################
-##################################################
-# class TestHodMockFactory(TestCase):
-#     """ Class providing tests of the `~halotools.empirical_models.HodMockFactory`.
-#     """
-
-#     def setUp(self):
-#         self.model = PrebuiltHodModelFactory('zheng07', threshold=-21)
-#         self.fakesim = FakeSimHalosNearBoundaries()
-
-#         self.model.populate_mock(self.fakesim)
-
-#         self.galaxy_table1 = deepcopy(self.model.mock.galaxy_table)
-
-#         def f100x(t):
-#             return t['halo_x'] > 100
-#         self.model.mock.populate(masking_function=f100x)
-#         self.galaxy_table2 = deepcopy(self.model.mock.galaxy_table)
-
-
-
-
-
-#         del model
-
-
-#     def tearDown(self):
-#         del self.model
-#         del self.galaxy_table1
-#         del self.galaxy_table2
