819f542dfd4e574cd6445dcb9cf59267a0cc5d68
Andrew Hearin <andrew.hearin@yale.edu>
2017-03-08 11:23:48 -0500

    Added 2d rectangular mesh

--- halotools/mock_observables/pair_counters/mesh_helpers.py
+++ halotools/mock_observables/pair_counters/mesh_helpers.py
@@ -64,6 +64,52 @@ def _enclose_in_box(x1, y1, z1, x2, y2, z2, min_size=None):
     return x1, y1, z1, x2, y2, z2, Lbox
 
 
+def _enclose_in_square(x1, y1, x2, y2, min_size=None):
+    """
+    Build box which encloses all points, shifting the points so that
+    the "leftmost" point is (0, 0).
+
+    Parameters
+    ----------
+    x1, y1 : array_like
+        cartesian positions of points
+
+    x2, y2 : array_like
+        cartesian positions of points
+
+    min_size : array_like
+        minimum lengths of a side of the box.  If the minimum box constructed around the
+        points has a side i less than ``min_size[i]``, then the box is padded in order to
+        obtain the minimum specified size.
+
+    Returns
+    -------
+    x1, y1, x2, y2, Lbox
+        shifted positions and box size.
+    """
+    xmin = np.min([np.min(x1), np.min(x2)])
+    ymin = np.min([np.min(y1), np.min(y2)])
+    xmax = np.max([np.max(x1), np.max(x2)])
+    ymax = np.max([np.max(y1), np.max(y2)])
+
+    xymin = np.min([xmin, ymin])
+    xymax = np.max([xmax, ymax])-xymin
+
+    x1 = x1 - xymin
+    y1 = y1 - xymin
+    x2 = x2 - xymin
+    y2 = y2 - xymin
+
+    Lbox = np.array([xymax, xymax])
+
+    if min_size is not None:
+        min_size = np.atleast_1d(min_size)
+        if np.any(Lbox < min_size):
+            Lbox[(Lbox < min_size)] = min_size[(Lbox < min_size)]
+
+    return x1, y1, x2, y2, Lbox
+
+
 def _set_approximate_cell_sizes(approx_cell1_size, approx_cell2_size, period):
     """
     process the approximate cell size parameters.
@@ -90,6 +136,41 @@ def _set_approximate_cell_sizes(approx_cell1_size, approx_cell2_size, period):
         approx_cell2_size = np.atleast_1d(approx_cell2_size)
         try:
             assert len(approx_cell2_size) == 3
+            assert type(approx_cell2_size) is np.ndarray
+            assert approx_cell2_size.ndim == 1
+        except AssertionError:
+            msg = ("Input ``approx_cell2_size`` must be a length-3 sequence")
+            raise ValueError(msg)
+
+    return approx_cell1_size, approx_cell2_size
+
+
+def _set_approximate_2d_cell_sizes(approx_cell1_size, approx_cell2_size, period):
+    """
+    process the approximate cell size parameters.
+    If either is set to None, apply default settings.
+    """
+
+    #################################################
+    # Set the approximate cell sizes of the trees
+    if approx_cell1_size is None:
+        approx_cell1_size = period/10.0
+    else:
+        approx_cell1_size = np.atleast_1d(approx_cell1_size)
+        try:
+            assert len(approx_cell1_size) == 2
+            assert type(approx_cell1_size) is np.ndarray
+            assert approx_cell1_size.ndim == 1
+        except AssertionError:
+            msg = ("Input ``approx_cell1_size`` must be a length-3 sequence")
+            raise ValueError(msg)
+
+    if approx_cell2_size is None:
+        approx_cell2_size = copy(approx_cell1_size)
+    else:
+        approx_cell2_size = np.atleast_1d(approx_cell2_size)
+        try:
+            assert len(approx_cell2_size) == 2
             assert type(approx_cell2_size) is np.ndarray
             assert approx_cell2_size.ndim == 1
         except AssertionError:
