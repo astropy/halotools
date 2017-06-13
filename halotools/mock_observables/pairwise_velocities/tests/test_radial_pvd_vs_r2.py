5ce68c5415f79228d8a09085542020443c5eccd8
Andrew Hearin <andrew.hearin@yale.edu>
2017-06-12 20:40:30 -0700

    Ported over all previous tests of radial velocity profile to testing of new implementation

--- halotools/mock_observables/pairwise_velocities/tests/test_radial_pvd_vs_r2.py
+++ halotools/mock_observables/pairwise_velocities/tests/test_radial_pvd_vs_r2.py
@@ -57,212 +57,177 @@ def test_radial_pvd_vs_r_correctness1():
     assert np.allclose(s1s2[1], correct_cross_pvd, rtol=0.001)
 
 
-# @pytest.mark.slow
-# def test_radial_pvd_vs_r_correctness2():
-#     """ This function tests that the
-#     `~halotools.mock_observables.radial_pvd_vs_r` function returns correct
-#     results for a controlled distribution of points whose radial velocity
-#     can be simply calculated.
+@pytest.mark.slow
+def test_radial_pvd_vs_r_correctness2():
+    """ This function tests that the
+    `~halotools.mock_observables.radial_pvd_vs_r` function returns correct
+    results for a controlled distribution of points whose radial velocity
+    can be simply calculated.
 
-#     For this test, the configuration is two tight localizations of points,
-#     the first at (0.5, 0.5, 0.1), the second at (0.5, 0.5, 0.95).
-#     The first set of points is moving with random uniform z-velocities;
-#     the second set of points is at rest.
+    For this test, the configuration is two tight localizations of points,
+    the first at (0.5, 0.5, 0.1), the second at (0.5, 0.5, 0.95).
+    The first set of points is moving with random uniform z-velocities;
+    the second set of points is at rest.
 
-#     PBCs are operative in this test.
+    PBCs are operative in this test.
 
-#     For every velocity in sample1, since we can count pairs analytically
-#     for this configuration we know exactly how many appearances of each
-#     velocity there will be, so we can calculate np.std on the exact
-#     same set of points as the marked pair-counter should operate on.
-#     """
-#     npts = 100
+    For every velocity in sample1, since we can count pairs analytically
+    for this configuration we know exactly how many appearances of each
+    velocity there will be, so we can calculate np.std on the exact
+    same set of points as the marked pair-counter should operate on.
+    """
+    npts = 100
 
-#     xc1, yc1, zc1 = 0.5, 0.5, 0.1
-#     xc2, yc2, zc2 = 0.5, 0.5, 0.95
+    xc1, yc1, zc1 = 0.5, 0.5, 0.1
+    xc2, yc2, zc2 = 0.5, 0.5, 0.95
 
-#     sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
-#     sample2 = generate_locus_of_3d_points(npts, xc=xc2, yc=yc2, zc=zc2, seed=fixed_seed)
+    sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
+    sample2 = generate_locus_of_3d_points(npts, xc=xc2, yc=yc2, zc=zc2, seed=fixed_seed)
 
-#     velocities1 = np.zeros(npts*3).reshape(npts, 3)
-#     velocities2 = np.zeros(npts*3).reshape(npts, 3)
-#     with NumpyRNGContext(fixed_seed):
-#         velocities1[:, 2] = np.random.uniform(0, 1, npts)
+    velocities1 = np.zeros(npts*3).reshape(npts, 3)
+    velocities2 = np.zeros(npts*3).reshape(npts, 3)
+    with NumpyRNGContext(fixed_seed):
+        velocities1[:, 2] = np.random.uniform(0, 1, npts)
 
-#     rbins = np.array([0.001, 0.1, 0.3])
-#     s1s1, s1s2, s2s2 = radial_pvd_vs_r(sample1, velocities1, rbins,
-#         sample2=sample2, velocities2=velocities2, period=1)
+    rbins = np.array([0.001, 0.1, 0.3])
+    s1s2 = radial_pvd_vs_r(sample1, velocities1, rbins_absolute=rbins,
+        sample2=sample2, velocities2=velocities2, period=1)
 
-#     correct_cross_pvd = np.std(np.repeat(velocities1[:, 2], npts))
+    correct_cross_pvd = np.std(np.repeat(velocities1[:, 2], npts))
 
-#     assert np.allclose(s1s2[0], 0, rtol=0.1)
-#     assert np.allclose(s1s2[1], correct_cross_pvd, rtol=0.001)
-
-
-# @pytest.mark.slow
-# def test_radial_pvd_vs_r_correctness3():
-#     """ This function tests that the
-#     `~halotools.mock_observables.radial_pvd_vs_r` function returns correct
-#     results for a controlled distribution of points whose radial velocity
-#     can be simply calculated.
-
-#     For this test, the configuration is two tight localizations of points,
-#     the first at (0.5, 0.5, 0.1), the second at (0.5, 0.5, 0.25).
-#     The first set of points is moving with random uniform z-velocities;
-#     the second set of points is at rest.
-
-#     PBCs are turned off in this test.
-
-#     For every velocity in sample1, since we can count pairs analytically
-#     for this configuration we know exactly how many appearances of each
-#     velocity there will be, so we can calculate np.std on the exact
-#     same set of points as the marked pair-counter should operate on.
-
-#     This is the same test as test_radial_pvd_vs_r_correctness3, only here we
-#     bundle the two sets of points into the same sample.
-#     """
-#     npts = 100
-
-#     xc1, yc1, zc1 = 0.5, 0.5, 0.1
-#     xc2, yc2, zc2 = 0.5, 0.5, 0.25
-
-#     sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
-#     sample2 = generate_locus_of_3d_points(npts, xc=xc2, yc=yc2, zc=zc2, seed=fixed_seed)
-
-#     velocities1 = np.zeros(npts*3).reshape(npts, 3)
-#     velocities2 = np.zeros(npts*3).reshape(npts, 3)
-#     with NumpyRNGContext(fixed_seed):
-#         velocities1[:, 2] = np.random.uniform(0, 1, npts)
-
-#     sample = np.concatenate((sample1, sample2))
-#     velocities = np.concatenate((velocities1, velocities2))
-
-#     rbins = np.array([0.001, 0.1, 0.3])
-#     s1s1 = radial_pvd_vs_r(sample, velocities, rbins)
-
-#     correct_cross_pvd = np.std(np.repeat(velocities1[:, 2], npts))
-
-#     assert np.allclose(s1s1[1], correct_cross_pvd, rtol=0.001)
+    assert np.allclose(s1s2[0], 0, rtol=0.1)
+    assert np.allclose(s1s2[1], correct_cross_pvd, rtol=0.001)
 
 
-# @pytest.mark.slow
-# def test_radial_pvd_vs_r_correctness4():
-#     """ This function tests that the
-#     `~halotools.mock_observables.radial_pvd_vs_r` function returns correct
-#     results for a controlled distribution of points whose radial velocity
-#     can be simply calculated.
+@pytest.mark.slow
+def test_radial_pvd_vs_r_correctness3():
+    """ This function tests that the
+    `~halotools.mock_observables.radial_pvd_vs_r` function returns correct
+    results for a controlled distribution of points whose radial velocity
+    can be simply calculated.
 
-#     For this test, the configuration is two tight localizations of points,
-#     the first at (0.5, 0.5, 0.1), the second at (0.5, 0.5, 0.95).
-#     The first set of points is moving with random uniform z-velocities;
-#     the second set of points is at rest.
+    For this test, the configuration is two tight localizations of points,
+    the first at (0.5, 0.5, 0.1), the second at (0.5, 0.5, 0.25).
+    The first set of points is moving with random uniform z-velocities;
+    the second set of points is at rest.
 
-#     PBCs are operative in this test.
+    PBCs are turned off in this test.
 
-#     For every velocity in sample1, since we can count pairs analytically
-#     for this configuration we know exactly how many appearances of each
-#     velocity there will be, so we can calculate np.std on the exact
-#     same set of points as the marked pair-counter should operate on.
-#     """
-#     npts = 100
+    For every velocity in sample1, since we can count pairs analytically
+    for this configuration we know exactly how many appearances of each
+    velocity there will be, so we can calculate np.std on the exact
+    same set of points as the marked pair-counter should operate on.
 
-#     xc1, yc1, zc1 = 0.5, 0.5, 0.1
-#     xc2, yc2, zc2 = 0.5, 0.5, 0.95
+    This is the same test as test_radial_pvd_vs_r_correctness3, only here we
+    bundle the two sets of points into the same sample.
+    """
+    npts = 100
 
-#     sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
-#     sample2 = generate_locus_of_3d_points(npts, xc=xc2, yc=yc2, zc=zc2, seed=fixed_seed)
+    xc1, yc1, zc1 = 0.5, 0.5, 0.1
+    xc2, yc2, zc2 = 0.5, 0.5, 0.25
 
-#     velocities1 = np.zeros(npts*3).reshape(npts, 3)
-#     velocities2 = np.zeros(npts*3).reshape(npts, 3)
-#     with NumpyRNGContext(fixed_seed):
-#         velocities1[:, 2] = np.random.uniform(0, 1, npts)
+    sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
+    sample2 = generate_locus_of_3d_points(npts, xc=xc2, yc=yc2, zc=zc2, seed=fixed_seed)
 
-#     sample = np.concatenate((sample1, sample2))
-#     velocities = np.concatenate((velocities1, velocities2))
+    velocities1 = np.zeros(npts*3).reshape(npts, 3)
+    velocities2 = np.zeros(npts*3).reshape(npts, 3)
+    with NumpyRNGContext(fixed_seed):
+        velocities1[:, 2] = np.random.uniform(0, 1, npts)
 
-#     rbins = np.array([0.001, 0.1, 0.3])
-#     s1s1 = radial_pvd_vs_r(sample, velocities, rbins, period=1)
+    sample = np.concatenate((sample1, sample2))
+    velocities = np.concatenate((velocities1, velocities2))
 
-#     correct_cross_pvd = np.std(np.repeat(velocities1[:, 2], npts))
+    rbins = np.array([0.001, 0.1, 0.3])
+    s1s1 = radial_pvd_vs_r(sample, velocities, rbins_absolute=rbins)
 
-#     assert np.allclose(s1s1[1], correct_cross_pvd, rtol=0.001)
+    correct_cross_pvd = np.std(np.repeat(velocities1[:, 2], npts))
 
-
-# @pytest.mark.slow
-# def test_radial_pvd_vs_r1():
-#     """ Verify that different choices for the cell size has no
-#     impact on the results.
-#     """
-
-#     npts = 100
-
-#     xc1, yc1, zc1 = 0.5, 0.5, 0.1
-
-#     sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
-
-#     velocities1 = np.zeros(npts*3).reshape(npts, 3)
-#     with NumpyRNGContext(fixed_seed):
-#         velocities1[:, 2] = np.random.uniform(0, 1, npts)
-
-#     rbins = np.linspace(0, 0.3, 10)
-#     result1 = radial_pvd_vs_r(sample1, velocities1, rbins)
-#     result2 = radial_pvd_vs_r(sample1, velocities1, rbins,
-#         approx_cell1_size=[0.2, 0.2, 0.2])
-#     assert np.allclose(result1, result2, rtol=0.0001)
+    assert np.allclose(s1s1[1], correct_cross_pvd, rtol=0.001)
 
 
-# @pytest.mark.slow
-# def test_radial_pvd_vs_r_auto_consistency():
-#     """ Verify that we get self-consistent auto-correlation results
-#     regardless of whether we ask for cross-correlations.
-#     """
-#     npts = 100
+@pytest.mark.slow
+def test_radial_pvd_vs_r_correctness4():
+    """ This function tests that the
+    `~halotools.mock_observables.radial_pvd_vs_r` function returns correct
+    results for a controlled distribution of points whose radial velocity
+    can be simply calculated.
 
-#     xc1, yc1, zc1 = 0.5, 0.5, 0.1
-#     xc2, yc2, zc2 = 0.5, 0.5, 0.95
+    For this test, the configuration is two tight localizations of points,
+    the first at (0.5, 0.5, 0.1), the second at (0.5, 0.5, 0.95).
+    The first set of points is moving with random uniform z-velocities;
+    the second set of points is at rest.
 
-#     sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
-#     sample2 = generate_locus_of_3d_points(npts, xc=xc2, yc=yc2, zc=zc2, seed=fixed_seed)
+    PBCs are operative in this test.
 
-#     velocities1 = np.zeros(npts*3).reshape(npts, 3)
-#     velocities2 = np.zeros(npts*3).reshape(npts, 3)
-#     with NumpyRNGContext(fixed_seed):
-#         velocities1[:, 2] = np.random.uniform(0, 1, npts)
+    For every velocity in sample1, since we can count pairs analytically
+    for this configuration we know exactly how many appearances of each
+    velocity there will be, so we can calculate np.std on the exact
+    same set of points as the marked pair-counter should operate on.
+    """
+    npts = 100
 
-#     rbins = np.linspace(0, 0.3, 10)
-#     s1s1a, s1s2a, s2s2a = radial_pvd_vs_r(sample1, velocities1, rbins,
-#         sample2=sample2, velocities2=velocities2)
-#     s1s1b, s2s2b = radial_pvd_vs_r(sample1, velocities1, rbins,
-#         sample2=sample2, velocities2=velocities2,
-#         do_cross=False)
+    xc1, yc1, zc1 = 0.5, 0.5, 0.1
+    xc2, yc2, zc2 = 0.5, 0.5, 0.95
 
-#     assert np.allclose(s1s1a, s1s1b, rtol=0.001)
-#     assert np.allclose(s2s2a, s2s2b, rtol=0.001)
+    sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
+    sample2 = generate_locus_of_3d_points(npts, xc=xc2, yc=yc2, zc=zc2, seed=fixed_seed)
+
+    velocities1 = np.zeros(npts*3).reshape(npts, 3)
+    velocities2 = np.zeros(npts*3).reshape(npts, 3)
+    with NumpyRNGContext(fixed_seed):
+        velocities1[:, 2] = np.random.uniform(0, 1, npts)
+
+    sample = np.concatenate((sample1, sample2))
+    velocities = np.concatenate((velocities1, velocities2))
+
+    rbins = np.array([0.001, 0.1, 0.3])
+    s1s1 = radial_pvd_vs_r(sample, velocities, rbins_absolute=rbins, period=1)
+
+    correct_cross_pvd = np.std(np.repeat(velocities1[:, 2], npts))
+
+    assert np.allclose(s1s1[1], correct_cross_pvd, rtol=0.001)
 
 
-# @pytest.mark.slow
-# def test_radial_pvd_vs_r_cross_consistency():
-#     """ Verify that we get self-consistent cross-correlation results
-#     regardless of whether we ask for auto-correlations.
-#     """
-#     npts = 100
+@pytest.mark.slow
+def test_radial_pvd_vs_r1():
+    """ Verify that different choices for the cell size has no
+    impact on the results.
+    """
 
-#     xc1, yc1, zc1 = 0.5, 0.5, 0.1
-#     xc2, yc2, zc2 = 0.5, 0.5, 0.95
+    npts = 100
 
-#     sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
-#     sample2 = generate_locus_of_3d_points(npts, xc=xc2, yc=yc2, zc=zc2, seed=fixed_seed)
+    xc1, yc1, zc1 = 0.5, 0.5, 0.1
 
-#     velocities1 = np.zeros(npts*3).reshape(npts, 3)
-#     velocities2 = np.zeros(npts*3).reshape(npts, 3)
-#     with NumpyRNGContext(fixed_seed):
-#         velocities1[:, 2] = np.random.uniform(0, 1, npts)
+    sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
 
-#     rbins = np.linspace(0, 0.3, 10)
-#     s1s1a, s1s2a, s2s2a = radial_pvd_vs_r(sample1, velocities1, rbins,
-#         sample2=sample2, velocities2=velocities2)
-#     s1s2b = radial_pvd_vs_r(sample1, velocities1, rbins,
-#         sample2=sample2, velocities2=velocities2,
-#         do_auto=False)
+    velocities1 = np.zeros(npts*3).reshape(npts, 3)
+    with NumpyRNGContext(fixed_seed):
+        velocities1[:, 2] = np.random.uniform(0, 1, npts)
 
-#     assert np.allclose(s1s2a, s1s2b, rtol=0.001)
+    rbins = np.linspace(0, 0.3, 10)
+    result1 = radial_pvd_vs_r(sample1, velocities1, rbins_absolute=rbins)
+    result2 = radial_pvd_vs_r(sample1, velocities1, rbins_absolute=rbins,
+        approx_cell1_size=[0.2, 0.2, 0.2])
+    assert np.allclose(result1, result2, rtol=0.0001)
+
+
+@pytest.mark.slow
+def test_radial_pvd_vs_r_auto_consistency():
+    """ Verify that we get self-consistent auto-correlation results
+    regardless of how we do the cross-correlation.
+    """
+    npts = 100
+
+    xc1, yc1, zc1 = 0.5, 0.5, 0.1
+
+    sample1 = generate_locus_of_3d_points(npts, xc=xc1, yc=yc1, zc=zc1, seed=fixed_seed)
+    velocities1 = np.zeros(npts*3).reshape(npts, 3)
+    with NumpyRNGContext(fixed_seed):
+        velocities1[:, 2] = np.random.uniform(0, 1, npts)
+
+    rbins = np.linspace(0, 0.3, 10)
+    s1s1a = radial_pvd_vs_r(sample1, velocities1, rbins_absolute=rbins)
+    s1s1b = radial_pvd_vs_r(sample1, velocities1, rbins_absolute=rbins,
+        sample2=sample1, velocities2=velocities1)
+
+    assert np.allclose(s1s1a, s1s1b, rtol=0.001)
