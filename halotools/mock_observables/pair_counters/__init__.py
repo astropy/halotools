24b8dd3184a08493e9aa85b58d3b51d9d9ad39a4
Andrew Hearin <andrew.hearin@yale.edu>
2017-09-20 10:35:00 -0500

    Implemented python driver of weighted_npairs_s_mu function. Still needs an engine

--- halotools/mock_observables/pair_counters/__init__.py
+++ halotools/mock_observables/pair_counters/__init__.py
@@ -9,6 +9,7 @@ from .marked_npairs_3d import marked_npairs_3d
 from .marked_npairs_xy_z import marked_npairs_xy_z
 from .npairs_jackknife_3d import npairs_jackknife_3d
 from .npairs_s_mu import npairs_s_mu
+from .weighted_npairs_s_mu import weighted_npairs_s_mu
 from .npairs_per_object_3d import npairs_per_object_3d
 from .pairwise_distance_3d import pairwise_distance_3d
 from .pairwise_distance_xy_z import pairwise_distance_xy_z
