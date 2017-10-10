fixed covariance calculation in tpcf_jackknife function

halotools/mock_observables/two_point_clustering/wp_jackknife.py
@@ -309,9 +309,11 @@ def wp_jackknife(sample1, randoms, rp_bins, pi_max, Nsub=[5, 5, 5],
     xi_12_cov = np.matrix(np.cov(xi_12_sub.T, bias=True))*(N_sub_vol-1)
     xi_22_cov = np.matrix(np.cov(xi_22_sub.T, bias=True))*(N_sub_vol-1)
     
-    #xi_11_cov = calculate_cov(xi_11_sub)
-    #xi_12_cov = calculate_cov(xi_12_sub)
-    #xi_22_cov = calculate_cov(xi_22_sub)
+    #calculate the covariance matrix using the explicitly defined function below
+    #but you get the same result as above
+    #xi_11_cov =jackknife_covariance(xi_11_sub)
+    #xi_12_cov = jackknife_covariance(xi_12_sub)
+    #xi_22_cov = jackknife_covariance(xi_22_sub)
     
     if _sample1_is_sample2:
         return xi_11_full, xi_11_cov