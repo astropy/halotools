fixed error in covariance matrix caclulation

halotools/mock_observables/two_point_clustering/wp_jackknife.py
@@ -296,7 +296,7 @@ def wp_jackknife(sample1, randoms, rp_bins, pi_max, Nsub=[5, 5, 5],
     xi_12_sub = _TP_estimator(D1D2_sub, D1R_sub, RR_sub, N1_subs, N2_subs, NR_subs, NR_subs, estimator)
     xi_22_sub = _TP_estimator(D2D2_sub, D2R_sub, RR_sub, N2_subs, N2_subs, NR_subs, NR_subs, estimator)
     
-    #account for factor of 2*pi_max in integration
+    # account for factor of 2*pi_max in the integration
     xi_11_full = 2.0*pi_max*xi_11_full
     xi_12_full = 2.0*pi_max*xi_12_full
     xi_22_full = 2.0*pi_max*xi_22_full
@@ -305,10 +305,14 @@ def wp_jackknife(sample1, randoms, rp_bins, pi_max, Nsub=[5, 5, 5],
     xi_22_sub = 2.0*pi_max*xi_22_sub
     
     # calculate the covariance matrix
-    xi_11_cov = np.matrix(np.cov(xi_11_sub.T))
-    xi_12_cov = np.matrix(np.cov(xi_12_sub.T))
-    xi_22_cov = np.matrix(np.cov(xi_22_sub.T))
-
+    xi_11_cov = np.matrix(np.cov(xi_11_sub.T, bias=True))*(N_sub_vol-1)
+    xi_12_cov = np.matrix(np.cov(xi_12_sub.T, bias=True))*(N_sub_vol-1)
+    xi_22_cov = np.matrix(np.cov(xi_22_sub.T, bias=True))*(N_sub_vol-1)
+    
+    #xi_11_cov = calculate_cov(xi_11_sub)
+    #xi_12_cov = calculate_cov(xi_12_sub)
+    #xi_22_cov = calculate_cov(xi_22_sub)
+    
     if _sample1_is_sample2:
         return xi_11_full, xi_11_cov
     else:
@@ -319,6 +323,38 @@ def wp_jackknife(sample1, randoms, rp_bins, pi_max, Nsub=[5, 5, 5],
         elif do_cross is True:
             return xi_12_full, xi_12_cov
 
+def jackknife_covariance(x):
+    """
+    caclulate the jackknife covariance matrix
+    
+    Parameters
+    ----------
+    x : array_like
+        array of shape (N jacknife samples, N variables)
+    
+    Returns
+    -------
+    cov : numpy.matrix
+    
+    Notes
+    -----
+    This function returns the same result as np.cov(x.T, bias=True)*(Njk-1),
+    where Njk is the number of jackknife samples.
+    """
+    
+    Njk, Nvar = np.shape(x)
+    
+    mx = np.mean(x, axis=0)
+    
+    C = np.zeros((Nx,Nx))
+    for i in range(0,Nx):
+        for j in range(0,Nx):
+            for k in range(0,Nj):
+                C[i,j] = C[i,j]+(x[k,i]-mx[i])*(x[k,j]-mx[j])
+    
+    C = C*(Nj-1.0)/Nj
+    
+    return np.matrix(C)
 
 def jnpair_counts(sample1, sample2, j_index_1, j_index_2, N_sub_vol, rp_bins, pi_bins,
         period, num_threads, do_auto, do_cross, _sample1_is_sample2):