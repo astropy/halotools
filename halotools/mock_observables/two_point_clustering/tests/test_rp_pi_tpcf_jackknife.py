70765508dff2e097459ce287c44e1df22da680f8
Duncan Campbell <duncanc@andrew.cmu.edu>
2017-12-12 14:29:58 -0500

    added test to enforce consistency between wp_jackknife and rp_pi_tpcf_jackknife

--- halotools/mock_observables/two_point_clustering/tests/test_rp_pi_tpcf_jackknife.py
+++ halotools/mock_observables/two_point_clustering/tests/test_rp_pi_tpcf_jackknife.py
@@ -9,6 +9,8 @@ from astropy.utils.misc import NumpyRNGContext
 from ..rp_pi_tpcf_jackknife import rp_pi_tpcf_jackknife
 from ..rp_pi_tpcf import rp_pi_tpcf
 
+from ..wp_jackknife import wp_jackknife
+
 slow = pytest.mark.slow
 
 __all__ = ('test_tpcf_jackknife_corr_func', )
@@ -137,4 +139,38 @@ def test_do_auto_false():
     result1 = rp_pi_tpcf_jackknife(sample1, randoms, rp_bins, pi_bins,
         period=period, Nsub=3, num_threads=1, sample2=sample2,
         do_auto=False)
+
+@pytest.mark.slow
+def test_consitency_with_wp_jackknife():
+    """
+    Verify the result and covariance matrix is consistent with wp_jackknife
+    """
+    Npts = 300
+    with NumpyRNGContext(fixed_seed):
+        sample1 = np.random.random((Npts, 3))
+        randoms = np.random.random((Npts*3, 3))
+
+    
+    #use one large pi_bin
+    pi_max=0.1
+    pi_min=0.0
+    pi_bins_this_test = np.array([pi_min,pi_max])
+
+    result_1, err_1 = rp_pi_tpcf_jackknife(sample1, randoms, rp_bins, pi_bins_this_test,
+        Nsub=3, period=period, num_threads=1)
+
+    result_2, err_2 = wp_jackknife(sample1, randoms, rp_bins, pi_max=pi_max,
+        Nsub=3, period=period, num_threads=1)
+    
+    #account for wp integration
+    result_1 = result_1.flatten()*2.0*np.diff(pi_bins_this_test)
+
+    assert np.all(result_1 == result_2), "tpcf is not consistent between rp_pi_tpcf and wp"
+    
+    #account for wp integration
+    factor = (2.0*np.diff(pi_bins_this_test))**2.0
+    err_1 = err_1*factor[0]
+
+    assert np.allclose(err_1,err_2), "cov matrix is not consoistent with wp_jackknife"
+
 
