10cc29cfa0e17e7066bd755625ce7879ebd32c84
Andrew Hearin <andrew.hearin@yale.edu>
2017-12-14 21:44:21 -0600

    Added _sphericity_from_matrices and _triaxility_from_matrices convenience kernels

--- halotools/mock_observables/tensor_calculations/inertia_tensor.py
+++ halotools/mock_observables/tensor_calculations/inertia_tensor.py
@@ -27,6 +27,23 @@ def _principal_axes_from_matrices(matrices):
     return evecs[:, :, 2], evals[:, 2]
 
 
+def _sphericity_from_matrices(matrices):
+    evals, __ = np.linalg.eigh(matrices)
+    third_evals = evals[:, 0]
+    first_evals = evals[:, 2]
+    sphericity = third_evals/first_evals
+    return sphericity
+
+
+def _triaxility_from_matrices(matrices):
+    evals, __ = np.linalg.eigh(matrices)
+    third_evals = evals[:, 0]
+    second_evals = evals[:, 1]
+    first_evals = evals[:, 2]
+    triaxility = (first_evals**2 - second_evals**2)/(first_evals**2 - third_evals**2)
+    return triaxility
+
+
 def inertia_tensors_principal_axes(sample1, sample2, weights, smoothing_scale):
     """
     """
