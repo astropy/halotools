6980b4b0f3794e12ba6fe1a209c839bde992c41c
Andrew Hearin <andrew.hearin@yale.edu>
2017-12-15 20:29:35 -0600

    Added elementwise_norm function to matrix_operations_3d module

--- halotools/utils/matrix_operations_3d.py
+++ halotools/utils/matrix_operations_3d.py
@@ -7,7 +7,6 @@ from astropy.utils.misc import NumpyRNGContext
 
 
 __all__ = ('elementwise_dot', )
-__author__ = ('Andrew Hearin', )
 
 
 def elementwise_dot(x, y):
@@ -38,3 +37,26 @@ def elementwise_dot(x, y):
     x = np.atleast_2d(x)
     y = np.atleast_2d(y)
     return np.sum(x*y, axis=1)
+
+
+def elementwise_norm(x):
+    """ Calculate the normalization of each element in a list of 3d points.
+
+    Parameters
+    ----------
+    x : ndarray
+        Numpy array of shape (npts, 3) storing a collection of 3d points
+
+    Returns
+    -------
+    result : ndarray
+        Numpy array of shape (npts, ) storing the norm of each 3d point in x.
+
+    Examples
+    --------
+    >>> npts = int(1e3)
+    >>> x = np.random.random((npts, 3))
+    >>> norms = elementwise_norm(x)
+    """
+    x = np.atleast_2d(x)
+    return np.sqrt(np.sum(x**2, axis=1))
