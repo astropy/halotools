4858ec01dd0abab1c289782e35d0fc0998e24cb7
Duncan Campbell <duncan.campbell@yale.edu>
2018-01-24 17:35:06 -0500

    setting up EE function

--- halotools/mock_observables/alignments/__init__.py
+++ halotools/mock_observables/alignments/__init__.py
@@ -8,4 +8,6 @@ from .w_gminus import w_gminus
 from .w_plusplus import w_plusplus
 from .w_minusminus import w_minusminus
 
-__all__ = ('w_gplus', 'w_gminus', 'w_plusplus', 'w_minusminus')
+from .eta import eta
+
+__all__ = ('w_gplus', 'w_gminus', 'w_plusplus', 'w_minusminus', 'eta')
