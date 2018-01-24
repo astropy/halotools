04793fbf4cfee6e4af1deb95a4d14de443ab8cd4
Duncan Campbell <duncan.campbell@yale.edu>
2018-01-24 16:47:01 -0500

    updated init

--- halotools/mock_observables/alignments/__init__.py
+++ halotools/mock_observables/alignments/__init__.py
@@ -4,5 +4,8 @@ on galaxy/halo alignments.
 from __future__ import absolute_import
 
 from .w_gplus import w_gplus
+from .w_gminus import w_gminus
+from .w_plusplus import w_plusplus
+from .w_minusminus import w_minusminus
 
-__all__ = ('w_gplus')
+__all__ = ('w_gplus', 'w_gminus', 'w_plusplus', 'w_minusminus')
