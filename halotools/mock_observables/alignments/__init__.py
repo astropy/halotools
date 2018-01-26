026b748622c1d0fe9e55f57c804d194e457d5f06
Duncan Campbell <duncan.campbell@yale.edu>
2018-01-26 13:54:08 -0500

    renaming alignment functions

--- halotools/mock_observables/alignments/__init__.py
+++ halotools/mock_observables/alignments/__init__.py
@@ -1,13 +1,31 @@
-""" Subpackage containing modules of functions that calculate many variations
-on galaxy/halo alignments.
+"""
+Subpackage containing modules of functions that calculate many variations on galaxy/halo alignments.
 """
 from __future__ import absolute_import
 
-from .w_gplus import w_gplus
-from .w_gminus import w_gminus
-from .w_plusplus import w_plusplus
-from .w_minusminus import w_minusminus
+# ellipticity-ellipticity tpcf
+from .ee_3d import ee_3d
+from .ee_projected import ee_projected
 
-from .eta import eta
+# ellipticity-direction tpcf
+from .ed_3d import ed_3d
+from .ed_projected import ed_projected
 
-__all__ = ('w_gplus', 'w_gminus', 'w_plusplus', 'w_minusminus', 'eta')
+# gravitational shear-intrinsic ellipticity tpcf
+from .gi_plus_3d import gi_plus_3d
+from .gi_plus_projected import gi_plus_projected
+from .gi_minus_3d import gi_minus_3d
+from .gi_minus_projected import gi_minus_projected
+
+# intrinsic ellipticity-intrinsic ellipticity tpcf
+from .ii_plus_3d import ii_plus_3d
+from .ii_plus_projected import ii_plus_projected
+from .ii_minus_3d import ii_minus_3d
+from .ii_minus_projected import ii_minus_projected
+
+__all__ = ('ee_3d', 'ee_projected',
+           'ed_3d', 'ed_projected',
+           'gi_plus_3d', 'gi_plus_projected',
+           'gi_minus_3d', 'gi_minus_projected',
+           'ii_plus_3d', 'ii_plus_projected',
+           'ii_minus_3d', 'ii_minus_projected')
--- halotools/mock_observables/alignments/ed_3d.py
+++ halotools/mock_observables/alignments/ed_3d.py
@@ -1,5 +1,5 @@
 r"""
-Module containing the `~halotools.mock_observables.alignments.eta` function used to
+Module containing the `~halotools.mock_observables.alignments.omega` function used to
 calculate the ellipticity-ellipticity correlation functon
 """
 
@@ -12,7 +12,7 @@ from .alignment_helpers import process_3d_alignment_args
 from ..mock_observables_helpers import (enforce_sample_has_correct_shape,
     get_separation_bins_array, get_line_of_sight_bins_array, get_period, get_num_threads)
 from ..pair_counters.mesh_helpers import _enforce_maximum_search_length
-from ..pair_counters import marked_npairs_3d
+from ..pair_counters import positional_marked_npairs_3d, npairs_3d
 
 __all__ = ['eta']
 __author__ = ['Duncan Campbell']
@@ -21,10 +21,10 @@ __author__ = ['Duncan Campbell']
 np.seterr(divide='ignore', invalid='ignore')  # ignore divide by zero in e.g. DD/RR
 
 
-def eta(sample1, orientations1, sample2, orientations2, rbins, weights1=None, weights2=None,
+def omega(sample1, orientations1, sample2, rbins, weights1=None, weights2=None,
         period=None, num_threads=1, approx_cell1_size=None, approx_cell2_size=None):
     r"""
-    Calculate the 3-D ellipticity-ellipticity correlation function (EE), :math:`\eta(r)`.
+    Calculate the 3-D ellipticity-direction correlation function (ED), :math:`\omega(r)`.
 
     Parameters
     ----------
@@ -42,19 +42,9 @@ def eta(sample1, orientations1, sample2, orientations2, rbins, weights1=None, we
     sample2 : array_like, optional
         Npts2 x 3 array containing 3-D positions of points.
 
-    orientations2 : array_like
-        Npts1 x 2 numpy array containing projected orientation vectors for each point in ``sample2``.
-        these will be normalized if not already.
-
     rbins : array_like
         array of boundaries defining the radial bins in which pairs are counted.
         Length units are comoving and assumed to be in Mpc/h, here and throughout Halotools.
-
-    weights1 : array_like, optional
-        Npts1 array of weghts.  If this parameter is not specified, it is set to numpy.ones(Npts1).
-
-    weights2 : array_like, optional
-        Npts2 array of weghts.  If this parameter is not specified, it is set to numpy.ones(Npts2).
 
     period : array_like, optional
         Length-3 sequence defining the periodic boundary conditions
@@ -96,10 +86,9 @@ def eta(sample1, orientations1, sample2, orientations2, rbins, weights1=None, we
     The ellipticity-ellipticity correlation function is defined as:
 
     .. math::
-        \eta = \sum_{i \neq j}\frac{|\hat{e}_i \cdot \hat{e}_j|^2}{w_iw_j} - \frac{1}{3}
+        \eta = \sum_{i \neq j}\frac{|\hat{e}_i \cdot \hat{e}_j|^2} - \frac{1}{3}
 
-    where e.g. :math:`\hat{e}_i` is the orientation of the :math:`i`-th galaxy,
-    and :math:`w_i` is the weight given to it.  By default, all weights are set to 1.
+    where e.g. :math:`\hat{e}_i` is the orientation of the :math:`i`-th galaxy.
 
     Example
     --------
@@ -130,17 +119,17 @@ def eta(sample1, orientations1, sample2, orientations2, rbins, weights1=None, we
     We can the calculate the auto-EE correlation between these points:
 
     >>> rbins = np.logspace(-1,1,10)
-    >>> result = eta(sample1, random_orientations, sample1, random_orientations, rbins, period=Lbox)
+    >>> result = omega(sample1, random_orientations, sample1, rbins, period=Lbox)
 
     """
 
     # process arguments
-    alignment_args = (sample1, orientations1, None, weights1,
-                      sample2, orientations2, None, weights2,
+    alignment_args = (sample1, orientations1, None, None,
+                      sample2, None, None, None,
                       None, None, None, None)
     dum = 0.0  # dummy variable to store arguments not needed for this function
-    sample1, orientations1, dum, weights1,\
-    sample2, orientations2, dum, weights2,\
+    sample1, orientations1, dum, dum,\
+    sample2, dum, dum, dum,\
     dum, dum, dum, dum = process_3d_alignment_args(*alignment_args)
 
     function_args = (sample1, rbins, sample2, period, num_threads)
@@ -151,18 +140,17 @@ def eta(sample1, orientations1, sample2, orientations2, rbins, weights1=None, we
     N2 = len(sample2)
 
     marks1 = orientations1
-    marks2 = orientations2
-    marked_counts = marked_npairs_3d(sample1, sample2, rbins,
-                          period=period, weights1=marks1, weights2=marks2,
-                          weight_func_id=13, verbose=False, num_threads=num_threads,
-                          approx_cell1_size=approx_cell1_size, approx_cell2_size=approx_cell1_size)
+    marks2 = np.zeros((N2,3))
+    marked_counts = positional_marked_npairs_3d(sample1, sample2, rbins,
+                                                period=period, weights1=marks1, weights2=marks2,
+                                                weight_func_id=4, verbose=False, num_threads=num_threads,
+                                                approx_cell1_size=approx_cell1_size, approx_cell2_size=approx_cell1_size)
+    marked_counts = np.diff(marked_counts)
 
-    marks1 = weights1
-    marks2 = weights2
-    counts = marked_npairs_3d(sample1, sample2, rbins,
-                          period=period, weights1=marks1, weights2=marks2,
-                          weight_func_id=1, verbose=False, num_threads=num_threads,
-                          approx_cell1_size=approx_cell1_size, approx_cell2_size=approx_cell1_size)
+    counts = npairs_3d(sample1, sample2, rbins, period=period,
+                       verbose=False, num_threads=num_threads,
+                       approx_cell1_size=approx_cell1_size, approx_cell2_size=approx_cell1_size)
+    counts = np.diff(counts)
 
     return marked_counts/counts - 1.0/3.0
 
