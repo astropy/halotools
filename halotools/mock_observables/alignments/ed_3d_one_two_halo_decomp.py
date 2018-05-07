fixed masking

halotools/mock_observables/alignments/ed_3d_one_two_halo_decomp.py
@@ -145,10 +145,14 @@ def ed_3d_one_two_halo_decomp(sample1, orientations1, sample1_host_halo_id,
 
     >>> random_orientations = np.random.random((Npts,3))
 
+    And a set of random halo ids for each point
+
+    >>> halo_ids = np.random.randint(1, 10, Npts)
+
     We can the calculate the auto-ED correlation between these points:
 
     >>> rbins = np.logspace(-1,1,10)
-    >>> result = ed_3d(sample1, random_orientations, sample1, rbins, period=Lbox)
+    >>> result = ed_3d_one_two_halo_decomp(sample1, random_orientations,  halo_ids, sample1, halo_ids, rbins, period=Lbox)
 
     """
 
@@ -242,15 +246,15 @@ def ed_3d_one_two_halo_decomp(sample1, orientations1, sample1_host_halo_id,
     marks2[:,0] = sample2_host_halo_id
     marks2[:,1] = weights2
 
-    counts_1h = marked_npairs_3d(sample1, sample2, rbins,
-                       weights1=marks1, weights2=marks2, weight_func_id=3,
+    counts_1h = marked_npairs_3d(sample1[mask1], sample2[mask2], rbins,
+                       weights1=marks1[mask1], weights2=marks2[mask2], weight_func_id=3,
                        period=period, verbose=False, num_threads=num_threads,
                        approx_cell1_size=approx_cell1_size,
                        approx_cell2_size=approx_cell2_size)
     counts_1h = np.diff(counts_1h)
 
-    counts_2h = marked_npairs_3d(sample1, sample2, rbins,
-                       weights1=marks1, weights2=marks2, weight_func_id=4,
+    counts_2h = marked_npairs_3d(sample1[mask1], sample2[mask2], rbins,
+                       weights1=marks1[mask1], weights2=marks2[mask2], weight_func_id=4,
                        period=period, verbose=False, num_threads=num_threads,
                        approx_cell1_size=approx_cell1_size,
                        approx_cell2_size=approx_cell2_size)

halotools/mock_observables/alignments/ee_3d_one_two_halo_decomp.py
@@ -23,9 +23,12 @@ np.seterr(divide='ignore', invalid='ignore')  # ignore divide by zero in e.g. DD
 
 
 def ee_3d_one_two_halo_decomp(sample1, orientations1, sample1_host_halo_id,
-          sample2, orientations2, sample2_host_halo_id,
-          rbins, weights1=None, weights2=None, period=None,
-          num_threads=1, approx_cell1_size=None, approx_cell2_size=None):
+                              sample2, orientations2, sample2_host_halo_id,
+                              rbins, 
+                              weights1=None, weights2=None,
+                              mask1=None, mask2=None,
+                              period=None, num_threads=1,
+                              approx_cell1_size=None, approx_cell2_size=None):
     r"""
     Calculate the one and two halo componenents of the 3-D ellipticity-ellipticity
     correlation function (EE), :math:`\eta_{\rm 1h}(r)`, and :math:`\eta_{\rm 2h}(r)`.
@@ -140,7 +143,7 @@ def ee_3d_one_two_halo_decomp(sample1, orientations1, sample1_host_halo_id,
 
     And a set of random halo ids for each point
 
-    halo_ids = np.random.random_integers(1, 10, Npts)
+    >>> halo_ids = np.random.randint(1, 10, Npts)
 
     We can the calculate the auto-EE correlation between these points:
 
@@ -165,6 +168,24 @@ def ee_3d_one_two_halo_decomp(sample1, orientations1, sample1_host_halo_id,
     N1 = len(sample1)
     N2 = len(sample2)
 
+    # process mask1
+    if mask1 is None:
+        mask1 = np.array([True]*N1)
+    else:
+        mask1 = np.atleast_1d(mask1).astype('bool')
+        if np.shape(mask1) != (N1,):
+            msg = ('`mask1` is not the correct shape.')
+            raise ValueError(msg)
+    
+    # process mask2
+    if mask2 is None:
+        mask2 = np.array([True]*N2)
+    else:
+        mask2 = np.atleast_1d(mask2).astype('bool')
+        if np.shape(mask2) != (N2,):
+            msg = ('`mask2` is not the correct shape.')
+            raise ValueError(msg)
+
     # process halo ids
     sample1_host_halo_id = np.atleast_1d(sample1_host_halo_id).astype('int')
     sample2_host_halo_id = np.atleast_1d(sample2_host_halo_id).astype('int')
@@ -188,14 +209,14 @@ def ee_3d_one_two_halo_decomp(sample1, orientations1, sample1_host_halo_id,
     marks2[:,3] = orientations2[:,2]
     marks2[:,4] = sample2_host_halo_id
 
-    marked_counts_1h = marked_npairs_3d(sample1, sample2, rbins,
-                                        period=period, weights1=marks1, weights2=marks2,
+    marked_counts_1h = marked_npairs_3d(sample1[mask1], sample2[mask2], rbins,
+                                        period=period, weights1=marks1[mask1], weights2=marks2[mask2],
                                         weight_func_id=16, verbose=False, num_threads=num_threads,
                                         approx_cell1_size=approx_cell1_size, approx_cell2_size=approx_cell2_size)
     marked_counts_1h = np.diff(marked_counts_1h)
 
-    marked_counts_2h = marked_npairs_3d(sample1, sample2, rbins,
-                                        period=period, weights1=marks1, weights2=marks2,
+    marked_counts_2h = marked_npairs_3d(sample1[mask1], sample2[mask2], rbins,
+                                        period=period, weights1=marks1[mask1], weights2=marks2[mask2],
                                         weight_func_id=17, verbose=False, num_threads=num_threads,
                                         approx_cell1_size=approx_cell1_size, approx_cell2_size=approx_cell2_size)
     marked_counts_2h = np.diff(marked_counts_2h)
@@ -222,15 +243,15 @@ def ee_3d_one_two_halo_decomp(sample1, orientations1, sample1_host_halo_id,
     marks2[:,0] = sample2_host_halo_id
     marks2[:,1] = weights2
 
-    counts_1h = marked_npairs_3d(sample1, sample2, rbins,
-                       weights1=marks1, weights2=marks2, weight_func_id=3,
+    counts_1h = marked_npairs_3d(sample1[mask1], sample2[mask2], rbins,
+                       weights1=marks1[mask1], weights2=marks2[mask2], weight_func_id=3,
                        period=period, verbose=False, num_threads=num_threads,
                        approx_cell1_size=approx_cell1_size,
                        approx_cell2_size=approx_cell2_size)
     counts_1h = np.diff(counts_1h)
 
-    counts_2h = marked_npairs_3d(sample1, sample2, rbins,
-                       weights1=marks1, weights2=marks2, weight_func_id=4,
+    counts_2h = marked_npairs_3d(sample1[mask1], sample2[mask2], rbins,
+                       weights1=marks1[mask1], weights2=marks2[mask2], weight_func_id=4,
                        period=period, verbose=False, num_threads=num_threads,
                        approx_cell1_size=approx_cell1_size,
                        approx_cell2_size=approx_cell2_size)