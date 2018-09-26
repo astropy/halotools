9cfb0a990f3349a137f9115d867a8156e7277374
Duncan Campbell <duncan.campbell@yale.edu>
2018-09-26 11:31:39 -0400

    added tests

--- tests/test_3d.py
+++ tests/test_3d.py
@@ -5,7 +5,9 @@ from astropy.utils.misc import NumpyRNGContext
 
 from ..rotations3d import *
 
-__all__ = ('test_rotation_matrices_from_vectors', )
+__all__ = ('test_rotation_matrices_from_vectors',
+           'test_rotation_matrices_from_angles',
+           'test_rotation_matrices_from_basis' )
 
 fixed_seed = 43
 
@@ -15,8 +17,46 @@ def test_rotation_matrices_from_vectors():
     test to make sure null rotations return identiy matrix
     """
 
-    N = 1000
-    v1 = np.random.random((N,3))
+    npts = 1000
+    ndim = 3
+    v1 = np.random.random((npts,ndim))
 
     rot_m = rotation_matrices_from_vectors(v1,v1)
+
+    assert np.all(~np.isnan(rot_m))
+
+
+def test_rotation_matrices_from_angles():
+    """
+    test to make sure null rotations return identiy matrix
+    """
+
+    npts = 1000
+    ndim = 3
+
+    uz = np.zeros((npts,ndim))
+    uz[:,2] = 1.0
+
+    rot_m = rotation_matrices_from_angles(np.zeros(npts), uz)
+
+    assert np.all(~np.isnan(rot_m))
+
+
+def test_rotation_matrices_from_basis():
+    """
+    test to make sure null rotations return identiy matrix
+    """
+
+    npts = 1000
+    ndim = 3
+
+    ux = np.zeros((npts,ndim))
+    ux[:,0] = 1.0
+    uy = np.zeros((npts,ndim))
+    uy[:,1] = 1.0
+    uz = np.zeros((npts,ndim))
+    uz[:,2] = 1.0
+
+    rot_m = rotation_matrices_from_basis(ux, uy, uz)
+
     assert np.all(~np.isnan(rot_m))
