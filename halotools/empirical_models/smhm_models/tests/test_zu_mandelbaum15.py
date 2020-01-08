33a521e7cff7ffe4f0df608bfb06289c5423d2d5
Andrew Hearin <ahearin@anl.gov>
2020-01-08 09:57:39 -0600

    Eliminated warnings due to complex models

--- halotools/empirical_models/smhm_models/tests/test_zu_mandelbaum15.py
+++ halotools/empirical_models/smhm_models/tests/test_zu_mandelbaum15.py
@@ -11,5 +11,5 @@ __all__ = ('test_mc_scatter1', )
 
 
 def test_mc_scatter1():
-    model = ZuMandelbaum15SmHm()
+    model = ZuMandelbaum15SmHm(redshift=0)
     sm = model.mc_stellar_mass(prim_haloprop=1e12)
