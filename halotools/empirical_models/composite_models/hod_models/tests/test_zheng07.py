0d42df8e319fb50fdcd8f39d5e2f3353956afc5d
Andrew Hearin <ahearin@anl.gov>
2021-02-19 16:34:12 -0600

    Added new failing unit test to address #993

--- halotools/empirical_models/composite_models/hod_models/tests/test_zheng07.py
+++ halotools/empirical_models/composite_models/hod_models/tests/test_zheng07.py
@@ -116,3 +116,20 @@ def test_mass_definition_flexibility():
         halocat.halo_table["halo_mvir"]
     )
     model.populate_mock(halocat, seed=43)
+
+
+def test_mass_definition_flexibility2():
+    """Regression test for Issue #993."""
+    halocat = FakeSim()
+    halocat.halo_table["halo_mass_custom"] = np.copy(halocat.halo_table["halo_mvir"])
+    halocat.halo_table["halo_radius_custom"] = np.copy(halocat.halo_table["halo_rvir"])
+    halocat.halo_table.remove_column("halo_upid")
+    halocat.halo_table.remove_column("halo_mvir")
+    halocat.halo_table.remove_column("halo_rvir")
+    model = PrebuiltHodModelFactory(
+        "zheng07",
+        mdef="custom",
+        prim_haloprop_key="halo_mass_custom",
+        halo_boundary_key="halo_radius_custom",
+    )
+    model.populate_mock(halocat)
