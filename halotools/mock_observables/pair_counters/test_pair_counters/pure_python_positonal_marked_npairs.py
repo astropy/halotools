ad9e183ee7471f7876201ce83fdc36a76e653902
Andrew Hearin <ahearin@anl.gov>
2021-08-04 12:32:45 -0500

    Switched import of normalized_vectors from utils.rotations3d to utils.vector_utilities

--- halotools/mock_observables/pair_counters/test_pair_counters/pure_python_positonal_marked_npairs.py
+++ halotools/mock_observables/pair_counters/test_pair_counters/pure_python_positonal_marked_npairs.py
@@ -8,7 +8,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import numpy as np
 
 from ..pairs import distance
-from ....utils.rotations3d import normalized_vectors
+from ....utils.vector_utilities import normalized_vectors
 
 __all__ = ["cos2theta_pairs"]
 __author__ = ["Duncan Campbell"]
