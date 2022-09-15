81aab0326910170671739b48df8f8fa9109a050f
Andrew Hearin <ahearin@anl.gov>
2022-09-15 09:52:06 -0500

    Resolve warning in test_process_optional_input_sample2_case3

--- halotools/mock_observables/two_point_clustering/tests/test_clustering_helpers.py
+++ halotools/mock_observables/two_point_clustering/tests/test_clustering_helpers.py
@@ -1,6 +1,7 @@
 """ Module provides unit-testing for `~halotools.mock_observables.clustering_helpers`.
 """
 from __future__ import absolute_import, division, print_function
+import warnings
 
 import pytest
 import numpy as np
@@ -8,16 +9,15 @@ import numpy as np
 from ..clustering_helpers import verify_tpcf_estimator
 from ..clustering_helpers import process_optional_input_sample2
 
-__all__ = ('test_verify_tpcf_estimator', )
+__all__ = ("test_verify_tpcf_estimator",)
 
 
 def test_verify_tpcf_estimator():
-    """
-    """
-    _ = verify_tpcf_estimator('Natural')
+    """ """
+    _ = verify_tpcf_estimator("Natural")
 
     with pytest.raises(ValueError) as err:
-        _ = verify_tpcf_estimator('Cuba Gooding, Jr.')
+        _ = verify_tpcf_estimator("Cuba Gooding, Jr.")
     substr = "is not in the list of available estimators:"
     assert substr in err.value.args[0]
 
@@ -30,7 +30,8 @@ def test_process_optional_input_sample2_case1():
     do_cross_in = True
 
     sample2_out, _sample1_is_sample2, do_cross = process_optional_input_sample2(
-        sample1_in, sample2_in, do_cross_in)
+        sample1_in, sample2_in, do_cross_in
+    )
     assert np.all(sample1_in == sample2_out)
     assert _sample1_is_sample2 is True
     assert do_cross == do_cross_in
@@ -44,14 +45,16 @@ def test_process_optional_input_sample2_case2():
 
     do_cross_in = False
     sample2_out, _sample1_is_sample2, do_cross = process_optional_input_sample2(
-        sample1_in, sample2_in, do_cross_in)
+        sample1_in, sample2_in, do_cross_in
+    )
     assert np.all(sample2_in == sample2_out)
     assert _sample1_is_sample2 is False
     assert do_cross == do_cross_in
 
     do_cross_in = True
     sample2_out, _sample1_is_sample2, do_cross = process_optional_input_sample2(
-        sample1_in, sample2_in, do_cross_in)
+        sample1_in, sample2_in, do_cross_in
+    )
     assert do_cross == do_cross_in
 
 
@@ -62,8 +65,12 @@ def test_process_optional_input_sample2_case3():
     sample2_in = sample1_in
 
     do_cross_in = True
-    sample2_out, _sample1_is_sample2, do_cross = process_optional_input_sample2(
-        sample1_in, sample2_in, do_cross_in)
+    with warnings.catch_warnings(record=True) as w:
+        sample2_out, _sample1_is_sample2, do_cross = process_optional_input_sample2(
+            sample1_in, sample2_in, do_cross_in
+        )
+        assert "exactly the same" in str(w[-1].message)
+
     assert np.all(sample2_in == sample2_out)
     assert _sample1_is_sample2 is True
     assert do_cross is False
