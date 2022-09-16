fa530c0eee8d23b987a19e6cf0addda480b871ea
Andrew Hearin <ahearin@anl.gov>
2022-09-16 17:10:58 -0500

    package_name --> name in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ rst_epilog += """
 # -- Project information ------------------------------------------------------
 
 # This does not *have* to match the package name, but typically does
-project = setup_cfg["package_name"]
+project = setup_cfg["name"]
 author = setup_cfg["author"]
 copyright = "{0}, {1}".format(datetime.datetime.now().year, setup_cfg["author"])
 
@@ -79,8 +79,8 @@ copyright = "{0}, {1}".format(datetime.datetime.now().year, setup_cfg["author"])
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 
-__import__(setup_cfg["package_name"])
-package = sys.modules[setup_cfg["package_name"]]
+__import__(setup_cfg["name"])
+package = sys.modules[setup_cfg["name"]]
 
 # The short X.Y version.
 version = package.__version__.split("-", 1)[0]
@@ -155,7 +155,7 @@ man_pages = [("index", project.lower(), project + " Documentation", [author], 1)
 if eval(setup_cfg.get("edit_on_github")):
     extensions += ["astropy_helpers.sphinx.ext.edit_on_github"]
 
-    versionmod = __import__(setup_cfg["package_name"] + ".version")
+    versionmod = __import__(setup_cfg["name"] + ".version")
     edit_on_github_project = setup_cfg["github_project"]
     if versionmod.version.release:
         edit_on_github_branch = "v" + versionmod.version.version
