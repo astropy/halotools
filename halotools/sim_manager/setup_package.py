1adb10564c68778b0d9468a9e5a232e78e64e177
Andrew Hearin <ahearin@anl.gov>
2022-09-19 11:15:49 -0500

    Change include_dirs from 'numpy' to np.get_include() in all setup_package.py files

--- halotools/sim_manager/setup_package.py
+++ halotools/sim_manager/setup_package.py
@@ -3,5 +3,4 @@
 
 
 def get_package_data():
-    return {
-        'halotools.sim_manager.tests': ['data/dummy_halocat_0.07812.list']}
+    return {"halotools.sim_manager.tests": ["data/dummy_halocat_0.07812.list"]}
