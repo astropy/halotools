9b1ca8d788eaf9f3f83f24f458671ef2a3d4953f
Nick Van Alfen <nvanalfen2@gmail.com>
2023-02-23 14:59:32 -0600

    Removed occupation_models and nfw_phase_space from this branch to limit work to ia

--- halotools/empirical_models/ia_models/__init__.py
+++ halotools/empirical_models/ia_models/__init__.py
@@ -1,6 +1,4 @@
 from .ia_model_components import *
 from .ia_strength_models import *
-from .nfw_phase_space import *
-from .occupation_models import *
 from .watson_distribution import DimrothWatson
 from .ia_correlations import *
